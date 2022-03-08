6d5d358f00d227e2153125477ce20c87b26c6c4b
Marcus Baw <marcusbaw@gmail.com>
2022-03-08 16:43:21 +0000

    updated cohort calculation to reject imossible cohort numbers

--- rcpch-audit-engine/epilepsy12/general_functions/cohort_number.py
+++ rcpch-audit-engine/epilepsy12/general_functions/cohort_number.py
@@ -7,21 +7,27 @@ from datetime import date
 
 def cohort_number_from_enrolment_date(enrolment_date:date) -> int:
     """
+    * Returns the cohort number as an `int` from the enrolment date as a Python `datetime.date`
     
+    * Dates which are too early to return a valid cohort number will return `None`. Ensure to test for a `None` return value before doing anything with the result.
+        
     * Cohort Number is used to identify the groups in the audit by year.
     
     * Cohorts are defined between 1st December year and 30th November in the subsequent year.
     
     * Time zone is not explicity supplied. Since this is a UK audit, time zone is assumed always to be UK.
 
-    ## Examples:
-        
+    #### Examples of cohort numbers:
     Cohort 4: 1 December 2020 - 30 November 2021
     Cohort 5: 1 December 2021 - 30 November 2022
     Cohort 6: 1 December 2022 - 30 November 2023
     Cohort 7: 1 December 2023 - 30 November 2024
     """
     
-    for cohort_starting_year in range(2020,2025):
+    # reject dates which are too early with a return value of None
+    if enrolment_date.year < 2020:
+        return None
+    
+    for cohort_starting_year in range(2020,2030):
         if enrolment_date >= date(cohort_starting_year, 12, 1) and enrolment_date <= date(cohort_starting_year+1, 11, 30):
             return cohort_starting_year - 2016    
