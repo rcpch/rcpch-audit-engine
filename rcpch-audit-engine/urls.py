7d0c3a1b1442a2c27498912534df05939887a841
Marcus Baw <marcusbaw@gmail.com>
2022-05-12 18:27:49 +0100

    add root path and tidy quotes

--- rcpch-audit-engine/urls.py
+++ rcpch-audit-engine/urls.py
@@ -3,10 +3,11 @@ from django.urls import path, include
 
 
 urlpatterns = [
+    path('', include('epilepsy12.urls')),
     path('admin/', admin.site.urls),
-    path("epilepsy12/", include("epilepsy12.urls")),
+    path('epilepsy12/', include('epilepsy12.urls')),
     path('epilepsy12/', include('django.contrib.auth.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     # reactive 3rd party library
-    path("unicorn/", include("django_unicorn.urls")),
+    path('unicorn/', include('django_unicorn.urls')),
 ]
