7fada5c49215fd4f26562ac25034516769a829a6
Simon Chapman <eatyourpeasapps@gmail.com>
2022-06-27 23:29:28 +0100

    add chartjs

--- epilepsy12/views.py
+++ epilepsy12/views.py
@@ -4,6 +4,10 @@ from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
+from epilepsy12.constants.ethnicities import ETHNICITIES
+from epilepsy12.models.case import Case
+from django.db.models import Count, When, Value, CharField
+from django.db.models import Case as DJANGO_CASE
 
 from .view_folder import *
 
@@ -25,9 +29,26 @@ def hospital_reports(request):
     template_name = 'epilepsy12/hospital.html'
     hospital_object = HospitalTrust.objects.get(
         OrganisationName=request.user.hospital_trust)
+
+    ethnicity_long_list = [When(ethnicity=k, then=Value(v))
+                           for k, v in ETHNICITIES]
+
+    case = (
+        Case.objects.values('ethnicity')
+        .annotate(
+            ethnicity_display=DJANGO_CASE(
+                *ethnicity_long_list, output_field=CharField()
+            )
+        )
+        .values('ethnicity_display')
+        .annotate(
+            ethnicities=Count('ethnicity')).order_by()
+    )
+
     return render(request=request, template_name=template_name, context={
         'user': request.user,
-        'hospital': hospital_object
+        'hospital': hospital_object,
+        'cases': case
     })
 
 
