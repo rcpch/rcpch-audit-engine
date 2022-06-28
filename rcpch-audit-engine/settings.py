4283e9bdb0752e0d4fdba1e04d8101993f2a22b9
Simon Chapman <eatyourpeasapps@gmail.com>
2022-06-28 22:54:36 +0100

    staticfix

--- rcpch-audit-engine/settings.py
+++ rcpch-audit-engine/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
