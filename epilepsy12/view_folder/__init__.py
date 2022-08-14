ba9c8bfeb50f4e6cb0d807802f0f8f83c6b99e3b
Simon Chapman <eatyourpeasapps@gmail.com>
2022-08-14 12:12:34 +0100

    investigation_ remove from init.py

--- epilepsy12/view_folder/__init__.py
+++ epilepsy12/view_folder/__init__.py
@@ -4,7 +4,6 @@ from .initial_assessment_views import *
 from .assessment_views import *
 from .epilepsy_context_views import *
 from .multiaxial_description_views import *
-from .investigation_management_views import *
 from .comorbidity_views import *
 from .hospital_trust_autocomplete import *
 from .semiology_keyword_autocomplete import *
