39e2bcff0b4440b4ae6e60267d5d0f1f00034702
Simon Chapman <eatyourpeasapps@gmail.com>
2022-08-19 21:46:31 +0100

    match routes for menu active item

--- epilepsy12/templatetags/epilepsy12_template_tags.py
+++ epilepsy12/templatetags/epilepsy12_template_tags.py
@@ -45,3 +45,19 @@ def matches_model_field(field_name, model):
             return True
         else:
             return False
+
+
+@register.filter
+def is_in(url_name, args):
+    """
+    receives the request.resolver_match.url_name
+    and compares with the template name (can be a list in a string separated by commas),
+    returning true if a match is present
+    """
+    if args is None:
+        return None
+    arg_list = [arg.strip() for arg in args.split(',')]
+    if url_name in arg_list:
+        return True
+    else:
+        return False
