930beeeba66518337c40bcf05f0fdcd24609d937
Simon Chapman <eatyourpeasapps@gmail.com>
2022-08-21 14:20:19 +0100

    remove AP creation

--- epilepsy12/views.py
+++ epilepsy12/views.py
@@ -37,17 +37,17 @@ def hospital_reports(request):
     For first run of app only and then comment out
     """
 
-    all_registrations = Registration.objects.all()
-    for registration in all_registrations.iterator():
-        ap = AuditProgress.objects.create(
-            initial_assessment_complete=False,
-            assessment_complete=False,
-            epilepsy_context_complete=False,
-            multiaxial_description_complete=False,
-            investigation_management_complete=False
-        )
-        registration.audit_progress = ap
-        registration.save()
+    # all_registrations = Registration.objects.all()
+    # for registration in all_registrations.iterator():
+    #     ap = AuditProgress.objects.create(
+    #         initial_assessment_complete=False,
+    #         assessment_complete=False,
+    #         epilepsy_context_complete=False,
+    #         multiaxial_description_complete=False,
+    #         investigation_management_complete=False
+    #     )
+    #     registration.audit_progress = ap
+    #     registration.save()
 
     deprivation_quintiles = (
         (1, 1),
