0848546e1ca4db574954a18acd75b5b0ee0dc948
Simon Chapman <eatyourpeasapps@gmail.com>
2022-10-18 14:32:06 +0100

    fix migrate error

--- epilepsy12/migrations/0015_remove_investigations_mri_brain_performed_date_and_more.py
+++ epilepsy12/migrations/0015_remove_investigations_mri_brain_performed_date_and_more.py
@@ -14,33 +14,34 @@ class Migration(migrations.Migration):
             model_name='investigations',
             name='mri_brain_performed_date',
         ),
-        migrations.RemoveField(
-            model_name='syndrome',
-            name='syndrome_diagnosis_active',
-        ),
         migrations.AddField(
             model_name='investigations',
             name='mri_brain_reported_date',
-            field=models.DateField(blank=True, default=None, help_text={'label': 'Date MRI brain reported', 'reference': 'Date MRI brain reported'}, null=True),
+            field=models.DateField(blank=True, default=None, help_text={
+                                   'label': 'Date MRI brain reported', 'reference': 'Date MRI brain reported'}, null=True),
         ),
         migrations.AlterField(
             model_name='investigations',
             name='ct_head_scan_status',
-            field=models.BooleanField(blank=True, default=None, help_text={'label': 'Has a CT head been performed?', 'reference': 'NICE states if MRI is contraindicated, consider a CT scan for children, young people and adults with epilepsy.'}, null=True),
+            field=models.BooleanField(blank=True, default=None, help_text={
+                                      'label': 'Has a CT head been performed?', 'reference': 'NICE states if MRI is contraindicated, consider a CT scan for children, young people and adults with epilepsy.'}, null=True),
         ),
         migrations.AlterField(
             model_name='investigations',
             name='eeg_indicated',
-            field=models.BooleanField(blank=True, default=None, help_text={'label': 'Has a first EEG been requested?', 'reference': 'All children with Epilepsy should have an EEG'}, null=True),
+            field=models.BooleanField(blank=True, default=None, help_text={
+                                      'label': 'Has a first EEG been requested?', 'reference': 'All children with Epilepsy should have an EEG'}, null=True),
         ),
         migrations.AlterField(
             model_name='investigations',
             name='mri_indicated',
-            field=models.BooleanField(blank=True, default=None, help_text={'label': 'Has a brain MRI been requested?', 'reference': 'NICE recommends that an MRI scan should be offered to children, young people and adults diagnosed with epilepsy, unless they have idiopathic generalised epilepsy or self-limited epilepsy with centrotemporal spikes. The MRI should be carried out within 6 weeks of the MRI referral.'}, null=True),
+            field=models.BooleanField(blank=True, default=None, help_text={
+                                      'label': 'Has a brain MRI been requested?', 'reference': 'NICE recommends that an MRI scan should be offered to children, young people and adults diagnosed with epilepsy, unless they have idiopathic generalised epilepsy or self-limited epilepsy with centrotemporal spikes. The MRI should be carried out within 6 weeks of the MRI referral.'}, null=True),
         ),
         migrations.AlterField(
             model_name='investigations',
             name='twelve_lead_ecg_status',
-            field=models.BooleanField(blank=True, default=None, help_text={'label': 'Has a 12-Lead ECG been performed?', 'reference': 'The Epilepsy12 standard is that all children with an convulsive episode should have a 12 lead ECG'}, null=True),
+            field=models.BooleanField(blank=True, default=None, help_text={
+                                      'label': 'Has a 12-Lead ECG been performed?', 'reference': 'The Epilepsy12 standard is that all children with an convulsive episode should have a 12 lead ECG'}, null=True),
         ),
     ]
