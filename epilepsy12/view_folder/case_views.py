8a5508df2e7df26a852baf931ff9ffbb87a03910
Simon Chapman <eatyourpeasapps@gmail.com>
2023-03-10 22:02:35 +0000

    Redirect on create case Fixes #273

--- epilepsy12/view_folder/case_views.py
+++ epilepsy12/view_folder/case_views.py
@@ -313,6 +313,7 @@ def create_case(request, hospital_id):
                 case=obj
             )
             messages.success(request, "You successfully created the case")
+            return redirect('cases', hospital_id=hospital_id)
         else:
             messages.error(request, "Case not created")
             return redirect('cases', hospital_id=hospital_id)
