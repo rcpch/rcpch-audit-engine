b335218d30272f1d6822a79d984184f7d6620caf
Simon Chapman <eatyourpeasapps@gmail.com>
2023-03-24 23:47:58 +0000

    fix postcode validaton for no fixed abode

--- epilepsy12/general_functions/postcode.py
+++ epilepsy12/general_functions/postcode.py
@@ -1,8 +1,22 @@
 import requests
+from ..constants import UNKNOWN_POSTCODES
 
 
 def is_valid_postcode(postcode):
+    """
+    Test if valid postcode using api.postcodes.io 
+    Allow any no fixed abode etc standard codes
+    """
 
+    # convert to upper case and remove spaces
+    formatted = postcode.upper().replace(' ', '')
+    # look for unknown postcodes
+    unknown = [code for code in UNKNOWN_POSTCODES if code.replace(
+        ' ', '') == formatted]
+    if len(unknown) > 0:
+        return True
+
+    # check against API
     url = f"https://api.postcodes.io/postcodes/{postcode}/validate"
     response = requests.get(url=url)
     if response.status_code == 404:
