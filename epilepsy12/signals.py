e3b57ecaaee49535aa2cc47d271d2301ed61a9ca
Marcus Baw <marcusbaw@gmail.com>
2023-03-29 09:57:24 +0100

    fix to get_client_ip so it doesn't return Port

--- epilepsy12/signals.py
+++ epilepsy12/signals.py
@@ -41,9 +41,4 @@ def log_user_logout(sender, request, user, **kwargs):
 
 # helper functions
 def get_client_ip(request):
-    x_forwarded_for = request.META.get("HTTP_X_FORWARDED_FOR")
-    return (
-        x_forwarded_for.split(",")[0]
-        if x_forwarded_for
-        else request.META.get("REMOTE_ADDR")
-    )
+    return request.META.get("REMOTE_ADDR")
