a6d6bc15f1a189e74b1976690965e47936678dac
Anchit <anchit97123@gmail.com>
2023-04-14 13:06:13 +0100

    update git hash context processor to 'Hash not found.' if no hash

--- rcpch-audit-engine/context_processors.py
+++ rcpch-audit-engine/context_processors.py
@@ -1,6 +1,11 @@
 import os 
 
 def export_vars(request):
+    """
+    Gets the `GIT_HASH` environment variable, created in GitHub Actions workflow, inserts into all templates.
+    """
     data = {}
-    data['GIT_HASH'] = os.environ['GIT_HASH']
+    data['GIT_HASH'] = os.environ.get('GIT_HASH')
+    if not data['GIT_HASH']:
+        data['GIT_HASH'] = 'Hash not found.'
     return data
