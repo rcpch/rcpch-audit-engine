4b2dfdf0d02e01048639498a294915762a07661b
anchit-chandran <anchit97123@gmail.com>
2023-04-19 10:28:21 +0100

    testing branch git display
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- rcpch-audit-engine/context_processors.py
+++ rcpch-audit-engine/context_processors.py
@@ -7,7 +7,11 @@ def export_vars(request):
     # Load the txt file containing hash
     try:
         with open('git_hash.txt', 'r') as f:
-            git_hash = f.read().replace('GIT_HASH=','')
+            
+            git_hash, git_branch = f.read().split(',')
+            git_hash = git_hash.replace('GIT_HASH=','')
+            git_branch = git_branch.replace('GIT_BRANCH=','')
+            print(f"{git_hash = }, {git_branch = }")
 
             # if file is empty
             if not git_hash:
@@ -15,4 +19,4 @@ def export_vars(request):
                 
     except FileNotFoundError:
         git_hash = 'No commit hash found! Ignore if running locally.'
-    return {'GIT_HASH': git_hash}
+    return {'GIT_HASH': git_hash, 'GIT_BRANCH':git_branch}
