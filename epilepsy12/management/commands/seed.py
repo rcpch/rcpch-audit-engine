cd56d4172d60941e003bdf362245142676d1274c
Anchit <anchit97123@gmail.com>
2023-04-25 11:04:20 +0100

    Revert "converts dummy_cases seed to migration"
    This reverts commit c631b507c1b39374ac00a04db8106055beb94aa9.

--- epilepsy12/management/commands/seed.py
+++ epilepsy12/management/commands/seed.py
@@ -24,7 +24,10 @@ class Command(BaseCommand):
         parser.add_argument('--mode', type=str, help="Mode")
 
     def handle(self, *args, **options):
-        if (options['mode'] == 'seed_registrations'):
+        if (options['mode'] == 'seed_dummy_cases'):
+            self.stdout.write('seeding with dummy case data...')
+            run_dummy_cases_seed()
+        elif (options['mode'] == 'seed_registrations'):
             self.stdout.write(
                 'register cases in audit and complete all fields with random answers...')
             run_registrations()
@@ -51,6 +54,82 @@ class Command(BaseCommand):
         self.stdout.write(image())
         print('\033[38;2;17;167;107m')
         self.stdout.write('done.')
+
+def run_dummy_cases_seed():
+    added = 0
+    print('\033[33m', 'Seeding fictional cases...', '\033[33m')
+    # there should not be any cases yet, but sometimes seed gets run more than once
+    if Case.objects.all().exists():
+        print(f'Cases already exist. Skipping this step...')
+        return
+
+    postcode_list = random_postcodes.generate_postcodes(requested_number=100)
+
+    random_organisations = []
+
+    # first populate Addenbrooke's for ease of dev testing
+    for _ in range(1, 11):
+        random_organisations.append(
+            Organisation.objects.get(ODSCode='RGT01'))
+
+    # seed the remaining 9
+    for j in range(9):
+        random_organisation = Organisation.objects.order_by("?").first()
+        for i in range(1, 11):
+            random_organisations.append(random_organisation)
+
+    for index in range(len(DUMMY_NAMES) - 1):
+        random_date = date(randint(2005, 2021), randint(1, 12), randint(1, 28))
+        nhs_number = randint(1000000000, 9999999999)
+        first_name = DUMMY_NAMES[index]['firstname']
+        surname = DUMMY_NAMES[index]['lastname']
+        gender_object = DUMMY_NAMES[index]['gender']
+        if gender_object == 'm':
+            sex = 1
+        else:
+            sex = 2
+        date_of_birth = random_date
+        postcode = postcode_list[index]
+        ethnicity = choice(ETHNICITIES)[0]
+
+        # get a random organisation
+        organisation = random_organisations[index]
+
+        case_has_error = False
+
+        try:
+            new_case = Case(
+                locked=False,
+                nhs_number=nhs_number,
+                first_name=first_name,
+                surname=surname,
+                sex=sex,
+                date_of_birth=date_of_birth,
+                postcode=postcode,
+                ethnicity=ethnicity
+            )
+            new_case.save()
+        except Exception as e:
+            print(f"Error saving case: {e}")
+            case_has_error = True
+
+        if not case_has_error:
+            try:
+                new_site = Site.objects.create(
+                    organisation=organisation,
+                    site_is_actively_involved_in_epilepsy_care=True,
+                    site_is_primary_centre_of_epilepsy_care=True,
+                    case=new_case
+                )
+                new_site.save()
+            except Exception as e:
+                print(f"Error saving site: {e}")
+
+            added += 1
+            print(
+                f"{new_case.first_name} {new_case.surname} at {new_site.organisation.OrganisationName} ({new_site.organisation.ParentOrganisation_OrganisationName})...")
+    print(f"Saved {added} cases.")
+
 
 def run_registrations():
     """
