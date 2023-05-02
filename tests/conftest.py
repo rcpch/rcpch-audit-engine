3307f21b5ed7b59ee853ebf6a3fe2bee2c8377b5
anchit-chandran <anchit97123@gmail.com>
2023-05-02 13:49:23 +0100

    initial attempt at loading data into test fixture
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import json
+import pytest
+import os
+from django.core.management import call_command
+
+    
+# @pytest.fixture(scope='session')
+# def django_db_setup(django_db_blocker):
+#     with django_db_blocker.unblock():
+#         call_command('loaddata', 'epilepsy12')
+        
--- tests/test_user_tests.py
+++ tests/test_user_tests.py
@@ -1,42 +1,9 @@
 import pytest
 
 from django.contrib.auth import get_user_model
-from epilepsy12.models import HospitalTrust
-
+from epilepsy12.models import Organisation
+from django.core.management import call_command
 
 @pytest.mark.django_db
-def test_when_new_Epilepsy12User_is_created():
-    
-    ########
-    # 1. ARRANGE
-    ########
-    
-    # user_details_dict
-    user_details = {
-        
-    }
-    
-    # set up objects
-    #get HT with "OrganisationName": "Great Ormond Street Hospital"
-    hospital_trust =  HospitalTrust.objects.filter(OrganisationName__contains = 'Ormond')
-    
-    db = get_user_model()
-
-    ########
-    # 2. ACT
-    ########
-    user = db.objects.create_user(
-        email="testuser@epilepsy12.com",
-        username="epilepsy12user",
-        password="epilepsy12password",
-        title=4,
-        first_name="Henry",
-        surname="Gastaut",
-        role=1,
-        hospital_trust=hospital_trust,
-    )
-
-    ######## 
-    # 3. ASSERT
-    ########
-    assert user.email == "testuser@epilepsy12.com"
+def test_epilepsy12user():
+    print(f"{Organisation.objects.count() = }")
