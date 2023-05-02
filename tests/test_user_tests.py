4ca0efdd2341ba2b316302bfd73e6c594147105d
anchit-chandran <anchit97123@gmail.com>
2023-05-02 15:00:20 +0100

    initial basic test pytest-django
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- tests/test_user_tests.py
+++ tests/test_user_tests.py
@@ -5,11 +5,12 @@ from epilepsy12.models import Organisation
 from django.core.management import call_command
 
 @pytest.mark.django_db
-def test_epilepsy12user():
+def test_create_epilepsy12user():
     user = get_user_model().objects.create(
         email='test@test.com',
         password='password',
         first_name='Test',
         role=1,
     )
-    print(get_user_model().objects.count())
+    assert get_user_model().objects.count() == 1
+    assert get_user_model().objects.first().email == 'test@test.com'
