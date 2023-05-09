618fe37b4b217b625dd4769b3a7877814af738d5
Anchit <anchit97123@gmail.com>
2023-05-09 16:18:10 +0100

    first working user model test
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- tests/test_basic_tests.py
+++ tests/test_basic_tests.py
@@ -1,12 +1,41 @@
 import pytest
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from epilepsy12.models import Organisation
+
+
+
+@pytest.mark.django_db
+def test_create_e12user_should_pass():
+   
+    from epilepsy12.management.commands.create_groups import create_groups
+    
+    create_groups()
+    
+    print(f"{Group.objects.all() = }")
+    
+    db = get_user_model()
+    user = db.objects.create_user(
+            email="testuser@epilepsy12.com",
+            password="epilepsy12password",
+            title=4,
+            first_name="Henry",
+            surname="Gastaut",
+            role=1,
+            organisation_employer = Organisation.objects.first()
+        )
+    print(f"{user=}")
+    
+
+    
 
 @pytest.mark.django_db
 def test_index_request_should_pass(client):
-    url = reverse('index')
+    url = reverse("index")
     response = client.get(url)
     EXPECTED_TEMPLATE_NAME = "epilepsy12/epilepsy12index.html"
-    
+
     assert response.status_code == 200
     assert response.templates[0].name == EXPECTED_TEMPLATE_NAME
 
@@ -16,9 +45,11 @@ def test_organisation_request_NOTAUTH_should_pass(client):
     """
     Non-authenticated user attempts accessing @login_required urls.
     """
-    url = reverse('organisation_reports')
+    url = reverse("organisation_reports")
     response = client.get(url)
     EXPECTED_TEMPLATE_NAME = "epilepsy12/organisation.html"
-    
+
     assert response.status_code != 200
-    
+
+
+
