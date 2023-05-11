e5755d79c4907e55d8a8e8a7224383641c4bd91e
Anchit <anchit97123@gmail.com>
2023-05-11 10:02:28 +0100

    restructure tests folder (naming etc)
    Signed-off-by: Anchit <anchit97123@gmail.com>

---
+++ epilepsy12/tests/model_tests/test_syndrome.py
@@ -0,0 +1,16 @@
+import pytest 
+
+from epilepsy12.models import (
+    Syndrome,
+    )
+
+@pytest.mark.django_db
+def test_syndrome(e12User_GOSH, e12User_GOSH_superuser):
+    """
+    Tests for the Syndrome model.
+    """
+    
+    
+    
+    
+    
