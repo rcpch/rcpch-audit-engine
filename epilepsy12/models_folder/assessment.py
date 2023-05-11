3fb8518a1eb8493689d081f72e096932bd76d63b
Marcus Baw <marcusbaw@gmail.com>
2023-05-11 14:00:14 +0100

    Adds test for assessment model, refactors and renames the elapsed time calculation function to reduce duplication and fix several bugs

--- epilepsy12/models_folder/assessment.py
+++ epilepsy12/models_folder/assessment.py
@@ -2,7 +2,7 @@ from django.contrib.gis.db import models
 from simple_history.models import HistoricalRecords
 from .help_text_mixin import HelpTextMixin
 
-from epilepsy12.general_functions import calculate_time_elapsed
+from epilepsy12.general_functions import stringify_time_elapsed
 from .time_and_user_abstract_base_classes import *
 
 
@@ -16,122 +16,123 @@ class Assessment(TimeStampAbstractBaseClass, UserStampAbstractBaseClass, HelpTex
     Detail
     The cohort number is calculated from the initial date of first paediatric assessment
     """
+
     childrens_epilepsy_surgical_service_referral_criteria_met = models.BooleanField(
         help_text={
-            'label': "Are ANY of these criteria present?",
-            'reference': "Have ANY of the criteria for referral to a children's epilepsy surgery service been met?",
+            "label": "Are ANY of these criteria present?",
+            "reference": "Have ANY of the criteria for referral to a children's epilepsy surgery service been met?",
         },
         blank=True,
         default=None,
-        null=True
+        null=True,
     )
     consultant_paediatrician_referral_made = models.BooleanField(
         help_text={
-            'label': 'Has a referral been made to a consultant paediatrician with expertise in epilepsies?',
-            'reference': 'Has a referral been made to a consultant paediatrician with expertise in epilepsies?',
+            "label": "Has a referral been made to a consultant paediatrician with expertise in epilepsies?",
+            "reference": "Has a referral been made to a consultant paediatrician with expertise in epilepsies?",
         },
         blank=True,
         default=None,
-        null=True
+        null=True,
     )
     consultant_paediatrician_referral_date = models.DateField(
         help_text={
-            'label': "Date of referral to a consultant paediatrician with expertise in epilepsies.",
-            'reference': 'Has a referral been made to a consultant paediatrician with expertise in epilepsies?',
+            "label": "Date of referral to a consultant paediatrician with expertise in epilepsies.",
+            "reference": "Has a referral been made to a consultant paediatrician with expertise in epilepsies?",
         },
         blank=True,
         null=True,
-        default=None
+        default=None,
     )
     consultant_paediatrician_input_date = models.DateField(
         help_text={
-            'label': 'Date seen by a consultant paediatrician with expertise in epilepsies.',
-            'reference': 'Date seen by a consultant paediatrician with expertise in epilepsies.'
+            "label": "Date seen by a consultant paediatrician with expertise in epilepsies.",
+            "reference": "Date seen by a consultant paediatrician with expertise in epilepsies.",
         },
         blank=True,
         default=None,
-        null=True
+        null=True,
     )
     paediatric_neurologist_referral_made = models.BooleanField(
         help_text={
-            'label': "Has a referral to a consultant paediatric neurologist been made?",
-            'reference': "Has a referral to a consultant paediatric neurologist been made?"
+            "label": "Has a referral to a consultant paediatric neurologist been made?",
+            "reference": "Has a referral to a consultant paediatric neurologist been made?",
         },
         blank=True,
         default=None,
-        null=True
+        null=True,
     )
     paediatric_neurologist_referral_date = models.DateField(
         help_text={
-            'label': "Date of referral to a consultant paediatric neurologist.",
-            'reference': "Date of referral to a consultant paediatric neurologist.",
+            "label": "Date of referral to a consultant paediatric neurologist.",
+            "reference": "Date of referral to a consultant paediatric neurologist.",
         },
         blank=True,
         default=None,
-        null=True
+        null=True,
     )
     paediatric_neurologist_input_date = models.DateField(
         help_text={
-            'label': "Date seen by consultant paediatric neurologist.",
-            'reference': "Date seen by consultant paediatric neurologist.",
+            "label": "Date seen by consultant paediatric neurologist.",
+            "reference": "Date seen by consultant paediatric neurologist.",
         },
         blank=True,
         null=True,
-        default=None
+        default=None,
     )
     childrens_epilepsy_surgical_service_referral_made = models.BooleanField(
         help_text={
-            'label': "Has a referral to a children's epilepsy surgery service been made?",
-            'reference': "Has a referral to a children's epilepsy surgery service been made?",
+            "label": "Has a referral to a children's epilepsy surgery service been made?",
+            "reference": "Has a referral to a children's epilepsy surgery service been made?",
         },
         blank=True,
         default=None,
-        null=True
+        null=True,
     )
     childrens_epilepsy_surgical_service_referral_date = models.DateField(
         help_text={
-            'label': "Date of referral to a children's epilepsy surgery service",
-            'reference': "Date of referral to a children's epilepsy surgery service",
+            "label": "Date of referral to a children's epilepsy surgery service",
+            "reference": "Date of referral to a children's epilepsy surgery service",
         },
         blank=True,
         default=None,
-        null=True
+        null=True,
     )
     childrens_epilepsy_surgical_service_input_date = models.DateField(
         help_text={
-            'label': "Date seen by children's epilepsy surgery service",
-            'reference': "Date seen by children's epilepsy surgery service",
+            "label": "Date seen by children's epilepsy surgery service",
+            "reference": "Date seen by children's epilepsy surgery service",
         },
         blank=True,
         default=None,
-        null=True
+        null=True,
     )
     epilepsy_specialist_nurse_referral_made = models.BooleanField(
         help_text={
-            'label': "Has a referral to an epilepsy nurse specialist been made?",
-            'reference': "Has a referral to an epilepsy nurse specialist been made?",
+            "label": "Has a referral to an epilepsy nurse specialist been made?",
+            "reference": "Has a referral to an epilepsy nurse specialist been made?",
         },
         blank=True,
         default=None,
-        null=True
+        null=True,
     )
     epilepsy_specialist_nurse_referral_date = models.DateField(
         help_text={
-            'label': "Date of referral to an epilepsy nurse specialist",
-            'reference': "Date of referral to an epilepsy nurse specialist",
+            "label": "Date of referral to an epilepsy nurse specialist",
+            "reference": "Date of referral to an epilepsy nurse specialist",
         },
         blank=True,
         default=None,
-        null=True
+        null=True,
     )
     epilepsy_specialist_nurse_input_date = models.DateField(
         help_text={
-            'label': "Date seen by an epilepsy nurse specialist",
-            'reference': "Date seen by an epilepsy nurse specialist",
+            "label": "Date seen by an epilepsy nurse specialist",
+            "reference": "Date seen by an epilepsy nurse specialist",
         },
         blank=True,
         default=None,
-        null=True
+        null=True,
     )
 
     history = HistoricalRecords()
@@ -144,42 +145,70 @@ class Assessment(TimeStampAbstractBaseClass, UserStampAbstractBaseClass, HelpTex
     def _history_user(self, value):
         self.updated_by = value
 
-    """
-    calculated fields
-    """
-
     def consultant_paediatrician_wait(self):
         """
         Calculated field. Returns time elapsed between date consultant paediatrician review requested and performed as a string.
         """
-        if self.consultant_paediatrician_referral_date and self.consultant_paediatrician_input_date:
-            return calculate_time_elapsed(self.consultant_paediatrician_referral_date, self.consultant_paediatrician_input_date)
+        if (
+            self.consultant_paediatrician_referral_date
+            and self.consultant_paediatrician_input_date
+        ):
+            return stringify_time_elapsed(
+                self.consultant_paediatrician_referral_date,
+                self.consultant_paediatrician_input_date,
+            )
+        else:
+            raise ValueError("Both referral and input dates must be provided")
 
     def paediatric_neurologist_wait(self):
         """
         Calculated field. Returns time elapsed between date paediatric neurologist review requested and performed as a string.
         """
-        if self.paediatric_neurologist_referral_date and self.paediatric_neurologist_input_date:
-            return calculate_time_elapsed(self.paediatric_neurologist_referral_date, self.paediatric_neurologist_input_date)
+        if (
+            self.paediatric_neurologist_referral_date
+            and self.paediatric_neurologist_input_date
+        ):
+            return stringify_time_elapsed(
+                self.paediatric_neurologist_referral_date,
+                self.paediatric_neurologist_input_date,
+            )
+        else:
+            raise ValueError("Both referral and input dates must be provided")
 
     def childrens_epilepsy_surgery_wait(self):
         """
         Calculated field. Returns time elapsed between date children's epilepsy surgery service review requested and performed as a string.
         """
-        if self.childrens_epilepsy_surgical_service_referral_date and self.childrens_epilepsy_surgical_service_input_date:
-            return calculate_time_elapsed(self.childrens_epilepsy_surgical_service_referral_date, self.childrens_epilepsy_surgical_service_input_date)
+        if (
+            self.childrens_epilepsy_surgical_service_referral_date
+            and self.childrens_epilepsy_surgical_service_input_date
+        ):
+            return stringify_time_elapsed(
+                self.childrens_epilepsy_surgical_service_referral_date,
+                self.childrens_epilepsy_surgical_service_input_date,
+            )
+        else:
+            raise ValueError("Both referral and input dates must be provided")
 
     def epilepsy_nurse_specialist_wait(self):
         """
         Calculated field. Returns time elapsed between date children's epilepsy surgery service review requested and performed as a string.
         """
-        if self.epilepsy_specialist_nurse_referral_date and self.epilepsy_specialist_nurse_input_date:
-            return calculate_time_elapsed(self.epilepsy_specialist_nurse_referral_date, self.epilepsy_specialist_nurse_input_date)
+        if (
+            self.epilepsy_specialist_nurse_referral_date
+            and self.epilepsy_specialist_nurse_input_date
+        ):
+            return stringify_time_elapsed(
+                self.epilepsy_specialist_nurse_referral_date,
+                self.epilepsy_specialist_nurse_input_date,
+            )
+        else:
+            raise ValueError("Both referral and input dates must be provided")
 
     registration = models.OneToOneField(
-        'epilepsy12.Registration',
+        "epilepsy12.Registration",
         on_delete=models.CASCADE,
-        verbose_name="related registration"
+        verbose_name="related registration",
     )
 
     class Meta:
@@ -189,9 +218,7 @@ class Assessment(TimeStampAbstractBaseClass, UserStampAbstractBaseClass, HelpTex
     def __str__(self) -> str:
         return f"Assessment Milestones for {self.registration.case}"
 
-    def save(
-            self,
-            *args, **kwargs) -> None:
+    def save(self, *args, **kwargs) -> None:
         # called on save of a record
 
         return super().save(*args, **kwargs)
--- epilepsy12/tests/model_tests/test_assessment.py
+++ epilepsy12/tests/model_tests/test_assessment.py
@@ -13,28 +13,50 @@ def registration():
 
 
 # The following tests check if the calculated wait times for each service are correct.
-# They are all using the same function behind the scenes.
+# They all use the same function (epilepsy12.general_functions.time_elapsed.stringify_time_elapsed)
+# behind the scenes, so the tests are designed to cover the range of cases handled by the logic in that function.
 
 
 @pytest.mark.django_db
-def test_consultant_paediatrician_wait_1_week(db, registration):
+def test_consultant_paediatrician_wait_only_one_date(db, registration):
+    """
+    Tests for an error when only one date is supplied
+    """
     # Create an Assessment object with referral and input dates for a consultant paediatrician
     assessment = Assessment.objects.create(
         registration=registration,
-        consultant_paediatrician_referral_date=date(2022, 1, 1),
         consultant_paediatrician_input_date=date(2022, 1, 10),
     )
 
-    # Check if the calculated wait time for the consultant paediatrician is correct
-    assert assessment.consultant_paediatrician_wait() == "1 week"
+    # Only one date was supplied so an error should be raised
+    with pytest.raises(ValueError):
+        assessment.consultant_paediatrician_wait()
 
 
 @pytest.mark.django_db
-def test_consultant_paediatrician_wait_2_weeks(db, registration):
+def test_consultant_paediatrician_wait_days(db, registration):
+    """
+    Tests output when the wait is in days
+    """
     assessment = Assessment.objects.create(
         registration=registration,
         consultant_paediatrician_referral_date=date(2022, 1, 1),
-        consultant_paediatrician_input_date=date(2022, 1, 15),
+        consultant_paediatrician_input_date=date(2022, 1, 3),
+    )
+
+    # Check if the calculated wait time for the consultant paediatrician is correct
+    assert assessment.consultant_paediatrician_wait() == "2 days"
+
+
+@pytest.mark.django_db
+def test_consultant_paediatrician_wait_weeks(db, registration):
+    """
+    Tests output when the wait is in weeks
+    """
+    assessment = Assessment.objects.create(
+        registration=registration,
+        consultant_paediatrician_referral_date=date(2022, 1, 1),
+        consultant_paediatrician_input_date=date(2022, 1, 21),
     )
 
     # Check if the calculated wait time for the consultant paediatrician is correct
@@ -42,43 +64,45 @@ def test_consultant_paediatrician_wait_2_weeks(db, registration):
 
 
 @pytest.mark.django_db
-def test_paediatric_neurologist_wait(registration):
-    # Create an Assessment object with referral and input dates for a paediatric neurologist
+def test_paediatric_neurologist_wait_months(registration):
+    """
+    Tests output when the wait is in months
+    """
     assessment = Assessment.objects.create(
         registration=registration,
         paediatric_neurologist_referral_date=date(2022, 1, 1),
-        paediatric_neurologist_input_date=date(2022, 1, 15),
+        paediatric_neurologist_input_date=date(2022, 5, 15),
     )
 
     # Check if the calculated wait time for the paediatric neurologist is correct
-    assert assessment.paediatric_neurologist_wait() == "2 weeks"
+    assert assessment.paediatric_neurologist_wait() == "4 months"
 
 
 @pytest.mark.django_db
-def test_childrens_epilepsy_surgery_wait(registration):
+def test_childrens_epilepsy_surgery_wait_years(registration):
     """
-    Create an Assessment object with referral and input dates for a children's epilepsy surgery service
+    Tests output when the wait is in years
     """
     assessment = Assessment.objects.create(
         registration=registration,
         childrens_epilepsy_surgical_service_referral_date=date(2022, 1, 1),
-        childrens_epilepsy_surgical_service_input_date=date(2023, 3, 22),
+        childrens_epilepsy_surgical_service_input_date=date(2024, 3, 22),
     )
 
     # Check if the calculated wait time for the children's epilepsy surgery service is correct
-    assert assessment.childrens_epilepsy_surgery_wait() == "3 weeks"
+    assert assessment.childrens_epilepsy_surgery_wait() == "2 years, 2 months"
 
 
 @pytest.mark.django_db
-def test_epilepsy_nurse_specialist_wait(registration):
+def test_epilepsy_nurse_specialist_wait_same_day(registration):
     """
-    Create an Assessment object with referral and input dates for an epilepsy nurse specialist
+    Tests output when the wait in 0 days (same day)
     """
     assessment = Assessment.objects.create(
         registration=registration,
         epilepsy_specialist_nurse_referral_date=date(2022, 1, 1),
-        epilepsy_specialist_nurse_input_date=date(2022, 1, 29),
+        epilepsy_specialist_nurse_input_date=date(2022, 1, 1),
     )
 
     # Check if the calculated wait time for the epilepsy nurse specialist is correct
-    assert assessment.epilepsy_nurse_specialist_wait() == "4 weeks"
+    assert assessment.epilepsy_nurse_specialist_wait() == "Same day"
