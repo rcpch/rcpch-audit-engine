681355daf66ff38cbb044836e1497491ddef19b4
Marcus Baw <marcusbaw@gmail.com>
2023-05-11 18:11:17 +0100

    adds further tests to the Case class

--- epilepsy12/tests/model_tests/test_case.py
+++ epilepsy12/tests/model_tests/test_case.py
@@ -11,28 +11,50 @@ from datetime import date
 # RCPCH imports
 from epilepsy12.models import Case, Organisation
 
+
 @pytest.fixture
 @pytest.mark.django_db
-def organisation():
+def case(db):
     """
-    Creates a minimal Organisation object instance for the test
+    Creates a Case object to test
     """
-    return Organisation.objects.create(name="Test Hospital")
+    return Case.objects.create()
+
+
+@pytest.fixture
+@pytest.mark.django_db
+def organisation(db):
+    # Gets an Organisation object to assign to Case
+    return Organisation.objects.get(ODSCode="RP401")
 
 
 @pytest.mark.django_db
-def test_case_creation_and_age(db):
-    """
-    Tests that a case can be created and that automatic functions work as expected
-    """
-    case = Case.objects.create()
+def test_case_age_calculation(db, case, organisation):
+    # Test that the age function works as expected
     fixed_testing_date = date(2023, 6, 17)
     case.date_of_birth = date(2018, 5, 11)
 
     assert case.age(fixed_testing_date) == "5 years, 1 month"
 
-    """
-    Tests that an organisation can be assigned to a case
-    """
-    case.assign_organisation("Test Hospital")
-    assert case.organisations.all()[0].name == "Test Hospital"
+
+@pytest.mark.django_db
+def test_case_organisation_assignment(db, case, organisation):
+    # Tests that an organisation can be assigned to a case
+    case.organisation = organisation
+    assert case.organisation.ODSCode == "RP401"
+
+
+@pytest.mark.django_db
+def test_case_save_unknown_postcode(db, case, organisation):
+    # Tests that the save method works as expected using one of the 'unknown' postcodes
+    case.postcode = "ZZ99 3CZ"
+    case.save()
+    assert case.index_of_multiple_deprivation_quintile is None
+
+
+@pytest.mark.django_db
+def test_case_save_postcode_obtain_imdq(db, case, organisation):
+    # Tests that the save method works as expected using a known postcode IMD
+    case.postcode = "WC1X 8SH"  # RCPCH address
+    case.save()
+    assert case.index_of_multiple_deprivation_quintile == 4
