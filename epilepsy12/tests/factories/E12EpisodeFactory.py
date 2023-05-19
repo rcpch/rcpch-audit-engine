313529f0fdcf8f7d31d2f8fff8f6b34b0a33691d
Anchit <anchit97123@gmail.com>
2023-05-19 13:28:50 +0100

    adds minimum default EpisodeFactory
    Signed-off-by: Anchit <anchit97123@gmail.com>

---
+++ epilepsy12/tests/factories/E12EpisodeFactory.py
@@ -0,0 +1,130 @@
+"""Factory fn to create new E12 Multiaxial diagnoses, related to a created Case.
+"""
+# standard imports
+from datetime import timedelta
+
+# third-party imports
+import factory
+
+# rcpch imports
+from epilepsy12.models import (
+    Episode,
+)
+from epilepsy12.constants import (
+    DATE_ACCURACY,
+    EPISODE_DEFINITION,
+    EPILEPSY_SEIZURE_TYPE,
+    GENERALISED_SEIZURE_TYPE,
+)
+
+class E12EpisodeFactory(factory.django.DjangoModelFactory):
+    """Dependency factory for creating a minimum viable E12MaDFactory.
+    
+    This E12EpisodeFactory is generated AFTER a E12MaDFactory generated.
+    
+    Default:
+        - Seizure onset 7 days before registration date
+        - Approximate date confidence
+        - Single Episode
+        - Epileptic
+        - Description TODO
+        - seizure type = Focal Onset
+            - Left-sided, atonic with impaired awareness and temporal EEG findings
+    
+    Flags:
+        - `epileptic_seizure_onset_type_generalised`: if True, resets Focal Onset fields, sets Generalised onset with Tonic Clonic
+        - `epileptic_seizure_onset_type_unknown`: if True, resets Focal Onset fields, sets to unknown seizure type
+        - `epileptic_seizure_onset_type_Unclassified`: if True, resets Focal Onset fields, sets to unclassified seizure type
+    """
+    class Meta:
+        model = Episode
+    
+    # Once MultiaxialDiagnosis instance made, it will attach to this instance
+    multiaxial_diagnosis = None
+    
+    # Once an Episode is made, set seizure_onset_date as 7 days before registration date
+    @factory.lazy_attribute
+    def seizure_onset_date(self): 
+        return self.multiaxial_diagnosis.registration.registration_date - timedelta(days=7)
+    
+    seizure_onset_date_confidence=DATE_ACCURACY[0][0] # Apx DATE
+    
+    episode_definition=EPISODE_DEFINITION[0][0] # SINGLE EPISODE
+    
+    # TODO: add the other options as Traits
+    epilepsy_or_nonepilepsy_status = "E" # FIRST EPISODE MUST BE EPILEPTIC, subsequent can be random
+    
+    # TODO: make this True for a minimum viable MDA.Episode and fill in dependencies
+    has_description_of_the_episode_or_episodes_been_gathered = False 
+    
+    # TODO: add the other sub-options which depend on this field, using factory.Trait below
+    epileptic_seizure_onset_type = EPILEPSY_SEIZURE_TYPE[0][0] # 'FO' Focal onset
+    focal_onset_impaired_awareness = True
+    focal_onset_automatisms = None
+    focal_onset_atonic = True
+    focal_onset_clonic = None
+    focal_onset_left = True
+    focal_onset_right = None
+    focal_onset_epileptic_spasms = None
+    focal_onset_hyperkinetic = None
+    focal_onset_myoclonic = None
+    focal_onset_tonic = None
+    focal_onset_autonomic = None
+    focal_onset_behavioural_arrest = None
+    focal_onset_cognitive = None
+    focal_onset_emotional = None
+    focal_onset_sensory = None
+    focal_onset_centrotemporal = None
+    focal_onset_temporal = True
+    focal_onset_frontal = None
+    focal_onset_parietal = None
+    focal_onset_occipital = None
+    focal_onset_gelastic = None
+    focal_onset_focal_to_bilateral_tonic_clonic = None
+    
+    
+    class Params:
+        # Helper flag to reset all fields, used by other traits to start with clean fields
+        reset = factory.Trait(
+                focal_onset_impaired_awareness = None,
+                focal_onset_automatisms = None,
+                focal_onset_atonic = None,
+                focal_onset_clonic = None,
+                focal_onset_left = None,
+                focal_onset_right = None,
+                focal_onset_epileptic_spasms = None,
+                focal_onset_hyperkinetic = None,
+                focal_onset_myoclonic = None,
+                focal_onset_tonic = None,
+                focal_onset_autonomic = None,
+                focal_onset_behavioural_arrest = None,
+                focal_onset_cognitive = None,
+                focal_onset_emotional = None,
+                focal_onset_sensory = None,
+                focal_onset_centrotemporal = None,
+                focal_onset_temporal = None,
+                focal_onset_frontal = None,
+                focal_onset_parietal = None,
+                focal_onset_occipital = None,
+                focal_onset_gelastic = None,
+                focal_onset_focal_to_bilateral_tonic_clonic = None,
+        )
+        
+        # Set appropriate fields if Generalised onset type
+        epileptic_seizure_onset_type_generalised = factory.Trait(
+            reset=True,
+            epileptic_seizure_onset_type = EPILEPSY_SEIZURE_TYPE[1][0], # 'GO' Generalised onset
+            epileptic_generalised_onset = GENERALISED_SEIZURE_TYPE[-3][0], # TCl Tonic-clonic
+        )
+        
+        # Set appropriate fields if unknown onset type
+        epileptic_seizure_onset_type_unknown = factory.Trait(
+            reset=True,
+            epileptic_seizure_onset_type = EPILEPSY_SEIZURE_TYPE[3][0] # 'UO' "Unknown onset onset
+        )
+        
+        # Set appropriate fields if Generalised onset type
+        epileptic_seizure_onset_type_Unclassified = factory.Trait(
+            reset=True,
+            epileptic_seizure_onset_type = EPILEPSY_SEIZURE_TYPE[4][0] # 'UO' Unclassified onset onset
+        )
