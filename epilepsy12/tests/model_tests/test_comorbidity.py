4e938e938b7f70034547078fef704221c70d9b5d
Anchit <anchit97123@gmail.com>
2023-05-19 13:45:31 +0100

    refactors test_comorbidity with new multiax factory fixture
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/tests/model_tests/test_comorbidity.py
+++ epilepsy12/tests/model_tests/test_comorbidity.py
@@ -18,21 +18,9 @@ def comorbidity_entity(db):
 
 
 @pytest.fixture
-def multiaxial_diagnosis(db, e12Registration_2022):
-    return MultiaxialDiagnosis.objects.create(
-        syndrome_present=False,
-        epilepsy_cause_known=False,
-        relevant_impairments_behavioural_educational=False,
-        mental_health_screen=False,
-        mental_health_issue_identified=False,
-        registration=e12Registration_2022,
-    )
-
-
-@pytest.fixture
-def comorbidity(db, multiaxial_diagnosis, comorbidity_entity):
+def comorbidity(db, e12MultiaxialDiagnosis_2022, comorbidity_entity):
     return Comorbidity.objects.create(
-        multiaxial_diagnosis=multiaxial_diagnosis,
+        multiaxial_diagnosis=e12MultiaxialDiagnosis_2022,
         comorbidityentity=comorbidity_entity,
     )
 
