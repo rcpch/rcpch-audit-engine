313169d37cd51c6d8f032785f97b94659d98a897
Anchit <anchit97123@gmail.com>
2023-05-22 15:01:25 +0100

    adds 100 valid nhs nums as a constant file
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/tests/model_tests/test_registration.py
+++ epilepsy12/tests/model_tests/test_registration.py
@@ -20,33 +20,22 @@ import pytest
 # RCPCH imports
 from epilepsy12.models import (
     Registration,
-    Case,
-    AuditProgress,
-    KPI,
-    Site,
 )
 
 
 @pytest.mark.django_db
-def test_create_registration(e12Registration):
-    print(e12Registration)
-    print(e12Registration.registration_date)
-    print(e12Registration.kpi)
-
-
-@pytest.mark.django_db
 def test_registration_custom_method_audit_submission_date_calculation(
-    registration,
+    e12Registration_2022,
 ):
     """
-    Tests the `audit_submission_date_calculation` accurately calculates audit submission date.
+    Tests the `audit_submission_date_calculation` accurately calculates audit submission date, depending on different registration dates.
 
     This is always the second Tuesday of January, following 1 year after the first paediatric assessment.
 
     If registration date + 1 year IS the 2nd Tues of Jan, the submission date is the same as registration + 1 year.
     """
 
-    dates = [
+    registration_dates = [
         # (registration date, expected audit submission date)
         (date(2022, 11, 1), date(2024, 1, 9)),
         (date(2022, 12, 31), date(2024, 1, 9)),
@@ -55,18 +44,18 @@ def test_registration_custom_method_audit_submission_date_calculation(
         (date(2022, 1, 11), date(2024, 1, 9)),
     ]
 
-    for expected_input_output in dates:
-        registration.registration_date = expected_input_output[0]
-        registration.save()
+    for expected_input_output in registration_dates:
+        e12Registration_2022.registration_date = expected_input_output[0]
+        e12Registration_2022.save()
 
         assert (
-            registration.audit_submission_date == expected_input_output[1]
+            e12Registration_2022.audit_submission_date == expected_input_output[1]
         )
 
 
 @pytest.mark.django_db
 def test_registration_custom_method_registration_date_one_year_on(
-    registration,
+    e12Registration_2022,
 ):
     """
     Tests the `registration_date_one_year_on` accurately calculates one year on (registration close date).
@@ -84,18 +73,18 @@ def test_registration_custom_method_registration_date_one_year_on(
     ]
 
     for expected_input_output in expected_inputs_outputs:
-        registration.registration_date = expected_input_output[0]
-        registration.save()
+        e12Registration_2022.registration_date = expected_input_output[0]
+        e12Registration_2022.save()
 
         assert (
-            registration.registration_close_date
+            e12Registration_2022.registration_close_date
             == expected_input_output[1]
         )
 
 
 @pytest.mark.django_db
 def test_registration_cohort(
-    registration,
+    e12Registration_2022,
 ):
     """
     Tests cohort number is set accurately, dependent on registration_date.
@@ -121,17 +110,18 @@ def test_registration_cohort(
     ]
 
     for expected_input_output in expected_inputs_outputs:
-        registration.registration_date = expected_input_output[0]
-        registration.save()
+        e12Registration_2022.registration_date = expected_input_output[0]
+        e12Registration_2022.save()
 
-        assert registration.cohort == expected_input_output[1]
+        assert e12Registration_2022.cohort == expected_input_output[1]
 
 
 @patch.object(Registration, "get_current_date", return_value=date(2022, 11, 30))
 @pytest.mark.django_db
 def test_registration_days_remaining_before_submission(
     mocked_get_current_date,
-    registration,
+    e12Registration_2022,
+    e12Registration_2023,
 ):
     """
     Tests `days_remaining_before_submission` property calculated properly.
@@ -142,37 +132,15 @@ def test_registration_days_remaining_before_submission(
 
     NOTE: if `audit_submission_date` is before today, returns 0.
     """
-    
-    registrations = [
-        
-        registration(registration_date = date(2021, 1, 1)),
-        
-        ]
-    
-    
-    
-    # registration.registration_date=date(2022, 11, 30)
 
-    # expected_inputs_outputs = [
-    #     # (submission date, expected number of days left)
-    #     (date(2022, 11, 30), 0),
-    #     (date(2023, 11, 30), 365),
-    #     (date(2024, 11, 30), 731),
-    #     (date(2025, 11, 30), 1096),
-    # ]
+    expected_inputs_outputs = [
+        # (submission date, expected number of days left)
+        (e12Registration_2022.audit_submission_date, 0),
+        (e12Registration_2022.audit_submission_date, 365),
+    ]
 
-    # for expected_input_output in expected_inputs_outputs:
-    #     registration.audit_submission_date = expected_input_output[0]
-    #     registration.save()
-        
-    #     print(f"{registration.registration_date = }")
-    #     print(f"{registration.audit_submission_date = }")
-    #     diff = registration.audit_submission_date - date(2022, 11, 30)
-    #     print(f"{diff =}")
-
-    #     assert (
-    #         registration.days_remaining_before_submission
-    #         == expected_input_output[1]
-    #     )
+    for expected_input_output in expected_inputs_outputs:
+        reg = expected_input_output[0]
+        print(reg.days_remaining_before_submission())
 
         
