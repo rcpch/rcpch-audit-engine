c42581a218a299e74c098d75478070ce8a4f0d10
Marcus Baw <marcusbaw@gmail.com>
2023-05-22 15:01:30 +0100

    marks this test a XFAIL because we need to rethink how we test this model

--- epilepsy12/tests/model_tests/test_multiaxial_diagnosis.py
+++ epilepsy12/tests/model_tests/test_multiaxial_diagnosis.py
@@ -11,7 +11,11 @@ from epilepsy12.models import (
     MultiaxialDiagnosis,
 )
 
+
+@pytest.mark.xfail
 @pytest.mark.django_db
-def test_working_e12MultiaxialDiagnosis_e12Case_relation(e12MultiaxialDiagnosis, e12Case):
-    
+def test_working_e12MultiaxialDiagnosis_e12Case_relation(
+    e12MultiaxialDiagnosis, e12Case
+):
+    # this assertion fails because the Case factory creates two differently-numbered Case objects
     assert e12MultiaxialDiagnosis.registration.case == e12Case
