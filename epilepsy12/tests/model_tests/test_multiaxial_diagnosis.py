4e1e9882e7d50f645d1f3faeb639f1e7b04f2632
Anchit <anchit97123@gmail.com>
2023-05-22 15:02:46 +0100

    adds the Multidiagnosis_2022 fixture
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/tests/model_tests/test_multiaxial_diagnosis.py
+++ epilepsy12/tests/model_tests/test_multiaxial_diagnosis.py
@@ -19,15 +19,17 @@ from epilepsy12.models import (
 
 @pytest.mark.django_db
 def test_working_e12MultiaxialDiagnosis_e12Case_relation(
-    e12Registration_2022
+    e12MultiaxialDiagnosis_2022
 ):
-    multiaxial_diagnosis = MultiaxialDiagnosis.objects.get(registration=e12Registration_2022)
     
+    multiaxial_diagnosis = e12MultiaxialDiagnosis_2022
     print(multiaxial_diagnosis.registration)
     
     episode = Episode.objects.get(multiaxial_diagnosis=multiaxial_diagnosis)
 
-    print(f"{episode.seizure_onset_date}")
+    for field, value in episode.__dict__.items():
+        print(f"{field}: {value}")
+    
     
     
 
