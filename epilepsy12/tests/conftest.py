eda36dec0c7c8401ef639239682073040521df7a
Marcus Baw <marcusbaw@gmail.com>
2023-05-22 15:01:27 +0100

    added import comments and reduces space used by docstrings by switching to single-line comments. Moved fixtures into alphabetical order

--- epilepsy12/tests/conftest.py
+++ epilepsy12/tests/conftest.py
@@ -1,8 +1,15 @@
+"""conftest.py
+Configures pytest fixtures for epilepsy12 app tests.
+"""
+
+# standard imports
 from datetime import date
 
+# third-party imports
 import pytest
 from pytest_factoryboy import register
 
+# rcpch imports
 from epilepsy12.tests.factories import (
     groups_cases_seeder,
     E12CaseFactory,
@@ -35,56 +42,8 @@ register(E12RegistrationFactory)  # => e12_registration_factory
 register(E12UserFactory)  # => e12_user_factory
 register(E12RegistrationFactory)  # => e12_registration_factory
 
-"""
-AVAILABLE USERS FOR TESTS
-------------------------------------------
-"""
 
-
-@pytest.mark.django_db
-@pytest.fixture()
-def e12User_GOSH():
-    """
-    Creates a single authenticated E12 User object instance for tests.
-    """
-
-    return E12UserFactory.create()
-
-
-@pytest.mark.django_db
-@pytest.fixture()
-def e12User_GOSH_superuser():
-    """
-    Creates a single authenticated SUPERUSER E12 User object instance for tests.
-    """
-
-    return E12UserFactory.create(is_superuser=True)
-
-
-"""
-------------------------------------------
-"""
-
-
-"""
-AVAILABLE CASES FOR TESTS
-------------------------------------------
-"""
-
-
-@pytest.fixture
-def e12Case():
-    # create new e12 case (with associated site) and save to db
-    return E12CaseFactory.create()
-
-
-"""
-------------------------------------------
-"""
-"""
-AVAILABLE AUDITPROGRESS FOR TESTS
-------------------------------------------
-"""
+# AVAILABLE AUDITPROGRESS FOR TESTS
 
 
 @pytest.mark.django_db
@@ -92,9 +51,7 @@ AVAILABLE AUDITPROGRESS FOR TESTS
 def e12AuditProgress():
     """
     Creates a single E12 AuditProgress object instance for tests.
-
     """
-
     return AuditProgress.objects.create(
         registration_complete=False,
         first_paediatric_assessment_complete=False,
@@ -120,37 +77,16 @@ def e12AuditProgress():
     )
 
 
-"""
-------------------------------------------
-"""
-"""
-AVAILABLE SITE FOR TESTS
-------------------------------------------
-"""
+# AVAILABLE CASES FOR TESTS
 
 
-@pytest.mark.django_db
-@pytest.fixture()
-def e12Site(e12Case):
-    """
-    Creates a single E12 Site object instance for tests.
-
-    """
-
-    return Site.objects.filter(
-        case=e12Case,
-        site_is_primary_centre_of_epilepsy_care=True,
-        site_is_actively_involved_in_epilepsy_care=True,
-    ).get()
+@pytest.fixture
+def e12Case():
+    # create new e12 case (with associated site) and save to db
+    return E12CaseFactory.create()
 
 
-"""
-------------------------------------------
-"""
-"""
-AVAILABLE KPI FOR TESTS
-------------------------------------------
-"""
+# AVAILABLE KPI FOR TESTS
 
 
 @pytest.mark.django_db
@@ -158,7 +94,6 @@ AVAILABLE KPI FOR TESTS
 def e12KPI(e12Site):
     """
     Creates a single E12 KPI object instance for tests.
-
     """
 
     return KPI.objects.create(
@@ -188,14 +123,7 @@ def e12KPI(e12Site):
     )
 
 
-"""
-------------------------------------------
-"""
-
-"""
-AVAILABLE MULTIAXIALDIAGNOSIS FOR TESTS
-------------------------------------------
-"""
+# AVAILABLE MULTIAXIALDIAGNOSIS FOR TESTS
 
 
 @pytest.mark.django_db
@@ -203,16 +131,13 @@ AVAILABLE MULTIAXIALDIAGNOSIS FOR TESTS
 def e12MultiaxialDiagnosis(e12Registration):
     """
     Creates a single E12 Multiaxial Diagnosis object instance for tests.
-
     `epilepsy_cause` = Hereditary oculoleptomeningeal amyloid angiopathy
     `epilepsy_cause_categories` = Genetic + Structural
     `mental_health_issue` = Anxiety
     """
-
     epilepsy_cause = EpilepsyCauseEntity.objects.filter(conceptId="43532007").first()
     epilepsy_cause_categories = [EPILEPSY_CAUSES[0][0], EPILEPSY_CAUSES[4][0]]
     mental_health_issue = NEUROPSYCHIATRIC[0][0]
-
     return MultiaxialDiagnosis.objects.create(
         syndrome_present=True,
         epilepsy_cause_known=True,
@@ -226,44 +151,9 @@ def e12MultiaxialDiagnosis(e12Registration):
     )
 
 
-"""
-------------------------------------------
-"""
-"""
-AVAILABLE SYNDROME FOR TESTS
-------------------------------------------
-"""
+# AVAILABLE REGISTRATIONS FOR TESTS
 
 
-@pytest.mark.django_db
-@pytest.fixture()
-def e12Syndrome(e12MultiaxialDiagnosis):
-    """
-    Creates a single E12 Syndrome object instance for tests.
-
-    Syndrome = Sturge Weber
-    `syndrome_diagnosis_date` = 27 Mar 1919 (date of 'William Allen Sturge's death)
-
-    """
-    syndrome_diagnosis_date = date(1919, 3, 19)
-    syndrome_name = SYNDROMES[30][1]
-    syndrome = SyndromeEntity.objects.filter(syndrome_name=syndrome_name)
-
-    return Syndrome.objects.create(
-        syndrome_diagnosis_date=syndrome_diagnosis_date,
-        syndrome=syndrome,
-        multiaxial_diagnosis=e12MultiaxialDiagnosis,
-    )
-
-
-"""
-------------------------------------------
-"""
-
-"""
-AVAILABLE REGISTRATIONS FOR TESTS
-------------------------------------------
-"""
 @pytest.mark.django_db
 @pytest.fixture()
 def e12Registration_2023():
@@ -271,8 +161,8 @@ def e12Registration_2023():
     Creates a single E12 Registration object instance for tests.
     Uses default registration_date of 2023-1-1.
     """
-
     return E12RegistrationFactory.create()
+
 
 @pytest.mark.django_db
 @pytest.fixture()
@@ -281,9 +171,62 @@ def e12Registration_2022():
     Creates a single E12 Registration object instance for tests.
     Overrides default registration date to 2022-1-1
     """
+    return E12RegistrationFactory.create(registration_date=date(2022, 1, 1))
 
-    return E12RegistrationFactory.create(registration_date=date(2022,1,1))
 
-"""
-------------------------------------------
-"""
+# AVAILABLE SITES FOR TESTS
+
+
+@pytest.mark.django_db
+@pytest.fixture()
+def e12Site(e12Case):
+    """
+    Creates a single E12 Site object instance for tests.
+    """
+    return Site.objects.filter(
+        case=e12Case,
+        site_is_primary_centre_of_epilepsy_care=True,
+        site_is_actively_involved_in_epilepsy_care=True,
+    ).get()
+
+
+# AVAILABLE SYNDROMES FOR TESTS
+
+
+@pytest.mark.django_db
+@pytest.fixture()
+def e12Syndrome(e12MultiaxialDiagnosis):
+    """
+    Creates a single E12 Syndrome object instance for tests.
+    Syndrome = Sturge Weber
+    `syndrome_diagnosis_date` = 27 Mar 1919 (date of 'William Allen Sturge's death)
+    """
+    syndrome_diagnosis_date = date(1919, 3, 19)
+    syndrome_name = SYNDROMES[30][1]
+    syndrome = SyndromeEntity.objects.filter(syndrome_name=syndrome_name)
+    return Syndrome.objects.create(
+        syndrome_diagnosis_date=syndrome_diagnosis_date,
+        syndrome=syndrome,
+        multiaxial_diagnosis=e12MultiaxialDiagnosis,
+    )
+
+
+# AVAILABLE USERS FOR TESTS
+
+
+@pytest.mark.django_db
+@pytest.fixture()
+def e12User_GOSH():
+    """
+    Creates a single authenticated E12 User object instance for tests.
+    """
+    return E12UserFactory.create()
+
+
+@pytest.mark.django_db
+@pytest.fixture()
+def e12User_GOSH_superuser():
+    """
+    Creates a single authenticated SUPERUSER E12 User object instance for tests.
+    """
+    return E12UserFactory.create(is_superuser=True)
