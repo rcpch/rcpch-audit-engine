2b4a697820bfd792d9001b6e70facfdcc19d941a
Anchit <anchit97123@gmail.com>
2023-05-22 15:01:21 +0100

    refactors test_case to use E12Case fixture
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/tests/model_tests/test_case.py
+++ epilepsy12/tests/model_tests/test_case.py
@@ -11,67 +11,43 @@ from datetime import date
 # RCPCH imports
 from epilepsy12.models import Case, Organisation, Site
 
-@pytest.mark.django_db
-def test_create_e12case(e12Case):
-    
-    # create new e12 case (with associated site) and save to db
-    case = e12Case.create()
-    
-    assert case.organisations.count() > 0
-
-
-@pytest.fixture
-@pytest.mark.django_db
-def case(db):
-    """
-    Creates a Case object to test
-    """
-    return Case.objects.create()
-
-
-@pytest.fixture
-@pytest.mark.django_db
-def organisation(db):
-    # Gets an Organisation object to assign to Case
-    return Organisation.objects.get(ODSCode="RP401")
-
 
 @pytest.mark.django_db
-def test_case_age_calculation(db, case, organisation):
+def test_case_age_calculation(e12Case):
     # Test that the age function works as expected
     fixed_testing_date = date(2023, 6, 17)
-    case.date_of_birth = date(2018, 5, 11)
+    e12Case.date_of_birth = date(2018, 5, 11)
 
-    assert case.age(fixed_testing_date) == "5 years, 1 month"
-
-
-@pytest.mark.django_db
-def test_case_organisation_assignment(db, case, organisation):
-    # Tests that an organisation can be assigned to a case
-    case.organisation = organisation
-    assert case.organisation.ODSCode == "RP401"
+    assert e12Case.age(fixed_testing_date) == "5 years, 1 month"
 
 
 @pytest.mark.django_db
-def test_case_save_unknown_postcode(db, case, organisation):
+def test_case_organisation_assignment(e12Case):
+    """Test that case has organisation(s) assigned
+    """
+    assert e12Case.organisations.count() > 0
+
+
+@pytest.mark.django_db
+def test_case_save_unknown_postcode(e12Case):
     # Tests that the save method works as expected using one of the 'unknown' postcodes
-    case.postcode = "ZZ99 3CZ"
-    case.save()
-    assert case.index_of_multiple_deprivation_quintile is None
+    e12Case.postcode = "ZZ99 3CZ"
+    e12Case.save()
+    assert e12Case.index_of_multiple_deprivation_quintile is None
 
 
 @pytest.mark.django_db
-def test_case_save_postcode_obtain_imdq(db, case, organisation):
+def test_case_save_postcode_obtain_imdq(e12Case):
     # Tests that the save method works as expected using a known postcode IMD
-    case.postcode = "WC1X 8SH"  # RCPCH address
-    case.save()
-    assert case.index_of_multiple_deprivation_quintile == 4
+    e12Case.postcode = "WC1X 8SH"  # RCPCH address
+    e12Case.save()
+    assert e12Case.index_of_multiple_deprivation_quintile == 4
 
 
 @pytest.mark.django_db
-def test_case_save_invalid_postcode(db, case, organisation):
+def test_case_save_invalid_postcode(e12Case):
     # Tests that the save method works as expected using an invalid postcode
-    case.postcode = "GARBAGE"
-    case.save()
-    assert case.postcode == "GARBAGE"
-    assert case.index_of_multiple_deprivation_quintile is None
+    e12Case.postcode = "GARBAGE"
+    e12Case.save()
+    assert e12Case.postcode == "GARBAGE"
+    assert e12Case.index_of_multiple_deprivation_quintile is None
