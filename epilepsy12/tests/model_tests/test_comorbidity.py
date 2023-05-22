42ecc2bd86902c2fa6adce66d4efb218c0046697
Marcus Baw <marcusbaw@gmail.com>
2023-05-22 15:01:30 +0100

    fixes comorbidty test stub to work

--- epilepsy12/tests/model_tests/test_comorbidity.py
+++ epilepsy12/tests/model_tests/test_comorbidity.py
@@ -8,29 +8,34 @@ import pytest
 # Third party imports
 
 # RCPCH imports
-from epilepsy12.models import Comorbidity, MultiaxialDiagnosis
+from epilepsy12.models import Comorbidity, ComorbidityEntity, MultiaxialDiagnosis
 
 
 @pytest.fixture
-@pytest.mark.django_db
-def multiaxial_diagnosis(db):
+def comorbidity_entity(db):
+    # picks the first comorbidity entity from the database
+    return ComorbidityEntity.objects.get(pk=1)
+
+
+@pytest.fixture
+def multiaxial_diagnosis(db, e12Registration_2022):
     return MultiaxialDiagnosis.objects.create(
         syndrome_present=False,
         epilepsy_cause_known=False,
         relevant_impairments_behavioural_educational=False,
         mental_health_screen=False,
         mental_health_issue_identified=False,
+        registration=e12Registration_2022,
     )
 
 
 @pytest.fixture
-@pytest.mark.django_db
-def comorbidity(db):
+def comorbidity(db, multiaxial_diagnosis, comorbidity_entity):
     return Comorbidity.objects.create(
         multiaxial_diagnosis=multiaxial_diagnosis,
+        comorbidityentity=comorbidity_entity,
     )
 
 
-@pytest.mark.django_db
-def test_comorbidity_diagnosis_date(comorbidity):
+def test_comorbidity_diagnosis_date(db, comorbidity):
     assert comorbidity.comorbidity_diagnosis_date == None
