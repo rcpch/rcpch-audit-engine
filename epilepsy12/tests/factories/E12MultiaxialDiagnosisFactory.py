6367d8b4a93b67645ecdd80bf277f6a97419c4ff
Anchit <anchit97123@gmail.com>
2023-05-22 14:59:29 +0100

    full multiaxial diagnosis factory+fixture with completed answers attached to registration
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/tests/factories/E12MultiaxialDiagnosisFactory.py
+++ epilepsy12/tests/factories/E12MultiaxialDiagnosisFactory.py
@@ -8,8 +8,10 @@ import factory
 # rcpch imports
 from epilepsy12.models import (
     MultiaxialDiagnosis,
+    SyndromeEntity,
 )
 from .E12EpisodeFactory import E12EpisodeFactory
+from epilepsy12.constants import SYNDROMES
 
 class E12MultiaxialDiagnosisFactory(factory.django.DjangoModelFactory):
     
@@ -18,6 +20,12 @@ class E12MultiaxialDiagnosisFactory(factory.django.DjangoModelFactory):
     
     # Once Registration instance made, it will attach to this instance
     registration = None
+    
+    syndrome_present = False
+    epilepsy_cause_known = False
+    relevant_impairments_behavioural_educational = False
+    mental_health_screen = False
+    mental_health_issue_identified = False
     
     # Reverse dependencies, see docstrings for available flags to change default values
     episode = factory.RelatedFactory(
