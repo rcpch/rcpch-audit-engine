62ce9c9e9d16377ced7d48661e26cca8ae5ab42f
Anchit <anchit97123@gmail.com>
2023-05-23 14:45:44 +0100

    refactors aed test
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/tests/model_tests/test_antiepilepsy_medicine.py
+++ epilepsy12/tests/model_tests/test_antiepilepsy_medicine.py
@@ -14,46 +14,34 @@ from epilepsy12.models import (
 
 @pytest.mark.django_db
 def test_antiepilepsy_pregnancy_checks_set_only_when_female_child_bearing_age(
-    e12_case_factory,
     e12_registration_factory,
 ):
-    registration_boy = e12_registration_factory(
+    management_boy = e12_registration_factory(
         case__first_name="boy",
         case__sex=1,
         case__date_of_birth=date(2004, 1, 1),
-    )
-    management_boy = registration_boy.management
+    ).management
 
-    registration_girl_childbearing = e12_registration_factory(
+    management_girl_childbearing = e12_registration_factory(
         case__first_name="girl",
         case__sex=2,
         case__date_of_birth=date(2004, 1, 1),
-    )
-    management__girl_childbearing = registration_girl_childbearing.management
+    ).management
 
-    registration_girl_NOT_childbearing = e12_registration_factory(
+    management_girl_NOT_childbearing = e12_registration_factory(
         case__first_name="girl",
         case__sex=2,
         case__date_of_birth=date(2022, 1, 1),
-    )
-    management_girl_NOT_childbearing = registration_girl_NOT_childbearing.management
+    ).management
 
     assert not AntiEpilepsyMedicine.objects.filter(management=management_boy)[
         0
     ].is_a_pregnancy_prevention_programme_in_place
-    assert AntiEpilepsyMedicine.objects.filter(
-        management=management__girl_childbearing
-    )[0].is_a_pregnancy_prevention_programme_in_place
+
+    assert AntiEpilepsyMedicine.objects.filter(management=management_girl_childbearing)[
+        0
+    ].is_a_pregnancy_prevention_programme_in_place
+
     assert not AntiEpilepsyMedicine.objects.filter(
         management=management_girl_NOT_childbearing
     )[0].is_a_pregnancy_prevention_programme_in_place
-
-
-@pytest.mark.django_db
-def test_debugging(
-    e12_case_factory,
-    e12_registration_factory,
-    e12_management_factory,
-):
-    reg = e12_registration_factory(case__first_name="tester")
-    print(reg.case.first_name)
