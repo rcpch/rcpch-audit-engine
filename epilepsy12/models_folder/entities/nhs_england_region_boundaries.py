baa0b803b1cbfbc33bc5a4053856ce102aa8f0dc
Simon Chapman <eatyourpeasapps@gmail.com>
2023-05-23 22:40:24 +0100

    lower res nhsregions

--- epilepsy12/models_folder/entities/nhs_england_region_boundaries.py
+++ epilepsy12/models_folder/entities/nhs_england_region_boundaries.py
@@ -1,35 +1,26 @@
-# django
+# This is an auto-generated Django model module created by ogrinspect.
 from django.contrib.gis.db import models
-
-# 3rd party
-
-# rcpch
 
 
 class NHSEnglandRegionBoundaries(models.Model):
-    nhser20cd = models.CharField(max_length=9)
-    nhser20nm = models.CharField(max_length=24)
+    nhser22cd = models.CharField(max_length=9)
+    nhser22nm = models.CharField(max_length=24)
     bng_e = models.BigIntegerField()
     bng_n = models.BigIntegerField()
     long = models.FloatField()
     lat = models.FloatField()
     globalid = models.CharField(max_length=38)
-    geom = models.MultiPolygonField(srid=4326)
-
-    def __str__(self) -> str:
-        return self.nhser20nm
+    geom = models.MultiPolygonField(srid=17700)
 
 
-"""
 # Auto-generated `LayerMapping` dictionary for NHSEnglandRegionBoundaries model
-nhsenglandregionboundaries_mapping = {
-    'nhser20cd': 'nhser20cd',
-    'nhser20nm': 'nhser20nm',
-    'bng_e': 'bng_e',
-    'bng_n': 'bng_n',
-    'long': 'long',
-    'lat': 'lat',
-    'globalid': 'GlobalID',
-    'geom': 'MULTIPOLYGON',
-}
-"""
+# nhsenglandregionboundaries_mapping = {
+#     "nhser22cd": "NHSER22CD",
+#     "nhser22nm": "NHSER22NM",
+#     "bng_e": "BNG_E",
+#     "bng_n": "BNG_N",
+#     "long": "LONG",
+#     "lat": "LAT",
+#     "globalid": "GlobalID",
+#     "geom": "MULTIPOLYGON",
+# }
