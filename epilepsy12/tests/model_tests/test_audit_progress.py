76e8bfbb9b5fa143c8f2eefe2e701fd732bab2b1
Anchit <anchit97123@gmail.com>
2023-05-24 13:08:52 +0100

    makes start on audit progress test
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/tests/model_tests/test_audit_progress.py
+++ epilepsy12/tests/model_tests/test_audit_progress.py
@@ -13,27 +13,36 @@ from epilepsy12.common_view_functions.recalculate_form_generate_response import 
     update_audit_progress,
 )
 
-
 @pytest.mark.django_db
-def test_audit_progress_creation(e12Registration_2022):
+def test_audit_progress_creation(
+    e12_case_factory,
+):
+    """Creates an audit progress with audit values filled using default values.
     """
-    Tests that an empty AuditProgress object can be created and has correct initial values
-    """
-    # Create an AuditProgress object
-    audit_progress = AuditProgress.objects.create()
+    case = e12_case_factory().registration.audit_progress
+    
+    print(case.registration.audit_progress)
 
-    # Check that the AuditProgress has the correct initial values
-    assert audit_progress.registration_complete is False
-    assert audit_progress.registration_total_expected_fields == 0
-    assert audit_progress.registration_total_completed_fields == 0
-    assert audit_progress.audit_complete is False
+# @pytest.mark.django_db
+# def test_audit_progress_creation(e12Registration_2022):
+#     """
+#     Tests that an empty AuditProgress object can be created and has correct initial values
+#     """
+#     # Create an AuditProgress object
+#     audit_progress = AuditProgress.objects.create()
 
-    # Add the Registration object to the AuditProgress object
-    # Update the AuditProgress object
-    update_audit_progress(e12Registration_2022)
+#     # Check that the AuditProgress has the correct initial values
+#     assert audit_progress.registration_complete is False
+#     assert audit_progress.registration_total_expected_fields == 0
+#     assert audit_progress.registration_total_completed_fields == 0
+#     assert audit_progress.audit_complete is False
 
-    assert e12Registration_2022.audit_progress.registration_complete is False
-    assert e12Registration_2022.audit_progress.registration_total_expected_fields == 3
+#     # Add the Registration object to the AuditProgress object
+#     # Update the AuditProgress object
+#     update_audit_progress(e12Registration_2022)
+
+#     assert e12Registration_2022.audit_progress.registration_complete is False
+#     assert e12Registration_2022.audit_progress.registration_total_expected_fields == 3
 
 
 # first_paediatric_assessment_complete
