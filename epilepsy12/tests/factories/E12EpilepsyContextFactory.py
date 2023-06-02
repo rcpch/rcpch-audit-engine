9fc448f7817883dba07e5761fc67d46a2bcdda05
Anchit <anchit97123@gmail.com>
2023-06-02 16:46:20 +0100

    refactors EpilContext and Investigations factory to remove default values
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/tests/factories/E12EpilepsyContextFactory.py
+++ epilepsy12/tests/factories/E12EpilepsyContextFactory.py
@@ -1,6 +1,4 @@
 """Factory fn to create new E12 EpilepsyContext
-
-NOTE: calling this factory will cause dependencies to be created automatically e.g. Registration, Case related to an Organisation.
 """
 # standard imports
 
@@ -12,7 +10,6 @@ import factory
 from epilepsy12.models import (
     EpilepsyContext
 )
-from epilepsy12.constants import OPT_OUT_UNCERTAIN
 
 class E12EpilepsyContextFactory(factory.django.DjangoModelFactory):
 
@@ -22,11 +19,3 @@ class E12EpilepsyContextFactory(factory.django.DjangoModelFactory):
     # when a registration instance created, it will attach to this instance
     registration = None
     
-    previous_febrile_seizure=OPT_OUT_UNCERTAIN[0][0]
-    previous_acute_symptomatic_seizure=OPT_OUT_UNCERTAIN[0][0]
-    is_there_a_family_history_of_epilepsy=OPT_OUT_UNCERTAIN[0][0]
-    previous_neonatal_seizures=OPT_OUT_UNCERTAIN[0][0]
-    diagnosis_of_epilepsy_withdrawn=True
-    were_any_of_the_epileptic_seizures_convulsive=True
-    experienced_prolonged_generalized_convulsive_seizures=OPT_OUT_UNCERTAIN[0][0]
-    experienced_prolonged_focal_seizures=OPT_OUT_UNCERTAIN[0][0]
--- epilepsy12/tests/factories/E12InvestigationsFactory.py
+++ epilepsy12/tests/factories/E12InvestigationsFactory.py
@@ -1,7 +1,6 @@
 """Factory fn to create new E12 Investigations, related to a created Registration.
 """
 # standard imports
-from dateutil.relativedelta import relativedelta
 
 # third-party imports
 import factory
@@ -16,15 +15,7 @@ class E12InvestigationsFactory(factory.django.DjangoModelFactory):
     
     This Factory is generated AFTER a Registration is created.
     
-    Default:
-        - EEG requested on registration plus 1 month
-        - EEG performed on request date + 28 days
-        - MRI requested on registration plus 1 month
-        - MRI reported on request date + 28 days
-        - ECG + CTHead performed
-    Flags:
-        - `eeg_not_requested` : if True, sets request to False + dates to None
-        - `mri_not_requested` : if True, sets request to False + dates to None
+    
     """
     class Meta:
         model = Investigations
@@ -32,37 +23,3 @@ class E12InvestigationsFactory(factory.django.DjangoModelFactory):
     # Once Registration instance made, it will attach to this instance
     registration = None
     
-    eeg_indicated = True
-    # eeg requested is 365 days before registration
-    @factory.lazy_attribute
-    def eeg_request_date(self): 
-        return self.registration.registration_date + relativedelta(months=1)
-    # eeg performed is 28 days after requested date
-    @factory.lazy_attribute
-    def eeg_performed_date(self): 
-        return self.eeg_request_date + relativedelta(days=28)
-    
-    mri_indicated = True
-    # mri requested is 365 days before registration
-    @factory.lazy_attribute
-    def mri_brain_requested_date(self): 
-        return self.registration.registration_date + relativedelta(months=1)
-    # mri reported is 28 days after requested date
-    @factory.lazy_attribute
-    def mri_brain_reported_date(self): 
-        return self.mri_brain_requested_date + relativedelta(days=28)
-    
-    twelve_lead_ecg_status = True
-    ct_head_scan_status = True
-    
-    class Params:
-        eeg_not_requested = factory.Trait(
-            eeg_indicated = False,
-            eeg_request_date = None,
-            eeg_performed_date = None,
-        )
-        mri_not_requested = factory.Trait(
-            mri_indicated = False,
-            mri_brain_requested_date = None,
-            mri_brain_reported_date = None,
-        )
