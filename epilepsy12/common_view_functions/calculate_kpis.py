e529896fc45d25cfde965f37e3c8ea5cf57da869
Anchit <anchit97123@gmail.com>
2023-06-05 12:57:24 +0100

    refactors score kpi 8
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/common_view_functions/calculate_kpis.py
+++ epilepsy12/common_view_functions/calculate_kpis.py
@@ -402,39 +402,31 @@ def score_kpi_8(registration_instance, age_at_first_paediatric_assessment) -> in
 
     Denominator = Number of females aged 12 and above diagnosed with epilepsy at first year AND on valproate
     """
-    # denominator
-    if (
-        age_at_first_paediatric_assessment >= 12 and registration_instance.case.sex == 2
-    ) and (
-        registration_instance.management.has_an_aed_been_given
-        and AntiEpilepsyMedicine.objects.filter(
+    # ineligible - < 12yo or male
+    if age_at_first_paediatric_assessment < 12 or registration_instance.case.sex != 2:
+        return KPI_SCORE["INELIGIBLE"]
+    
+    # not scored
+    if registration_instance.management.has_an_aed_been_given is None:
+        return KPI_SCORE["NOT_SCORED"]
+    
+    # ineligible
+    if not AntiEpilepsyMedicine.objects.filter(
             management=registration_instance.management,
-            medicine_entity=MedicineEntity.objects.filter(
-                medicine_name__icontains="valproate"
-            ).first(),
-        ).exists()
-    ):
-        # eligible for this measure
-        sodium_valproate = 0
-        if (
-            age_at_first_paediatric_assessment >= 12
-            and registration_instance.case.sex == 2
-        ) and (
-            registration_instance.management.has_an_aed_been_given
-            and AntiEpilepsyMedicine.objects.filter(
-                management=registration_instance.management,
-                medicine_entity=MedicineEntity.objects.filter(
-                    medicine_name__icontains="valproate"
-                ).first(),
-                is_a_pregnancy_prevention_programme_needed=True,
-                has_a_valproate_annual_risk_acknowledgement_form_been_completed=True,
-            ).exists()
-        ):
-            # criteria met
-            sodium_valproate = 1
+            medicine_entity=MedicineEntity.objects.get(medicine_name="Sodium valproate")
+        ).exists():
+        return KPI_SCORE["INELIGIBLE"]
+    
+    # get valproate assigned
+    valproate = AntiEpilepsyMedicine.objects.get(
+            management=registration_instance.management,
+            medicine_entity=MedicineEntity.objects.get(medicine_name="Sodium valproate"))
+    
+    if valproate.is_a_pregnancy_prevention_programme_needed and valproate.has_a_valproate_annual_risk_acknowledgement_form_been_completed:
+        return KPI_SCORE["PASS"]
     else:
-        # not eligible for this measure
-        sodium_valproate = 2
+        return KPI_SCORE["FAIL"]
+    
 
 
 def calculate_kpis(registration_instance):
