153fa880f069445948983f16289e5ac6ac636a95
Anchit <anchit97123@gmail.com>
2023-06-06 16:39:47 +0100

    adds paed_expert_epilepsy pass/fail flag to case factory constructor
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/tests/common_view_functions_tests/test_aggregate_by.py
+++ epilepsy12/tests/common_view_functions_tests/test_aggregate_by.py
@@ -15,17 +15,17 @@ from epilepsy12.common_view_functions import (
     all_registered_cases_for_cohort_and_abstraction_level,
     calculate_kpis,
 )
-from epilepsy12.models import Organisation, Case
+from epilepsy12.models import Organisation, Case, KPI
 from epilepsy12.constants import SEX_TYPE, DEPRIVATION_QUINTILES, ETHNICITIES
 
 
 @pytest.mark.django_db
 def test_cases_aggregated_by_sex_correct_output(e12_case_factory):
     """Tests the cases_aggregated_by_sex fn returns correct count."""
-    
+
     # define constants
     ORGANISATION = Organisation.objects.first()
-    
+
     # Create 10 cases of each available sex type
     for sex_type in SEX_TYPE:
         # set an organisation constant
@@ -35,8 +35,8 @@ def test_cases_aggregated_by_sex_correct_output(e12_case_factory):
         for _ in range(10):
             e12_case_factory.create(
                 sex=sex_type[0],
-                registration=None, # ensure related audit factories not generated
-                organisations__organisation = ORGANISATION,
+                registration=None,  # ensure related audit factories not generated
+                organisations__organisation=ORGANISATION,
             )
 
     total_count = cases_aggregated_by_sex(selected_organisation=organisation).count()
@@ -141,11 +141,11 @@ def test_aggregate_all_eligible_kpi_fields_correct_count(e12_case_factory):
 
     aggregated_kpis = aggregate_all_eligible_kpi_fields(organisation_level)
 
-    total_count_kpis = -1 # start at -1 to exclude "total_number_of_cases"
+    total_count_kpis = -1  # start at -1 to exclude "total_number_of_cases"
     for key in aggregated_kpis:
         if "total" in key:
-            total_count_kpis+=1
-    
+            total_count_kpis += 1
+
     assert total_count_kpis == 21
 
 
@@ -157,5 +157,10 @@ def test_aggregate_all_eligible_kpi_fields_correct_kpi_scoring(e12_case_factory)
     ORGANISATION = Organisation.objects.first()
 
     for _ in range(10):
-        case = e12_case_factory.create(organisations__organisation=ORGANISATION)
-        print(f"{case} -> {calculate_kpis(case.registration)}")
+        case = e12_case_factory.create(
+            organisations__organisation=ORGANISATION,
+            registration__assessment__fail_paediatrician_with_expertise_in_epilepsies=True,
+        )
+        calculate_kpis(case.registration)
+        kpi = KPI.objects.get(pk=case.registration.pk)
+        print(kpi.paediatrician_with_expertise_in_epilepsies)
