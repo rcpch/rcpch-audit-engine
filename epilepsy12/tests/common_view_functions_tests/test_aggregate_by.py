1c5702e368ca787b18e2ba9a0c70140687927952
Anchit <anchit97123@gmail.com>
2023-06-06 14:55:37 +0100

    adds test aggregate by ehtnicity
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/tests/common_view_functions_tests/test_aggregate_by.py
+++ epilepsy12/tests/common_view_functions_tests/test_aggregate_by.py
@@ -10,9 +10,10 @@ import pytest
 from epilepsy12.common_view_functions.aggregate_by import (
     cases_aggregated_by_sex,
     cases_aggregated_by_deprivation_score,
+    cases_aggregated_by_ethnicity,
 )
 from epilepsy12.models import Organisation
-from epilepsy12.constants.common import SEX_TYPE, DEPRIVATION_QUINTILES
+from epilepsy12.constants import SEX_TYPE, DEPRIVATION_QUINTILES, ETHNICITIES
 
 
 @pytest.mark.django_db
@@ -25,10 +26,12 @@ def test_cases_aggregated_by_sex_correct_output(e12_case_factory, e12_site_facto
 
         # For each sex, assign 10 cases
         for _ in range(10):
-            case = e12_case_factory.create(sex=sex_type[0], registration=None)
-
-            # clean default organisations created in factory
-            case.organisations.clear()
+            case = e12_case_factory.create(
+                sex=sex_type[0],
+                # clean default organisations / relate registration+audit forms created in factory
+                registration=None,
+                organisations=None,
+            )
 
             # relate to organisation constant
             case.organisations.add(
@@ -61,11 +64,10 @@ def test_cases_aggregated_by_deprivation_score(e12_case_factory, e12_site_factor
         for _ in range(10):
             case = e12_case_factory.create(
                 index_of_multiple_deprivation_quintile=deprivation_type[1],
+                # clean default organisations / relate registration+audit forms created in factory
                 registration=None,
+                organisations=None,
             )
-
-            # clean default organisations created in factory
-            case.organisations.clear()
 
             # relate to organisation constant
             case.organisations.add(
@@ -80,6 +82,45 @@ def test_cases_aggregated_by_deprivation_score(e12_case_factory, e12_site_factor
     matching_count = (
         cases_aggregated_by_deprivation_score(selected_organisation=organisation)
         .filter(cases_aggregated_by_deprivation=10)
+        .count()
+    )
+
+    assert (
+        total_count == matching_count
+    ), f"Not returning correct count. {total_count=} should equal {matching_count=}"
+
+
+@pytest.mark.django_db
+def test_cases_aggregated_by_ethnicity(e12_case_factory, e12_site_factory):
+    """Tests the cases_aggregated_by_ethnicity fn returns correct count."""
+
+    # Loop through each ethnicity
+    for ethnicity_type in ETHNICITIES:
+        # set an organisation constant
+        organisation = Organisation.objects.first()
+
+        # For each ethnicity, assign 10 cases
+        for _ in range(10):
+            case = e12_case_factory.create(
+                ethnicity=ethnicity_type[0],
+                # clean default organisations / relate registration+audit forms created in factory
+                registration=None,
+                organisations=None,
+            )
+
+            # relate to organisation constant
+            case.organisations.add(
+                e12_site_factory(case=case, organisation=organisation).organisation
+            )
+            case.save()
+
+    total_count = cases_aggregated_by_ethnicity(
+        selected_organisation=organisation
+    ).count()
+
+    matching_count = (
+        cases_aggregated_by_ethnicity(selected_organisation=organisation)
+        .filter(ethnicities=10)
         .count()
     )
 
