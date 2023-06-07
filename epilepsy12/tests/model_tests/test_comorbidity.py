7351f2062ae63f020e87b7de42fdaf6a1bf6c3c4
Anchit <anchit97123@gmail.com>
2023-06-07 16:12:49 +0100

    rms comorbidity unit test as nothing to test anymore
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/tests/model_tests/test_comorbidity.py
+++ epilepsy12/tests/model_tests/test_comorbidity.py
@@ -11,12 +11,4 @@ import pytest
 from epilepsy12.models import Comorbidity
 
 
-@pytest.mark.django_db
-def test_working_comorbidity(e12_case_factory):
-    """Tests simply ensures a valid comorbidity is attached, with default values, to a case.
-    """
-    case = e12_case_factory()
-      
-    assert Comorbidity.objects.filter(multiaxial_diagnosis = case.registration.multiaxialdiagnosis)
-
     
