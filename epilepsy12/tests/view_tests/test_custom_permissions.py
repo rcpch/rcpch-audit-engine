2d6619b5f44e5184eadf0a456a581b62b456ce24
Simon Chapman <eatyourpeasapps@gmail.com>
2023-06-14 19:36:23 +0100

    custom permissions

---
+++ epilepsy12/tests/view_tests/test_custom_permissions.py
@@ -0,0 +1,29 @@
+"""
+
+## Custom Permissions Tests
+TODO #511
+
+Opt out
+[] Assert an Audit Centre Administrator CANNOT let a child opt out of Epilepsy12
+[] Assert an audit centre clinician CANNOT let a child opt out of Epilepsy12
+[] Assert an Audit Centre Lead Clinician can let a child within their own Trust opt out of Epilepsy12
+[] Assert an Audit Centre Lead Clinician cannot let a child outside their own Trust opt out of Epilepsy12
+[] Assert an RCPCH Audit Lead can let a child opt out of Epilepsy12
+
+Locking
+[] Assert an Audit Centre Administrator CANNOT lock a child from being edited
+[] Assert an audit centre clinician CANNOT unlock a child for editing
+[] Assert an Audit Centre Clinician CAN lock a child from being edited
+[] Assert an Audit Centre Lead Clinician CAN lock a child from being edited
+[] Assert an Audit Centre Lead Clinician CANNOT unlock a child for editing
+[] Assert an RCPCH Audit Lead can lock a child from being edited
+[] Assert an RCPCH Audit Lead can unlock a child from being edited
+
+Locking
+[] Assert an Audit Centre Administrator CANNOT consent for a child to be included in Epilepsy12
+[] Assert an audit centre clinician CANNOT consent for a child to be included in Epilepsy12
+[] Assert an Audit Centre Lead Clinician CANNOT consent for a child to be included in Epilepsy12
+[] Assert an RCPCH Audit Lead CANNOT consent for a child to be included in Epilepsy12
+[] Assert a Child, Young Person or family can member consent for a child to be included in Epilepsy12
+
+"""
