c05a303813e45642020d828cf6448531896fe1a7
Simon Chapman <eatyourpeasapps@gmail.com>
2023-06-14 21:19:12 +0100

    change first paediatric assessment

--- epilepsy12/tests/view_tests/test_custom_permissions.py
+++ epilepsy12/tests/view_tests/test_custom_permissions.py
@@ -26,4 +26,12 @@ Locking
 [] Assert an RCPCH Audit Lead CANNOT consent for a child to be included in Epilepsy12
 [] Assert a Child, Young Person or family can member consent for a child to be included in Epilepsy12
 
+can_register_child_in_epilepsy12
+[] Assert an Audit Centre Administrator CANNOT register a child in Epilepsy12
+[] Assert an audit centre clinician CAN register a child within their own Trust in Epilepsy12
+[] Assert an audit centre clinician CANNOT register a child outside their own Trust in Epilepsy12
+[] Assert an Audit Centre Lead Clinician CAN register a child within their own Trust in Epilepsy12
+[] Assert an Audit Centre Lead Clinician CANNOT register a child outside their own Trust in Epilepsy12
+[] Assert an RCPCH Audit Lead CAN register a child within any Trust in Epilepsy12
+
 """
--- epilepsy12/tests/view_tests/test_permissions_transfer.py
+++ epilepsy12/tests/view_tests/test_permissions_transfer.py
@@ -1,8 +1,9 @@
 """
 ## Transfer Tests
 
+['allocate_lead_site', 'edit_lead_site', 'transfer_lead_site']
 [] Assert an Audit Centre Administrator CANNOT transfer patients
 [] Assert an audit centre clinician CANNOT transfer patients
-[] Assert an Audit Centre Lead Clinician can only transfer patients within own Trust
-[] Assert an RCPCH Audit Lead can transfer patients nationally, within any organisations
+[] Assert an Audit Centre Lead Clinician can transfer patients
+[] Assert an RCPCH Audit Lead can transfer patients
 """
