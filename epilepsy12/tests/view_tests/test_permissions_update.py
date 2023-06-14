3ceb2c55ac18d47555c977bd345aab47571e3be3
Simon Chapman <eatyourpeasapps@gmail.com>
2023-06-14 21:26:17 +0100

    epilepsy context test

--- epilepsy12/tests/view_tests/test_permissions_update.py
+++ epilepsy12/tests/view_tests/test_permissions_update.py
@@ -35,6 +35,23 @@ for field in fields: [
 
 # First Paediatric Assessment
 for field in fields: [
+    'first_paediatric_assessment_in_acute_or_nonacute_setting',
+    'has_number_of_episodes_since_the_first_been_documented',
+    'general_examination_performed',
+    'neurological_examination_performed',
+    'developmental_learning_or_schooling_problems',
+    'behavioural_or_emotional_problems',
+]
+[ ] Assert an Audit Centre Administrator can change 'field' inside own Trust - response.status_code == 200
+[ ] Assert an Audit Centre Administrator cannot change 'field' inside a different Trust - response.status_code == 403
+[ ] Assert an Audit Centre Clinician can change 'rfield' inside own Trust - response.status_code == 200
+[ ] Assert an Audit Centre Clinician cannot vchange 'field' inside a different Trust - response.status_code == 403
+[ ] Assert an Audit Centre Lead Clinician can change 'field' inside own Trust - response.status_code == 200
+[ ] Assert an Audit Centre Lead Clinician cannot change 'field' inside a different Trust - response.status_code == 403
+[ ] Assert an RCPCH Audit Lead can change 'field' - response.status_code == 200
+
+# Epilepsy Context
+for field in fields: [
     'previous_febrile_seizure',
     'previous_acute_symptomatic_seizure',
     'is_there_a_family_history_of_epilepsy',
@@ -51,4 +68,6 @@ for field in fields: [
 [ ] Assert an Audit Centre Lead Clinician can change 'field' inside own Trust - response.status_code == 200
 [ ] Assert an Audit Centre Lead Clinician cannot change 'field' inside a different Trust - response.status_code == 403
 [ ] Assert an RCPCH Audit Lead can change 'field' - response.status_code == 200
+
+
 """
