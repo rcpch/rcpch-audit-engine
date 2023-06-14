14d35e21d37055a3140dca92d267a72a58ad0cca
Anchit <anchit97123@gmail.com>
2023-06-14 11:08:23 +0100

    undo changes
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/tests/view_tests/test_permissions.py
+++ epilepsy12/tests/view_tests/test_permissions.py
@@ -35,8 +35,8 @@ def test_audit_centre_clinician(client, e12_user_factory, seed_groups_fixture):
         is_staff=False,
         is_rcpch_audit_team_member=False,
         role=AUDIT_CENTRE_CLINICIAN,
-        organisation_employer = TEST_USER_ORGANISATION,
-        groups=[AUDIT_CENTRE_CLINICIAN_GROUP]
+        organisation_employer=TEST_USER_ORGANISATION,
+        groups=[AUDIT_CENTRE_CLINICIAN_GROUP],
     )
 
     # Create another 2 users in the same Organisation (GOSH is default)
@@ -45,10 +45,10 @@ def test_audit_centre_clinician(client, e12_user_factory, seed_groups_fixture):
         is_staff=True,
         is_rcpch_audit_team_member=False,
         role=AUDIT_CENTRE_CLINICIAN,
-        organisation_employer = TEST_USER_ORGANISATION,
-        groups=[AUDIT_CENTRE_CLINICIAN_GROUP]
+        organisation_employer=TEST_USER_ORGANISATION,
+        groups=[AUDIT_CENTRE_CLINICIAN_GROUP],
     )
-    
+
     # Create another 2 users in a DIFFERENT Organisation
     batch = e12_user_factory.create_batch(
         2,
@@ -58,27 +58,38 @@ def test_audit_centre_clinician(client, e12_user_factory, seed_groups_fixture):
         organisation_employer=DIFFERENT_USER_ORGANISATION,
         groups=[AUDIT_CENTRE_CLINICIAN_GROUP],
     )
-    
-    
+
     # Log in Test User
     client.force_login(test_user)
-    
+
     # assert that user CAN view the 2 users in same organisation but NOT the 2 users in a different organisation
-    
-    response_same_org = client.get(f'/organisation/{TEST_USER_ORGANISATION.id}/epilepsy12_user_list/')
-    response_diff_org = client.get(f'/organisation/{DIFFERENT_USER_ORGANISATION.id}/epilepsy12_user_list/')
-    
-    assert response_same_org.status_code == 200, f"User from {TEST_USER_ORGANISATION} requested to see Users list from their own organisation, but not receiving 200 response"
-    
-    assert response_diff_org.status_code == 403, f"User from {TEST_USER_ORGANISATION} requested to see Users list from different organisation ({DIFFERENT_USER_ORGANISATION}). Expecting 403, receiving {response_diff_org.status_code}"
+
+    response_same_org = client.get(
+        f"/organisation/{TEST_USER_ORGANISATION.id}/epilepsy12_user_list/"
+    )
+    response_diff_org = client.get(
+        f"/organisation/{DIFFERENT_USER_ORGANISATION.id}/epilepsy12_user_list/"
+    )
+
+    assert (
+        response_same_org.status_code == 200
+    ), f"User from {TEST_USER_ORGANISATION} requested to see Users list from their own organisation, but not receiving 200 response"
+
+    assert (
+        response_diff_org.status_code == 403
+    ), f"User from {TEST_USER_ORGANISATION} requested to see Users list from different organisation ({DIFFERENT_USER_ORGANISATION}). Expecting 403, receiving {response_diff_org.status_code}"
 
 
-@pytest.mark.xfail(reason='The view does raise PermissionDenied and returns a 403 template, however the response code is still 200')
+@pytest.mark.xfail(
+    reason="The view does raise PermissionDenied and returns a 403 template, however the response code is still 200"
+)
 @pytest.mark.django_db
-def test_audit_centre_clinician_cases_list_access(client, e12_user_factory, seed_groups_fixture):
+def test_audit_centre_clinician_cases_list_access(
+    client, e12_user_factory, seed_groups_fixture
+):
     """Test to ensure Audit Centre Clinician (NOTE: NOT Lead Clinican) role can and can't view specified pages."""
-    
-     # set up constants
+
+    # set up constants
     GROUP_AUDIT_CENTRE_CLINICIAN = Group.objects.get(name=TRUST_AUDIT_TEAM_EDIT_ACCESS)
     ORGANISATION_TEST_USER = Organisation.objects.get(ODSCode="RP401")
     ORGANISATION_OTHER = Organisation.objects.get(ODSCode="RGT01")
@@ -89,17 +100,29 @@ def test_audit_centre_clinician_cases_list_access(client, e12_user_factory, seed
         is_rcpch_audit_team_member=False,
         is_superuser=False,
         role=AUDIT_CENTRE_CLINICIAN,
-        organisation_employer = ORGANISATION_TEST_USER,
+        organisation_employer=ORGANISATION_TEST_USER,
         groups=[GROUP_AUDIT_CENTRE_CLINICIAN],
     )
-    
+
     # log in user
     client.force_login(test_user)
-    
-    res_same_org = client.get(reverse('cases', kwargs={'organisation_id':ORGANISATION_TEST_USER.id}))
-    
-    assert res_same_org.status_code == 200, f"User from {ORGANISATION_TEST_USER} requesting cases list from {ORGANISATION_TEST_USER}. Expecting 200 status_code, receiving {res_same_org.status_code}"
 
-    res_other_org = client.get(reverse('cases', kwargs={'organisation_id':ORGANISATION_OTHER.id}))
-    assert res_other_org.status_code == 403, f"User from {ORGANISATION_TEST_USER} requesting cases list from {ORGANISATION_OTHER}. Expecting 403 status_code, receiving {res_other_org.status_code}"
+    with pytest.raises(PermissionDenied) as excinfo_same:
+        res_same_org = client.get(
+            reverse("cases", kwargs={"organisation_id": ORGANISATION_TEST_USER.id})
+        )
 
+    with pytest.raises(PermissionDenied) as excinfo_other:
+        res_other_org = client.get(
+            reverse("cases", kwargs={"organisation_id": ORGANISATION_OTHER.id})
+        )
+
+    assert (
+        str(excinfo_same.value) == f"PermissionError: [Errno 13] {res_same_org}"
+    )  # "User from {ORGANISATION_TEST_USER} requesting cases list from {ORGANISATION_TEST_USER}. Expecting 200 status_code, receiving {res_same_org.status_code}"
+    assert (
+        str(excinfo_other.value) == f"PermissionError: [Errno 13] {res_other_org}"
+    )  # User from {ORGANISATION_TEST_USER} requesting cases list from {ORGANISATION_OTHER}. Expecting 403 status_code, receiving {res_other_org.status_code}"
+
+    # res_other_org.status_code == 403, f"User from {ORGANISATION_TEST_USER} requesting cases list from {ORGANISATION_OTHER}. Expecting 403 status_code, receiving {res_other_org.status_code}"
+    # assert res_same_org.status_code == 200, f"User from {ORGANISATION_TEST_USER} requesting cases list from {ORGANISATION_TEST_USER}. Expecting 200 status_code, receiving {res_same_org.status_code}"
