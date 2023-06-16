5cb4bb3cd27de5df6960b9ea7fd1804766f6c3a8
Simon Chapman <eatyourpeasapps@gmail.com>
2023-06-16 20:22:17 +0100

    fix tests for RCPCH Audit Team

--- epilepsy12/tests/UserDataClasses.py
+++ epilepsy12/tests/UserDataClasses.py
@@ -10,18 +10,20 @@ from epilepsy12.constants.user_types import (
     AUDIT_CENTRE_ADMINISTRATOR,
     AUDIT_CENTRE_CLINICIAN,
     AUDIT_CENTRE_LEAD_CLINICIAN,
-    RCPCH_AUDIT_LEAD,
+    RCPCH_AUDIT_TEAM,
     TRUST_AUDIT_TEAM_VIEW_ONLY,
     TRUST_AUDIT_TEAM_EDIT_ACCESS,
     TRUST_AUDIT_TEAM_FULL_ACCESS,
     EPILEPSY12_AUDIT_TEAM_FULL_ACCESS,
 )
 
+
 @dataclass
 class TestUser:
     role: int
     role_str: str
     group_name: str
+
 
 test_user_audit_centre_administrator_data = TestUser(
     role=AUDIT_CENTRE_ADMINISTRATOR,
@@ -41,8 +43,8 @@ test_user_audit_centre_lead_clinician_data = TestUser(
     group_name=TRUST_AUDIT_TEAM_FULL_ACCESS,
 )
 
-test_user_rcpch_audit_lead_data = TestUser(
-    role=RCPCH_AUDIT_LEAD,
-    role_str="RCPCH_AUDIT_LEAD",
+test_user_rcpch_audit_team_data = TestUser(
+    role=RCPCH_AUDIT_TEAM,
+    role_str="RCPCH_AUDIT_TEAM",
     group_name=EPILEPSY12_AUDIT_TEAM_FULL_ACCESS,
 )
