b04442ed8694eef7f5f75dd4a1debc08b1c87b07
Anchit <anchit97123@gmail.com>
2023-06-29 11:26:28 +0100

    docstrings for testing recalculate_form_ fn
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/common_view_functions/recalculate_form_generate_response.py
+++ epilepsy12/common_view_functions/recalculate_form_generate_response.py
@@ -79,7 +79,54 @@ def update_audit_progress(model_instance):
     except DatabaseError as error:
         raise Exception(error)
 
+"""
+# Tests for `completed_fields` fn
 
+These tests occur on a model-basis.
+
+For each MODEL:
+
+    1)
+        - fill in ALL number of attributes
+        - assert completed_fields(MODEL) == COUNT(ALL)
+    2)
+        - fill in random(X) number of attributes
+        - assert completed_fields(MODEL) == X
+
+- `Registration`
+
+- `FirstPaediatricAssessment` 
+
+    completed_fields(MODEL) == 6
+        1. first_paediatric_assessment_in_acute_or_nonacute_setting
+        2. has_number_of_episodes_since_the_first_been_documented
+        3. general_examination_performed
+        4. neurological_examination_performed
+        5. developmental_learning_or_schooling_problems
+        6. behavioural_or_emotional_problems
+
+- `EpilepsyContext`
+
+    completed_fields(MODEL) == 8
+        1. previous_febrile_seizure
+        2. previous_acute_symptomatic_seizure
+        3. is_there_a_family_history_of_epilepsy
+        4. previous_neonatal_seizures
+        5. diagnosis_of_epilepsy_withdrawn
+        6. were_any_of_the_epileptic_seizures_convulsive
+        7. experienced_prolonged_generalized_convulsive_seizures
+        8. experienced_prolonged_focal_seizures
+    
+- `Assessment`
+- `Investigations`
+- `Management`
+    - `AntiepilepsyMedicine`
+- `MultiaxialDiagnosis`
+    - `Episode`
+    - `Syndrome`
+    
+
+"""
 # --8<-- [start:completed_fields]
 def completed_fields(model_instance):
     """
