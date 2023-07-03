82b38943edd98f310f48c28af9d0df9e5319090c
Anchit <anchit97123@gmail.com>
2023-07-03 15:44:07 +0100

    change str repr empty syndrome
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/models_folder/syndrome.py
+++ epilepsy12/models_folder/syndrome.py
@@ -56,7 +56,7 @@ class Syndrome(TimeStampAbstractBaseClass, UserStampAbstractBaseClass, HelpTextM
     )
 
     def __str__(self) -> str:
-        return f'{self.syndrome.syndrome_name} on {self.syndrome_diagnosis_date}' if self.syndrome else "No syndrome"
+        return f'{self.syndrome.syndrome_name} on {self.syndrome_diagnosis_date}' if self.syndrome else f"Empty syndrome"
 
     class Meta:
         verbose_name = 'Syndrome'
