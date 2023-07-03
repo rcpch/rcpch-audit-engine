13646761009559b784f1180998de03cade64608f
Anchit <anchit97123@gmail.com>
2023-07-03 09:59:48 +0100

    rms unused imports multiax factory
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/tests/factories/E12MultiaxialDiagnosisFactory.py
+++ epilepsy12/tests/factories/E12MultiaxialDiagnosisFactory.py
@@ -8,18 +8,10 @@ import factory
 # rcpch imports
 from epilepsy12.models import (
     MultiaxialDiagnosis,
-    EpilepsyCauseEntity,
 )
 from .E12EpisodeFactory import E12EpisodeFactory
 from .E12SyndromeFactory import E12SyndromeFactory
-from .E12ComorbidityFactory import E12ComorbidityFactory
-from epilepsy12.general_functions import (
-    fetch_ecl,
-)
-from epilepsy12.constants import (
-    EPILEPSY_CAUSES,
-    NEUROPSYCHIATRIC,
-)
+
 
 
 class E12MultiaxialDiagnosisFactory(factory.django.DjangoModelFactory):
