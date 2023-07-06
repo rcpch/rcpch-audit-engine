0aee38ecb00cd033dd691a4c3aea068f4d5546dc
anchit-chandran <68898006+anchit-chandran@users.noreply.github.com>
2023-07-06 11:57:08 +0100

    Adds test_total_fields_expected_investigations and registration
    Signed-off-by: anchit-chandran <68898006+anchit-chandran@users.noreply.github.com>

--- epilepsy12/tests/view_tests/form_calculations/test_total_fields_expected.py
+++ epilepsy12/tests/view_tests/form_calculations/test_total_fields_expected.py
@@ -41,7 +41,9 @@ from epilepsy12.constants import (
     Management_minimum_scorable_fields,
     AntiEpilepsyMedicine_minimum_scorable_fields,
 )
-from epilepsy12.tests.view_tests.form_calculations.test_number_of_completed_fields_in_related_models import get_random_answers_update_counter
+from epilepsy12.tests.view_tests.form_calculations.test_number_of_completed_fields_in_related_models import (
+    get_random_answers_update_counter,
+)
 
 
 def test_correct_output_scoreable_fields_for_model_class_name():
@@ -96,7 +98,6 @@ def test_count_episode_fields(e12_case_factory, GOSH):
     ), f"Single completely filled Focal Onset Episode ({episode_queryset=}) inserted into count_episode_fields() fn. Should return 8. Instead, returned {return_value}"
 
 
-
 @pytest.mark.django_db
 def test_total_fields_expected_multiaxial_diagnosis_episode_fields(
     e12_case_factory, GOSH
@@ -117,7 +118,10 @@ def test_total_fields_expected_multiaxial_diagnosis_episode_fields(
     expected_value = 15
     return_value = total_fields_expected(multiaxial_diagnosis)
 
-    assert return_value == expected_value, f"total_fields_expected(multiaxial_diagnosis) with fully completed Focal Onset episode should return {expected_value}, instead returned {return_value}"
+    assert (
+        return_value == expected_value
+    ), f"total_fields_expected(multiaxial_diagnosis) with fully completed Focal Onset episode should return {expected_value}, instead returned {return_value}"
+
 
 @pytest.mark.django_db
 def test_total_fields_expected_multiaxial_diagnosis_syndrome_fields(
@@ -131,36 +135,39 @@ def test_total_fields_expected_multiaxial_diagnosis_syndrome_fields(
     CASE = e12_case_factory(
         first_name=f"temp_child_{GOSH.OrganisationName}",
         organisations__organisation=GOSH,
-        registration__multiaxial_diagnosis__episode = None,
+        registration__multiaxial_diagnosis__episode=None,
         registration__multiaxial_diagnosis__syndrome_present=True,
-        registration__multiaxial_diagnosis__syndrome_entity = None
+        registration__multiaxial_diagnosis__syndrome_entity=None,
     )
 
     multiaxial_diagnosis = CASE.registration.multiaxialdiagnosis
 
     # Initial value because Multiaxial diagnosis fields minimum == 7 ++ no episodes == 5
     expected_value = 12
-    
+
     ADD_SYNDROMES = random.choice([None, True])
     if ADD_SYNDROMES is not None:
         # Create 3 syndromes
         SYNDROMES_LIST = SyndromeEntity.objects.all()[:3]
-        
+
         for i in range(3):
             e12_syndrome_factory(
                 multiaxial_diagnosis=multiaxial_diagnosis,
-                syndrome_diagnosis_date=date(2023,1,1),
-                syndrome=SYNDROMES_LIST[i]
+                syndrome_diagnosis_date=date(2023, 1, 1),
+                syndrome=SYNDROMES_LIST[i],
             )
-            
+
             # Each syndrome has 2 fields to complete
             expected_value += 2
     else:
-        expected_value += 2 # syndrome_present==True, so minimum value adds 2 expected
-        
+        expected_value += 2  # syndrome_present==True, so minimum value adds 2 expected
+
     return_value = total_fields_expected(multiaxial_diagnosis)
-    
-    assert return_value == expected_value, f"total_fields_expected(multiaxial_diagnosis) with {'3 syndromes registered' if ADD_SYNDROMES else 'syndrome_present==True but no syndromes entered'} expects return value of {expected_value} but received {return_value}"
+
+    assert (
+        return_value == expected_value
+    ), f"total_fields_expected(multiaxial_diagnosis) with {'3 syndromes registered' if ADD_SYNDROMES else 'syndrome_present==True but no syndromes entered'} expects return value of {expected_value} but received {return_value}"
+
 
 @pytest.mark.django_db
 def test_total_fields_expected_multiaxial_diagnosis_general_fields(
@@ -174,46 +181,46 @@ def test_total_fields_expected_multiaxial_diagnosis_general_fields(
     CASE = e12_case_factory(
         first_name=f"temp_child_{GOSH.OrganisationName}",
         organisations__organisation=GOSH,
-        registration__multiaxial_diagnosis__epilepsy_cause_known = True, # +2
-        registration__multiaxial_diagnosis__relevant_impairments_behavioural_educational=True, # +2
-        registration__multiaxial_diagnosis__mental_health_issue_identified = True, # +1
-        registration__multiaxial_diagnosis__global_developmental_delay_or_learning_difficulties = True, # +1
+        registration__multiaxial_diagnosis__epilepsy_cause_known=True,  # +2
+        registration__multiaxial_diagnosis__relevant_impairments_behavioural_educational=True,  # +2
+        registration__multiaxial_diagnosis__mental_health_issue_identified=True,  # +1
+        registration__multiaxial_diagnosis__global_developmental_delay_or_learning_difficulties=True,  # +1
     )
 
     multiaxial_diagnosis = CASE.registration.multiaxialdiagnosis
 
     # Initial value because Multiaxial diagnosis fields minimum == 7; ++ no episodes == 5; ++ general_fields all true == 6;
     expected_value = 23
-        
+
     return_value = total_fields_expected(multiaxial_diagnosis)
-    
-    assert return_value == expected_value, f"total_fields_expected(multiaxial_diagnosis) with general fields all True. Expected {expected_value} but got {return_value}"
+
+    assert (
+        return_value == expected_value
+    ), f"total_fields_expected(multiaxial_diagnosis) with general fields all True. Expected {expected_value} but got {return_value}"
 
 
 @pytest.mark.django_db
-def test_total_fields_expected_assessment(
-    e12_case_factory, GOSH
-):
+def test_total_fields_expected_assessment(e12_case_factory, GOSH):
     """
     Tests total_fields_expected(assessment) returns correct expected output, with all general fields all True.
     """
 
-    answer_set={}
-    expected_value=5 # minimum value when no fields complete
+    answer_set = {}
+    expected_value = 5  # minimum value when no fields complete
     fields = [
-        'consultant_paediatrician_referral_made',
-        'paediatric_neurologist_referral_made',
-        'childrens_epilepsy_surgical_service_referral_made',
-        'epilepsy_specialist_nurse_referral_made',
+        "consultant_paediatrician_referral_made",
+        "paediatric_neurologist_referral_made",
+        "childrens_epilepsy_surgical_service_referral_made",
+        "epilepsy_specialist_nurse_referral_made",
     ]
     for field in fields:
-        answer = random.choice([None,True])
-        
-        BASE_KEY_NAME = 'registration__assessment__'
-        answer_set.update({f"{BASE_KEY_NAME}{field}":answer})
-        
+        answer = random.choice([None, True])
+
+        BASE_KEY_NAME = "registration__assessment__"
+        answer_set.update({f"{BASE_KEY_NAME}{field}": answer})
+
         if answer is not None:
-            if field == 'epilepsy_specialist_nurse_referral_made':
+            if field == "epilepsy_specialist_nurse_referral_made":
                 expected_value += 2
             else:
                 expected_value += 3
@@ -223,9 +230,69 @@ def test_total_fields_expected_assessment(
         organisations__organisation=GOSH,
         **answer_set,
     )
-    
+
     assessment = CASE.registration.assessment
-        
+
     return_value = total_fields_expected(assessment)
-    
-    assert return_value == expected_value, f"total_fields_expected(assessment) expected {expected_value} but got {return_value}. Used answers: {answer_set}"
+
+    assert (
+        return_value == expected_value
+    ), f"total_fields_expected(assessment) expected {expected_value} but got {return_value}. Used answers: {answer_set}"
+
+
+@pytest.mark.django_db
+def test_total_fields_expected_investigations(e12_case_factory, GOSH):
+    """
+    Tests total_fields_expected(investigations) returns correct expected output, with all general fields all True.
+    """
+
+    answer_set = {}
+    expected_value = 4  # minimum value when no fields complete
+    fields = [
+        "eeg_indicated",
+        "mri_indicated",
+    ]
+    for field in fields:
+        answer = random.choice([None, True])
+
+        BASE_KEY_NAME = "registration__investigations__"
+        answer_set.update({f"{BASE_KEY_NAME}{field}": answer})
+
+        if answer is not None:
+            expected_value += 2
+
+    CASE = e12_case_factory(
+        first_name=f"temp_child_{GOSH.OrganisationName}",
+        organisations__organisation=GOSH,
+        **answer_set,
+    )
+
+    investigations = CASE.registration.investigations
+
+    return_value = total_fields_expected(investigations)
+
+    assert (
+        return_value == expected_value
+    ), f"total_fields_expected(investigations) expected {expected_value} but got {return_value}. Used answers: {answer_set}"
+
+@pytest.mark.django_db
+def test_total_fields_expected_registration(e12_case_factory, GOSH):
+    """
+    Tests total_fields_expected(registration) returns correct expected output, with all general fields all True.
+    """
+
+    answer_set = {}
+
+    CASE = e12_case_factory(
+        first_name=f"temp_child_{GOSH.OrganisationName}",
+        organisations__organisation=GOSH,
+    )
+
+    registration = CASE.registration
+
+    expected_value = 3  # minimum value when no fields complete
+    return_value = total_fields_expected(registration)
+
+    assert (
+        return_value == expected_value
+    ), f"total_fields_expected(registration) expected {expected_value} but got {return_value}. Used answers: {answer_set}"
