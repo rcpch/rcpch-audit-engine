2781b5d4538cd1000327cca08c47eef203774b29
anchit-chandran <68898006+anchit-chandran@users.noreply.github.com>
2023-07-07 16:11:24 +0100

    adds test_total_fields_expected_multiaxial_diagnosis_syndrome_fields
    Signed-off-by: anchit-chandran <68898006+anchit-chandran@users.noreply.github.com>

--- epilepsy12/tests/view_tests/form_calculations/test_total_fields_expected.py
+++ epilepsy12/tests/view_tests/form_calculations/test_total_fields_expected.py
@@ -21,6 +21,7 @@ import random
 # E12 imports
 from epilepsy12.models import (
     Episode,
+    SyndromeEntity,
 )
 from epilepsy12.common_view_functions.recalculate_form_generate_response import (
     scoreable_fields_for_model_class_name,
@@ -100,7 +101,7 @@ def test_total_fields_expected_multiaxial_diagnosis_episode_fields(
     e12_case_factory, GOSH
 ):
     """
-    Tests total_fields_expected(multiaxia_diagnosis) returns correct expected output, with a completed Focal Onset episode.
+    Tests total_fields_expected(multiaxial_diagnosis) returns correct expected output, with a completed Focal Onset episode.
     """
 
     CASE = e12_case_factory(
@@ -111,8 +112,52 @@ def test_total_fields_expected_multiaxial_diagnosis_episode_fields(
 
     multiaxial_diagnosis = CASE.registration.multiaxialdiagnosis
 
-    # Initial minimal value. Multiaxial diagnosis fields minimum == 7 ++ focal onset episode fields == 8
-    return_value = total_fields_expected(multiaxial_diagnosis)
+    # Multiaxial diagnosis fields minimum == 7 ++ focal onset episode fields == 8
     expected_value = 15
+    return_value = total_fields_expected(multiaxial_diagnosis)
 
     assert return_value == expected_value, f"total_fields_expected(multiaxial_diagnosis) with fully completed Focal Onset episode should return {expected_value}, instead returned {return_value}"
+
+@pytest.mark.django_db
+def test_total_fields_expected_multiaxial_diagnosis_syndrome_fields(
+    e12_case_factory, GOSH, e12_syndrome_factory
+):
+    """
+    Tests total_fields_expected(multiaxial_diagnosis) returns correct expected output, with a completed Syndrome. Each syndrome registered has 2 fields.
+
+    """
+
+    CASE = e12_case_factory(
+        first_name=f"temp_child_{GOSH.OrganisationName}",
+        organisations__organisation=GOSH,
+        registration__multiaxial_diagnosis__episode = None,
+        registration__multiaxial_diagnosis__syndrome_present=True,
+        registration__multiaxial_diagnosis__syndrome_entity = None
+    )
+
+    multiaxial_diagnosis = CASE.registration.multiaxialdiagnosis
+
+    # Initial value because Multiaxial diagnosis fields minimum == 7 ++ no episodes == 5
+    expected_value = 12
+    
+    ADD_SYNDROMES = random.choice([None, True])
+    if ADD_SYNDROMES is not None:
+        # Create 3 syndromes
+        SYNDROMES_LIST = SyndromeEntity.objects.all()[:3]
+        
+        for i in range(3):
+            e12_syndrome_factory(
+                multiaxial_diagnosis=multiaxial_diagnosis,
+                syndrome_diagnosis_date=date(2023,1,1),
+                syndrome=SYNDROMES_LIST[i]
+            )
+            
+            # Each syndrome has 2 fields to complete
+            expected_value += 2
+    else:
+        expected_value += 2 # syndrome_present==True, so minimum value adds 2 expected
+        
+    return_value = total_fields_expected(multiaxial_diagnosis)
+    
+    assert return_value == expected_value, f"total_fields_expected(multiaxial_diagnosis) with {'3 syndromes registered' if ADD_SYNDROMES else 'syndrome_present==True but no syndromes entered'} expects return value of {expected_value} but received {return_value}"
+        
