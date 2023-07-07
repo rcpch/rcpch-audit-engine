7909c039e81870fe79002b70b2fdd1c85e054d0d
Simon Chapman <eatyourpeasapps@gmail.com>
2023-07-07 15:29:08 +0100

    change first paediatric assessment

--- epilepsy12/tests/view_tests/test_permissions_update.py
+++ epilepsy12/tests/view_tests/test_permissions_update.py
@@ -11,10 +11,18 @@
 [] Assert an Audit Centre Lead Clinician can only update patients within own Trust
 [] Assert an RCPCH Audit Lead can update patients nationally, within any organisations
 
-
 [] Assert an Audit Centre Administrator CANNOT update patient records
 [] Assert an audit centre clinician can only update patient records within own organisation
 [] Assert an Audit Centre Lead Clinician can only update patient records within own Trust
 [] Assert an RCPCH Audit Lead can update patient records nationally, within any organisations
 
+# First Paediatric Assessment
+for field in fields: ['first_paediatric_assessment_in_acute_or_nonacute_setting','has_number_of_episodes_since_the_first_been_documented','general_examination_performed','neurological_examination_performed','developmental_learning_or_schooling_problems','behavioural_or_emotional_problems']
+[ ] Assert an Audit Centre Administrator can change 'field' inside own Trust - response.status_code == 200
+[ ] Assert an Audit Centre Administrator cannot change 'field' inside a different Trust - response.status_code == 403
+[ ] Assert an Audit Centre Clinician can change 'rfield' inside own Trust - response.status_code == 200
+[ ] Assert an Audit Centre Clinician cannot vchange 'field' inside a different Trust - response.status_code == 403
+[ ] Assert an Audit Centre Lead Clinician can change 'field' inside own Trust - response.status_code == 200
+[ ] Assert an Audit Centre Lead Clinician cannot change 'field' inside a different Trust - response.status_code == 403
+[ ] Assert an RCPCH Audit Lead can change 'field' - response.status_code == 200
 """
