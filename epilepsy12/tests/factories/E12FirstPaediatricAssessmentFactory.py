4e3581eb45dcad76e7a4e5ae9732efac197d3e1c
Anchit <anchit97123@gmail.com>
2023-07-07 15:30:35 +0100

    adds tests completed_fields(first_paediatric_assessment)
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/tests/factories/E12FirstPaediatricAssessmentFactory.py
+++ epilepsy12/tests/factories/E12FirstPaediatricAssessmentFactory.py
@@ -20,10 +20,3 @@ class E12FirstPaediatricAssessmentFactory(factory.django.DjangoModelFactory):
     
     # when a registration instance created, it will attach to this instance
     registration = None
-    
-    first_paediatric_assessment_in_acute_or_nonacute_setting=True
-    has_number_of_episodes_since_the_first_been_documented=True
-    general_examination_performed=True
-    neurological_examination_performed=True
-    developmental_learning_or_schooling_problems=True
-    behavioural_or_emotional_problems=True
