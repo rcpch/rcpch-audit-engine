4ee303678942c37c6a746783526362c7b23e8d62
Anchit <anchit97123@gmail.com>
2023-07-07 15:29:20 +0100

    xfails unfinished aggregate by test
    Signed-off-by: Anchit <anchit97123@gmail.com>

--- epilepsy12/tests/common_view_functions_tests/test_aggregate_by.py
+++ epilepsy12/tests/common_view_functions_tests/test_aggregate_by.py
@@ -138,7 +138,7 @@ def test_cases_aggregated_by_ethnicity(e12_case_factory):
     ), f"Not returning correct count. {total_count=} should equal {matching_count=}"
 
 
-@pytest.mark.xfail(reason='unfinished test')
+
 @pytest.mark.django_db
 def test_aggregate_all_eligible_kpi_fields_correct_count(e12_case_factory):
     """Tests the aggregate_all_eligible_kpi_fields fn returns correct count of KPIs."""
@@ -166,7 +166,7 @@ def test_aggregate_all_eligible_kpi_fields_correct_count(e12_case_factory):
 
     assert total_count_kpis == 21
 
-
+@pytest.mark.xfail(reason='unfinished test')
 @pytest.mark.django_db
 def test_aggregate_all_eligible_kpi_fields_correct_kpi_scoring(e12_case_factory):
     """Tests the aggregate_all_eligible_kpi_fields fn returns scoring of KPIs."""
