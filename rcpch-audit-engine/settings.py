3a2e1c1b62ac5b64a3af2fd86d7b3b9da8c6b124
anchit-chandran <anchit97123@gmail.com>
2023-07-11 11:39:46 +0100

    debugging
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- rcpch-audit-engine/settings.py
+++ rcpch-audit-engine/settings.py
@@ -134,9 +134,6 @@ DATABASES = {
         "PASSWORD": os.environ.get("E12_POSTGRES_DB_PASSWORD"),
         "HOST": os.environ.get("E12_POSTGRES_DB_HOST"),
         "PORT": os.environ.get("E12_POSTGRES_DB_PORT"),
-        "TEST": {
-            "NAME": os.environ.get("E12_POSTGRES_DB_NAME"),
-        },
     }
 }
 
