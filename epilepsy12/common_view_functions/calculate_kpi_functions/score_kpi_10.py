66aa081889daeba2366234fd01f8fbecb4bd1629
anchit-chandran <anchit97123@gmail.com>
2023-08-03 09:10:19 +0100

    score kpi 10
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- epilepsy12/common_view_functions/calculate_kpi_functions/score_kpi_10.py
+++ epilepsy12/common_view_functions/calculate_kpi_functions/score_kpi_10.py
@@ -7,9 +7,13 @@ from epilepsy12.constants import KPI_SCORE
 
 def score_kpi_10(registration_instance, age_at_first_paediatric_assessment) -> int:
     """10. school_individual_healthcare_plan
+    
     Percentage of children and young people with epilepsy aged 5 years and above with evidence of a school individual healthcare plan by 1 year after first paediatric assessment.
+    
     Calculation Method
+    
     Numerator = Number of children and young people aged 5 years and above diagnosed with epilepsy at first year AND with evidence of EHCP
+    
     Denominator =Number of children and young people aged 5 years and above diagnosed with epilepsy at first year
     """
 
