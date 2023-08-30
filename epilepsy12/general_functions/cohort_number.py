3ce7ae79a655a06b4c13a92b242b6244b9a40c12
anchit-chandran <anchit97123@gmail.com>
2023-08-30 16:06:05 +0100

    updates cohort num calcs
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- epilepsy12/general_functions/cohort_number.py
+++ epilepsy12/general_functions/cohort_number.py
@@ -7,7 +7,7 @@ from datetime import date
 from dateutil.relativedelta import relativedelta
 
 # e12 imports
-from .date_functions import first_tuesday_in_january
+from .date_functions import first_tuesday_in_january, nth_tuesday_of_year
 
 
 def cohort_number_from_enrolment_date(enrolment_date: date) -> int:
@@ -34,7 +34,9 @@ def cohort_number_from_enrolment_date(enrolment_date: date) -> int:
         return None
 
     for cohort_starting_year in range(2020, 2030):
-        if enrolment_date >= date(cohort_starting_year, 12, 1) and enrolment_date <= date(cohort_starting_year+1, 11, 30):
+        if enrolment_date >= date(
+            cohort_starting_year, 12, 1
+        ) and enrolment_date <= date(cohort_starting_year + 1, 11, 30):
             return cohort_starting_year - 2016
 
 
@@ -47,21 +49,34 @@ def cohort_start_date_from_cohort_number(cohort):
     return date(cohort_year, 12, 1)
 
 
-def current_cohort_start_date():
+def current_cohort_start_date(
+    current_date: date = None,
+) -> date:
     """
     Returns the start date of the current cohort
     """
-    today = date.today()
+    today = date.today() if current_date is None else current_date
 
     if date(today.year, 12, 1) < today:
         return date(today.year, 12, 1)
     else:
-        return date(today.year-1, 12, 1)
+        return date(today.year - 1, 12, 1)
 
 
-def get_current_cohort_data() -> dict:
+def days_remaining_before_submission(audit_submission_date: date) -> int:
+    if audit_submission_date:
+        remaining_dateime = audit_submission_date - date.today()
+        return remaining_dateime.days if remaining_dateime.days > 0 else 0
+
+
+def get_current_cohort_data(
+    current_date: date = None,
+) -> dict:
     """
-    Returns all current cohort dates and cohort number
+    Returns all current cohort dates and cohort number.
+
+    Can supply current_date if not using date.today() - most likely use is in testing.
+
     Return keys are:
     'cohort_start_date'
     'cohort_end_date'
@@ -69,15 +84,29 @@ def get_current_cohort_data() -> dict:
     'submission_date'
     'days_remaining'
     """
-    current_cohort = cohort_number_from_enrolment_date(
-        current_cohort_start_date())
+    current_cohort = cohort_number_from_enrolment_date(current_cohort_start_date())
+
+    cohort_start_date = cohort_start_date_from_cohort_number(current_cohort)
+    cohort_end_date = date(current_cohort_start_date().year + 1, 11, 30)
+
+    days_remaining_until_end_of_active_recruitment = (
+        cohort_end_date - cohort_start_date
+    ).days
+
+    # latest submission date is always cohort_end_date + 1 year, and then the first 2nd Tues in jan. So for Cohort 6, cohort end date is 2023-11-30, 1 year + first 2nd Tues in Jan will be 2025 01 14
+    submission_date = nth_tuesday_of_year(cohort_end_date.year + 2, n=2)
+
+    days_remaining_until_submission = days_remaining_before_submission(
+        audit_submission_date=submission_date
+    )
 
     cohort_data = {
-        'cohort_start_date': current_cohort_start_date(),
-        'cohort_end_date': date(current_cohort_start_date().year+1, 11, 30),
-        'cohort': current_cohort,
-        'submission_date': first_tuesday_in_january(current_cohort_start_date().year+2) + relativedelta(days=7),
-        'days_remaining':  relativedelta(first_tuesday_in_january(current_cohort_start_date().year+1) + relativedelta(days=7) - date.today()).days
+        "cohort": current_cohort,
+        "cohort_start_date": cohort_start_date,
+        "cohort_end_date": cohort_end_date,
+        "submission_date": submission_date,
+        "days_remaining": days_remaining_until_end_of_active_recruitment,
+        "days_remaining_until_latest_submission": days_remaining_until_submission,
     }
 
     return cohort_data
