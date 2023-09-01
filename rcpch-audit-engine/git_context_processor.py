83cf2c58f63a24715707b71e8facfab8c6273945
anchit-chandran <anchit97123@gmail.com>
2023-09-01 10:47:04 +0100

    implements combo version of git hash
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- rcpch-audit-engine/git_context_processor.py
+++ rcpch-audit-engine/git_context_processor.py
@@ -1,37 +1,40 @@
-"""
-Context processors
-"""
-
 from pathlib import Path
 
 
 def get_active_branch_and_commit(request):
     """
-    Gets the active Git branch and latest Git commit for the admin UI
+    Gets the `GIT_HASH` environment variable, created in GitHub Actions workflow, inserts into all templates.
     """
+    # Load the txt file containing hash
     try:
-        head_dir = Path(".") / ".git" / "HEAD"
-        with head_dir.open("r") as f:
-            head_branch = f.read().splitlines()
-        for line in head_branch:
-            if line[0:4] == "ref:":
-                active_git_branch = line.partition("refs/heads/")[2]
-            else:
-                raise Exception
-    except Exception as e:
-        print(f"Failed to get GitBranch. Error: {e}")
-        active_git_branch = "[branch name not found]"
+        with open("git_hash.txt", "r") as f:
+            git_branch, git_hash = f.read().split(",")
 
-    try:
-        refs_dir = Path(".") / ".git" / "refs" / "heads" / active_git_branch
-        with refs_dir.open("r") as f:
-            latest_git_commit = f.read().splitlines()[0]
+            latest_git_commit = git_hash.replace("GIT_HASH=", "")
+            active_git_branch = git_branch.replace("GIT_BRANCH=", "")
 
-    except Exception as e:
-        print(f"Failed to get commit hash. Error: {e}")
-        latest_git_commit = "[latest commit hash not found]"
+    except FileNotFoundError:
+        try:
+            head_dir = Path(".") / ".git" / "HEAD"
+            with head_dir.open("r") as f:
+                head_branch = f.read().splitlines()
+            for line in head_branch:
+                if line[0:4] == "ref:":
+                    active_git_branch = line.partition("refs/heads/")[2]
+        except:
+            active_git_branch = "[branch name not found]"
 
-    return {
-        "active_git_branch": active_git_branch,
+        try:
+            refs_dir = Path(".") / ".git" / "refs" / "heads" / active_git_branch
+            with refs_dir.open("r") as f:
+                latest_git_commit = f.read().splitlines()[0]
+
+        except:
+            latest_git_commit = "[latest commit hash not found]"
+
+    admin_data = {
         "latest_git_commit": latest_git_commit,
+        "active_git_branch": active_git_branch,
     }
+
+    return admin_data
