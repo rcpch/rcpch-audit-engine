d58f8216ab2d41b6aa71ef79aa4f55cad3e07850
anchit-chandran <anchit97123@gmail.com>
2023-09-11 11:15:38 +0100

    adds all base 2fa files + templates
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- rcpch-audit-engine/urls.py
+++ rcpch-audit-engine/urls.py
@@ -2,6 +2,7 @@ from rest_framework import routers
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.admindocs import urls
+from two_factor.urls import urlpatterns as tf_urls
 
 
 handler403 = "epilepsy12.views.rcpch_403"
@@ -15,4 +16,5 @@ urlpatterns = [
     path("admin/doc/", include(urls)),
     path("admin/", admin.site.urls),
     path("", include("epilepsy12.urls")),
+    path('', include(tf_urls)),
 ]
