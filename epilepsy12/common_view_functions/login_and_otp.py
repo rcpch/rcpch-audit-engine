5e7738f9e515c22c87ea22e137a9dcc2b0bf3cce
anchit-chandran <anchit97123@gmail.com>
2023-09-11 11:15:42 +0100

    Revert "new decorator to combine @login_required and @otp_required"
    This reverts commit 1f8f904129b800a27f0f520e3f2b1162b49a0852.

--- epilepsy12/common_view_functions/login_and_otp.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.auth.decorators import login_required
-from django_otp.decorators import otp_required
-
-def login_and_otp_required(view_func):
-    # Apply the login_required first and then otp_required decorator
-    decorated_view_func = login_required(otp_required(view_func))
-    return decorated_view_func
