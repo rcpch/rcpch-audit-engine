70d83213ef94f879a8fcf302eba303a137269e16
anchit-chandran <anchit97123@gmail.com>
2023-09-20 10:54:48 +0100

    replace all POSTCODES_IO_API_URL to new findthatpostcode envar
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- epilepsy12/general_functions/postcode.py
+++ epilepsy12/general_functions/postcode.py
@@ -3,7 +3,7 @@ from django.conf import settings
 from ..constants import UNKNOWN_POSTCODES_NO_SPACES
 
 
-def is_valid_postcode(postcode:str)->bool:
+def is_valid_postcode(postcode: str) -> bool:
     """
     Returns True if postcode valid.
     """
@@ -18,33 +18,27 @@ def is_valid_postcode(postcode:str)->bool:
     url = f"{settings.POSTCODE_API_BASE_URL}/postcodes/{postcode}"
 
     response = requests.get(url=url)
-    
+
     if response.status_code == 200:
         return True
-    
+
     # Only other possibility should be 404, but handle any other status code
-    print(f"Postcode validation failure. Could not validate postcode at {url}. {response.status_code=}")
+    print(
+        f"Postcode validation failure. Could not validate postcode at {url}. {response.status_code=}"
+    )
     return False
-        
 
 
-def return_random_postcode():
-    # get random postcode from API
-    url = f"{settings.POSTCODES_IO_API_URL}/random/postcodes"
+def return_random_postcode(country_boundary_identifier: str):
+    """Returns random postcode (str) inside country_boundary_identifier or `None` if invalid."""
+    url = f"{settings.POSTCODE_API_BASE_URL}/areas/{country_boundary_identifier}"
 
     response = requests.get(url=url)
+
     if response.status_code == 404:
         print("Postcode generation failure. Could not get random postcode.")
         return None
-    else:
-        return response.json()["result"]["postcode"]
 
-
-def call_api(postcode):
-    formatted = postcode.upper().replace(" ", "+")
-    api = f"https://findthatpostcode.uk/postcodes/{formatted}.json"
-    response = requests.get(url=api)
-    if response.status_code != 200:
-        return None
-    r = response.json()["data"]["attributes"]["laua"]
-    return r
+    return response.json()["data"]["relationships"]["example_postcodes"]["data"][0][
+        "id"
+    ].replace(" ", "")
