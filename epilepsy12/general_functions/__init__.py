863b272a7fb05a67b47ec27beb84bc3eea828799
anchit-chandran <anchit97123@gmail.com>
2023-09-20 10:19:01 +0100

    rm unused import
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- epilepsy12/general_functions/__init__.py
+++ epilepsy12/general_functions/__init__.py
@@ -4,7 +4,6 @@ Imports all the general functions
 from .cohort_number import *
 from .index_multiple_deprivation import *
 from .postcode import *
-from .random_postcodes import *
 from .fuzzy_matching import *
 from .time_elapsed import stringify_time_elapsed
 from .value_from_key import *
