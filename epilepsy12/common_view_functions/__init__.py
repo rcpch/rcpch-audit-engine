bda286bafd8e24ca4eb13e22b65bf32f23a2f63f
eatyourpeas <simon@eatyourpeas.co.uk>
2023-10-22 17:53:19 +0100

    limit comorbiditylist to colins selection

--- epilepsy12/common_view_functions/__init__.py
+++ epilepsy12/common_view_functions/__init__.py
@@ -31,3 +31,5 @@ from .report_queries import (
 from .sanction_user_access import return_selected_organisation, sanction_user
 from .group_for_group import group_for_role
 from .tiles_for_region import return_tile_for_region
+
+from .comorbidity_choices import get_comorbidity_choices
---
+++ epilepsy12/common_view_functions/comorbidity_choices.py
@@ -0,0 +1,23 @@
+from django.apps import apps
+from ..constants import COLIN_EDIT_SNOMED_NEURODISABILITY_REFSET
+
+def get_comorbidity_choices(multiaxial_diagnosis, comorbidity_id=None):
+    """
+    Returns list of comorbidities to populate dropdown lists
+    Removes any items that have already been selected
+    Accepts a multiaxial diagnosis instance
+    """
+
+    Comorbidity = apps.get_model("epilepsy12", "Comorbidity")
+    ComorbidityList = apps.get_model("epilepsy12", "ComorbidityList")
+
+    all_selected_comorbidityentities = Comorbidity.objects.filter(
+        multiaxial_diagnosis=multiaxial_diagnosis
+    ).exclude(pk=comorbidity_id).values_list("comorbidityentity", flat=True)
+
+    comorbidity_choices = (
+        ComorbidityList.objects.all().filter(preferredTerm__in=COLIN_EDIT_SNOMED_NEURODISABILITY_REFSET).exclude(pk__in=all_selected_comorbidityentities)
+        .order_by("preferredTerm")
+    )
+
+    return comorbidity_choices
