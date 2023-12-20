96fb2fed22809bcee79779fac8ff3b013ddd0c5c
anchit-chandran <anchit97123@gmail.com>
2023-12-20 10:47:00 +0000

    replace print w logging in signals.py
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- epilepsy12/signals.py
+++ epilepsy12/signals.py
@@ -29,7 +29,7 @@ def log_user_login_failed(sender, request, user=None, **kwargs):
         VisitActivity.objects.create(
             activity=2, ip_address=get_client_ip(request), epilepsy12user=user
         )
-        print(f"{user} ({user.email}) failed log in from {get_client_ip(request)}.")
+        logging.info(f"{user} ({user.email}) failed log in from {get_client_ip(request)}.")
     elif "credentials" in kwargs:
         if Epilepsy12User.objects.filter(
             email=kwargs["credentials"]["username"]
@@ -38,16 +38,16 @@ def log_user_login_failed(sender, request, user=None, **kwargs):
             VisitActivity.objects.create(
                 activity=2, ip_address=get_client_ip(request), epilepsy12user=user
             )
-            print(f"{user} ({user.email}) failed log in from {get_client_ip(request)}.")
+            logging.info(f"{user} ({user.email}) failed log in from {get_client_ip(request)}.")
         else:
-            print("Login failure by unknown user")
+            logging.info("Login failure by unknown user")
     else:
-        print("Login failure by unknown user")
+        logging.info("Login failure by unknown user")
 
 
 @receiver(user_logged_out)
 def log_user_logout(sender, request, user, **kwargs):
-    print(f"{user} ({user.email}) logged out from {get_client_ip(request)}.")
+    logging.info(f"{user} ({user.email}) logged out from {get_client_ip(request)}.")
     VisitActivity.objects.create(
         activity=3, ip_address=get_client_ip(request), epilepsy12user=user
     )
