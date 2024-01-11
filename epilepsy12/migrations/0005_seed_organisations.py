74c2bc13059c70bec6bf5a1b6f5fdfbb1f073861
eatyourpeas <simon@eatyourpeas.co.uk>
2024-01-11 18:34:16 +0000

    fix migration error on Organisation email field change

--- epilepsy12/migrations/0005_seed_organisations.py
+++ epilepsy12/migrations/0005_seed_organisations.py
@@ -16,6 +16,7 @@ from ..constants import (
 # Logging setup
 logger = logging.getLogger(__name__)
 
+
 def seed_organisations(apps, schema_editor):
     """
     Seed function which populates the Organisation table from JSON.
@@ -72,7 +73,6 @@ def seed_organisations(apps, schema_editor):
                     address2=rcpch_organisation["Address2"],
                     address3=rcpch_organisation["Address3"],
                     city=rcpch_organisation["City"],
-                    email=rcpch_organisation["Email"],
                     county=rcpch_organisation["County"],
                     latitude=latitude,
                     longitude=longitude,
