9ac04800cabcfd7b113675d9ca5b8dd3d1861224
eatyourpeas <eatyourpeasapps@gmail.com>
2024-01-16 21:24:50 +0000

    order epilepsy context by patient

--- epilepsy12/models_folder/epilepsy_context.py
+++ epilepsy12/models_folder/epilepsy_context.py
@@ -1,4 +1,3 @@
-
 # django
 from django.contrib.gis.db import models
 
@@ -11,7 +10,9 @@ from ..constants import OPT_OUT_UNCERTAIN
 from .time_and_user_abstract_base_classes import *
 
 
-class EpilepsyContext(TimeStampAbstractBaseClass, UserStampAbstractBaseClass, HelpTextMixin):
+class EpilepsyContext(
+    TimeStampAbstractBaseClass, UserStampAbstractBaseClass, HelpTextMixin
+):
     """
     This class records contextual information that defines epilepsy risk.
     It references the FirstPaediatricAssessment class, as each case optionally has a single epilepsy context.
@@ -19,48 +20,48 @@ class EpilepsyContext(TimeStampAbstractBaseClass, UserStampAbstractBaseClass, He
 
     previous_febrile_seizure = models.CharField(
         help_text={
-            'label': "At any point in time has the child had febrile seizure(s)?",
-            'reference': "At any point in time has the child had febrile seizure(s)?",
+            "label": "At any point in time has the child had febrile seizure(s)?",
+            "reference": "At any point in time has the child had febrile seizure(s)?",
         },
         max_length=2,
         choices=OPT_OUT_UNCERTAIN,
         default=None,
-        null=True
+        null=True,
     )
     previous_acute_symptomatic_seizure = models.CharField(
         help_text={
-            'label': "At any point in time has the child had acute symptomatic seizure(s)?",
-            'reference': "At any point in time has the child had acute symptomatic seizure(s)?"
+            "label": "At any point in time has the child had acute symptomatic seizure(s)?",
+            "reference": "At any point in time has the child had acute symptomatic seizure(s)?",
         },
         max_length=2,
         choices=OPT_OUT_UNCERTAIN,
         default=None,
-        null=True
+        null=True,
     )
     is_there_a_family_history_of_epilepsy = models.CharField(
         help_text={
-            'label': "Is there a family history of epilepsy?",
-            'reference': "Is there a family history of epilepsy?"
+            "label": "Is there a family history of epilepsy?",
+            "reference": "Is there a family history of epilepsy?",
         },
         max_length=3,
         choices=OPT_OUT_UNCERTAIN,
         default=None,
-        null=True
+        null=True,
     )
     previous_neonatal_seizures = models.CharField(
         help_text={
-            'label': 'Were there seizures in the neonatal period?',
-            'reference': 'Were there seizures in the neonatal period?'
+            "label": "Were there seizures in the neonatal period?",
+            "reference": "Were there seizures in the neonatal period?",
         },
         max_length=2,
         choices=OPT_OUT_UNCERTAIN,
         default=None,
-        null=True
+        null=True,
     )
     diagnosis_of_epilepsy_withdrawn = models.BooleanField(
         help_text={
-            'label': "Has the diagnosis of epilepsy been withdrawn?",
-            'reference': 'In the first year after first assessment, has a diagnosis of epilepsy been withdrawn because it has been subsequently deemed incorrect?'
+            "label": "Has the diagnosis of epilepsy been withdrawn?",
+            "reference": "In the first year after first assessment, has a diagnosis of epilepsy been withdrawn because it has been subsequently deemed incorrect?",
         },
         null=True,
         default=None,
@@ -68,17 +69,17 @@ class EpilepsyContext(TimeStampAbstractBaseClass, UserStampAbstractBaseClass, He
 
     were_any_of_the_epileptic_seizures_convulsive = models.BooleanField(
         help_text={
-            'label': "Were any of the epileptic seizures convulsive?",
-            'reference': "Were any of the epileptic seizures convulsive?",
+            "label": "Were any of the epileptic seizures convulsive?",
+            "reference": "Were any of the epileptic seizures convulsive?",
         },
         default=None,
-        null=True
+        null=True,
     )
 
     experienced_prolonged_generalized_convulsive_seizures = models.CharField(
         help_text={
-            'label': "Has the child at any point in time experienced prolonged generalised seizures?",
-            'reference': "Has the child at any point in time experienced prolonged generalised convulsive seizures > 5 min duration (or successive continuing > 5min)?",
+            "label": "Has the child at any point in time experienced prolonged generalised seizures?",
+            "reference": "Has the child at any point in time experienced prolonged generalised convulsive seizures > 5 min duration (or successive continuing > 5min)?",
         },
         max_length=2,
         default=None,
@@ -87,10 +88,9 @@ class EpilepsyContext(TimeStampAbstractBaseClass, UserStampAbstractBaseClass, He
     )
     experienced_prolonged_focal_seizures = models.CharField(
         help_text={
-            'label': "Has the child at any point in time experienced prolonged focal seizures?",
-            'reference': "Has the child at any point in time experienced prolonged focal seizures > 5 min duration (or successive continuing > 5min)?",
+            "label": "Has the child at any point in time experienced prolonged focal seizures?",
+            "reference": "Has the child at any point in time experienced prolonged focal seizures > 5 min duration (or successive continuing > 5min)?",
         },
-
         max_length=2,
         default=None,
         null=True,
@@ -109,23 +109,19 @@ class EpilepsyContext(TimeStampAbstractBaseClass, UserStampAbstractBaseClass, He
 
     # relationships
     registration = models.OneToOneField(
-        'epilepsy12.Registration',
+        "epilepsy12.Registration",
         on_delete=models.CASCADE,
         # verbose_name="Related Registration",
-        null=True
+        null=True,
     )
 
     class Meta:
-        verbose_name = 'Epilepsy Context'
-        verbose_name_plural = 'Epilepsy Contexts'
+        verbose_name = "Epilepsy Context"
+        verbose_name_plural = "Epilepsy Contexts"
+        ordering = ["registration__case"]
 
-    def save(
-            self,
-            *args, **kwargs) -> None:
+    def save(self, *args, **kwargs) -> None:
         return super().save(*args, **kwargs)
 
     def __str__(self) -> str:
-        if self.previous_neonatal_seizures or self.diagnosis_of_epilepsy_withdrawn or self.previous_acute_symptomatic_seizure or self.previous_febrile_seizure:
-            return 'This child has had previous symptomatic or neonatal or febrile seizures.'
-        else:
-            return 'This child has never had epileptic seizures or a previous diagnosis has been withdrawn.'
+        return f"Epilepsy context for {self.registration.case}"
