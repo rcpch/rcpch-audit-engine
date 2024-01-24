af961892de7261ebaabcfdc577dfaba32de9768a
Marcus Baw <marcusbaw@gmail.com>
2024-01-24 10:12:33 +0000

    refactor of git_context_processor

--- rcpch-audit-engine/git_context_processor.py
+++ rcpch-audit-engine/git_context_processor.py
@@ -1,34 +1,37 @@
+"""
+This module contains a function to get the active branch and latest commit hash
+from the git repo.
+"""
+
 from pathlib import Path
+import logging
+
+# set up logging
+logger = logging.getLogger(__name__)
 
 
-def get_active_branch_and_commit(request):
+def get_active_branch_and_commit(_request):
     """
     Gets the active branch and latest commit hash from the git repo
     """
 
-    # Load the txt file containing hash
-
-    try:
-        head_dir = Path(".") / ".git" / "HEAD"
-        with head_dir.open("r") as f:
-            head_branch = f.read().splitlines()
-        for line in head_branch:
-            if line[0:4] == "ref:":
-                active_git_branch = line.partition("refs/heads/")[2]
-    except:
-        active_git_branch = "[branch name not found]"
-
-    try:
-        refs_dir = Path(".") / ".git" / "refs" / "heads" / active_git_branch
-        with refs_dir.open("r") as f:
-            latest_git_commit = f.read().splitlines()[0]
-
-    except:
-        latest_git_commit = "[latest commit hash not found]"
-
-    admin_data = {
-        "latest_git_commit": latest_git_commit,
-        "active_git_branch": active_git_branch,
+    # set defaults/fallback for values in git_data dict
+    git_data = {
+        "active_git_branch": "[branch name not found]",
+        "latest_git_commit": "[latest commit hash not found]",
     }
 
-    return admin_data
+    head_dir = Path(".") / ".git" / "HEAD"
+    with head_dir.open("r") as f:
+        head_branch = f.read().splitlines()
+    for line in head_branch:
+        if line[0:4] == "ref:":
+            git_data["active_git_branch"] = line.partition("refs/heads/")[2]
+    logger.info("Active git branch: %s", git_data["active_git_branch"])
+
+    refs_dir = Path(".") / ".git" / "refs" / "heads" / git_data["active_git_branch"]
+    with refs_dir.open("r") as f:
+        git_data["latest_git_commit"] = f.read().splitlines()[0]
+    logger.info("Latest git commit: %s", git_data["latest_git_commit"])
+
+    return git_data
