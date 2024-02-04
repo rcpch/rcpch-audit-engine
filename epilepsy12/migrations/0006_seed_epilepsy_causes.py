b9799a47e1f9b88dafbc3ac9766c21410322bfc0
eatyourpeas <eatyourpeasapps@gmail.com>
2024-02-04 19:04:29 +0000

    add cmn to migration0006

--- epilepsy12/migrations/0006_seed_epilepsy_causes.py
+++ epilepsy12/migrations/0006_seed_epilepsy_causes.py
@@ -58,6 +58,7 @@ def seed_epilepsy_causes(apps, schema_editor):
     # Neonatal cerebral haemorrhage - 261808007
     # Cornelia de Lange Syndrome (CdLS) - 40354009
     # Anoxic encephalopathy - 389098007
+    # Congenital Melanocytic Naevus – 398696001
     extra_concept_ids = [
         764946008,
         52767006,
@@ -68,6 +69,7 @@ def seed_epilepsy_causes(apps, schema_editor):
         261808007,
         40354009,
         389098007,
+        398696001,
     ]
     add_epilepsy_cause_list_by_sctid(extra_concept_ids=extra_concept_ids)
 
