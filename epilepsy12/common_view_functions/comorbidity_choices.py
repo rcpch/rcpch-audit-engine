1ac70d539f1e5373553ee3df368975d1d9b2e1ec
eatyourpeas <simon@eatyourpeas.co.uk>
2024-04-27 10:29:11 +0100

    fix comorbidity test error not storing item from comorbidity list

--- epilepsy12/common_view_functions/comorbidity_choices.py
+++ epilepsy12/common_view_functions/comorbidity_choices.py
@@ -1,6 +1,7 @@
 from django.apps import apps
 from ..constants import COLIN_EDIT_SNOMED_NEURODISABILITY_REFSET
 
+
 def get_comorbidity_choices(multiaxial_diagnosis, comorbidity_id=None):
     """
     Returns list of comorbidities to populate dropdown lists
@@ -11,12 +12,16 @@ def get_comorbidity_choices(multiaxial_diagnosis, comorbidity_id=None):
     Comorbidity = apps.get_model("epilepsy12", "Comorbidity")
     ComorbidityList = apps.get_model("epilepsy12", "ComorbidityList")
 
-    all_selected_comorbidityentities = Comorbidity.objects.filter(
-        multiaxial_diagnosis=multiaxial_diagnosis
-    ).exclude(pk=comorbidity_id).values_list("comorbidityentity", flat=True)
+    all_selected_comorbidityentities = (
+        Comorbidity.objects.filter(multiaxial_diagnosis=multiaxial_diagnosis)
+        .exclude(pk=comorbidity_id)
+        .values_list("comorbidityentity", flat=True)
+    )
 
     comorbidity_choices = (
-        ComorbidityList.objects.all().filter(preferredTerm__in=COLIN_EDIT_SNOMED_NEURODISABILITY_REFSET).exclude(pk__in=all_selected_comorbidityentities)
+        ComorbidityList.objects.all()
+        .filter(preferredTerm__in=COLIN_EDIT_SNOMED_NEURODISABILITY_REFSET)
+        .exclude(pk__in=all_selected_comorbidityentities)
         .order_by("preferredTerm")
     )
 
