cdc12f5364f2a02f4ec6be0e64248ceb2bac3f21
Michael Barton <michael.barton@rcpch.ac.uk>
2024-08-19 14:51:48 +0100

    Periodically update the postgres access token
    At the moment that's achieved by a cron job which we won't be able to run once migrated to run within Azure Container Apps

--- epilepsy12/apps.py
+++ epilepsy12/apps.py
@@ -1,10 +1,30 @@
+import threading
+
 from django.apps import AppConfig
 
+from .management.commands.write_azure_pg_password_file import write_azure_pg_password_file 
+
+def periodically_update_azure_pg_password_file():
+    update_interval = 60 * 15 # 15 minutes
+
+    def _periodically_update_azure_pg_password_file():
+        write_azure_pg_password_file()
+        
+        thread = threading.Timer(update_interval, _periodically_update_azure_pg_password_file)
+        thread.daemon = True
+        thread.start()
+    
+    thread = threading.Timer(update_interval, _periodically_update_azure_pg_password_file)
+    thread.daemon = True
+    thread.start()
 
 class Epilepsy12Config(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'epilepsy12'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "epilepsy12"
 
     def ready(self) -> None:
         import epilepsy12.signals
+
+        periodically_update_azure_pg_password_file()
+
         return super().ready()
