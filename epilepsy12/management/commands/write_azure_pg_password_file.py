aea7aa6acc8cb43764d2f215ae97d8614f4a8590
Michael Barton <michael.barton@rcpch.ac.uk>
2024-08-19 14:51:48 +0100

    Periodically update the postgres access token
    At the moment that's achieved by a cron job which we won't be able to run once migrated to run within Azure Container Apps

--- epilepsy12/management/commands/write_azure_pg_password_file.py
+++ epilepsy12/management/commands/write_azure_pg_password_file.py
@@ -1,5 +1,4 @@
 import os
-import requests
 
 from django.core.management.base import BaseCommand
 from azure.identity import DefaultAzureCredential
@@ -12,19 +11,23 @@ from azure.identity import DefaultAzureCredential
 # Django specifically does not allow you to update settings at
 # runtime so we store the connection details in a separate file.
 
+def write_azure_pg_password_file():
+    password_file = os.environ.get('NPDA_POSTGRES_DB_PASSWORD_FILE')
+
+    if not password_file:
+        return
+
+    password = DefaultAzureCredential().get_token("https://ossrdbms-aad.database.windows.net").token
+
+    with open(password_file, "w") as f:
+        f.write(f"*:*:*:*:{password}")
+    
+    # libpg silently ignores the file if it's readable by anyone else
+    os.chmod(password_file, 0o600)
+
+
 class Command(BaseCommand):
     help = "Generate a Postgres password file to use an Azure managed identity to authenticate with the database."
 
     def handle(self, *args, **options):
-        password_file = os.environ.get('E12_POSTGRES_DB_PASSWORD_FILE')
-
-        if not password_file:
-            return
-
-        password = DefaultAzureCredential().get_token("https://ossrdbms-aad.database.windows.net").token
-
-        with open(password_file, "w") as f:
-            f.write(f"*:*:*:*:{password}")
-        
-        # libpg silently ignores the file if it's readable by anyone else
-        os.chmod(password_file, 0o600)
+        write_azure_pg_password_file()
