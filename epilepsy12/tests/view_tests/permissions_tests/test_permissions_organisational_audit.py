40832b6736fa5e3aa2c59504e6cd80cb54394c60
Michael Barton <michael@mbarton.cc>
2024-09-20 14:53:11 +0100

    Parameterise permission tests

--- epilepsy12/tests/view_tests/permissions_tests/test_permissions_organisational_audit.py
+++ epilepsy12/tests/view_tests/permissions_tests/test_permissions_organisational_audit.py
@@ -1,23 +1,57 @@
-def test_anonymous_user_cannot_access_organisational_audit():
+import pytest
+
+from django.urls import reverse
+
+@pytest.mark.parametrize("group", [
+    pytest.param("trust", id = "Trust"),
+    pytest.param("health_board", id = "Health Board")
+])
+def test_anonymous_user_cannot_access_organisational_audit(client, group):
+    response = client.get(reverse(f"organisational_audit_{group}", kwargs={ "id": 1 }))
+
+    assert(response.status_code == 302)
+    assert(response.headers["Location"] == "/account/login/")
+
+
+@pytest.mark.parametrize("group,role", [
+    ("trust", "clinician"),
+    ("trust", "administrator"),
+    ("health_board", "clinician"),
+    ("health_board", "administrator")
+])
+def test_non_leads_cannot_access_organisational_audit_for_their_group(client, seed_groups_fixture, seed_users_fixture, group, role):
+    # user =  
+
     raise Exception('not implemented yet')
 
-def test_clinicians_cannot_access_organisational_audit():
+
+@pytest.mark.parametrize("group,role", [
+    ("trust", "clinician"),
+    ("trust", "administrator"),
+    ("health_board", "clinician"),
+    ("health_board", "administrator")
+])
+def test_non_leads_cannot_access_organisational_audit_for_another_group(seed_groups_fixture, seed_users_fixture, group, role):
+    # user =  
+
     raise Exception('not implemented yet')
 
-def test_administrators_cannot_access_organisational_audit():
-    raise Exception('not implemented yet')
 
 def test_lead_clinician_can_access_organisational_audit_for_their_trust():
     raise Exception('not implemented yet')
 
+
 def test_lead_clinician_cannot_access_organisational_audit_for_another_trust():
     raise Exception('not implemented yet')
+
 
 def test_lead_clinician_cannot_access_organisational_audit_for_another_health_board():
     raise Exception('not implemented yet')
 
+
 def test_rcpch_audit_team_can_access_organisational_audit_for_any_trust():
     raise Exception('not implemented yet')
+
 
 def test_rcpch_audit_team_can_access_organisational_audit_for_any_health_board():
     raise Exception('not implemented yet')
