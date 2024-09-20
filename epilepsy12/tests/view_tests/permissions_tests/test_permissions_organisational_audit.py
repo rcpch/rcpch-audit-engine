1a0e36873dc36ca99a2f738a37864d840bedc964
Michael Barton <michael@mbarton.cc>
2024-09-20 14:00:03 +0100

    Permission test skeleton

---
+++ epilepsy12/tests/view_tests/permissions_tests/test_permissions_organisational_audit.py
@@ -0,0 +1,23 @@
+def test_anonymous_user_cannot_access_organisational_audit():
+    raise Exception('not implemented yet')
+
+def test_clinicians_cannot_access_organisational_audit():
+    raise Exception('not implemented yet')
+
+def test_administrators_cannot_access_organisational_audit():
+    raise Exception('not implemented yet')
+
+def test_lead_clinician_can_access_organisational_audit_for_their_trust():
+    raise Exception('not implemented yet')
+
+def test_lead_clinician_cannot_access_organisational_audit_for_another_trust():
+    raise Exception('not implemented yet')
+
+def test_lead_clinician_cannot_access_organisational_audit_for_another_health_board():
+    raise Exception('not implemented yet')
+
+def test_rcpch_audit_team_can_access_organisational_audit_for_any_trust():
+    raise Exception('not implemented yet')
+
+def test_rcpch_audit_team_can_access_organisational_audit_for_any_health_board():
+    raise Exception('not implemented yet')
