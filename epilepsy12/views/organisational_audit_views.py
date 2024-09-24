651cac984b1ac1e8af53009bb0c17c402e3a6fb9
Michael Barton <michael.barton@rcpch.ac.uk>
2024-09-24 14:08:51 +0100

    wip save submission on edit

--- epilepsy12/views/organisational_audit_views.py
+++ epilepsy12/views/organisational_audit_views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from ..models import Trust, LocalHealthBoard, OrganisationalAuditSubmissionPeriod
+from ..models import Trust, LocalHealthBoard, OrganisationalAuditSubmissionPeriod, OrganisationalAuditSubmission
 from ..forms_folder import OrganisationalAuditSubmissionForm
 
 from ..decorator import (
@@ -8,16 +8,17 @@ from ..decorator import (
     user_may_view_organisational_audit
 )
 
-def _organisational_audit(request, group_name):
-    submission_period = OrganisationalAuditSubmissionPeriod.objects.filter(is_open=True).order_by('-year').first()
-
+def _organisational_audit(request, group_name, submission_period, submission):
     context = {
         "group_name": group_name,
         "submission_period": submission_period,
-        "form": OrganisationalAuditSubmissionForm()
+        "form": OrganisationalAuditSubmissionForm(submission)
     }
     
     if request.method == "POST":
+        context["form"] = OrganisationalAuditSubmissionForm(request.POST)
+        context["form"].save()
+
         return render(request, "epilepsy12/partials/organisational_audit_form.html", context)
 
     return render(request, "epilepsy12/organisational_audit.html", context)
@@ -25,13 +26,25 @@ def _organisational_audit(request, group_name):
 @login_and_otp_required()
 @user_may_view_organisational_audit(Trust, "trust")
 def organisational_audit_trust(request, id):
+    submission_period = OrganisationalAuditSubmissionPeriod.objects.filter(is_open=True).order_by('-year').first()
+
     trust = Trust.objects.get(id=id)
 
-    return _organisational_audit(request, trust.name)
+    submission = None
+    if submission_period.submissions:
+        submission = submission_period.submissions.filter(trust=trust).first()
+
+    return _organisational_audit(request, trust.name, submission_period, submission)
 
 @login_and_otp_required()
 @user_may_view_organisational_audit(LocalHealthBoard, "local_health_board")
 def organisational_audit_local_health_board(request, id):
+    submission_period = OrganisationalAuditSubmissionPeriod.objects.filter(is_open=True).order_by('-year').first()
+    
     local_health_board = LocalHealthBoard.objects.get(id=id)
 
-    return _organisational_audit(request, local_health_board.name)
+    submission = None
+    if submission_period.submissions:
+        submission = submission_period.submissions.filter(local_health_board=local_health_board).first()
+
+    return _organisational_audit(request, local_health_board.name, submission_period, submission)
