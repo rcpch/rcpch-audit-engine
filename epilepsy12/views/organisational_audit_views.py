49dcda74f7fe441676822c6a88231b4916cc0b00
Michael Barton <michael.barton@rcpch.ac.uk>
2024-09-24 11:42:36 +0100

    Show trust/health board title

--- epilepsy12/views/organisational_audit_views.py
+++ epilepsy12/views/organisational_audit_views.py
@@ -1,22 +1,33 @@
 from django.shortcuts import render
 
-from ..models import Trust, LocalHealthBoard
+from ..models import Trust, LocalHealthBoard, OrganisationalAuditSubmissionPeriod
 
 from ..decorator import (
     login_and_otp_required,
     user_may_view_organisational_audit
 )
 
+def _organisational_audit(request, group_name):
+    submission_period = OrganisationalAuditSubmissionPeriod.objects.filter(is_open=True).order_by('-year').first()
+    template_name = "epilepsy12/organisational_audit.html"
+
+    context = {
+        "group_name": group_name,
+        "submission_period": submission_period
+    }
+    
+    return render(request, template_name, context)
+
 @login_and_otp_required()
 @user_may_view_organisational_audit(Trust, "trust")
 def organisational_audit_trust(request, id):
-    context = {}
-    template_name = "epilepsy12/organisational_audit.html"
-    return render(request, template_name, context)
+    trust = Trust.objects.get(id=id)
+
+    return _organisational_audit(request, trust.name)
 
 @login_and_otp_required()
 @user_may_view_organisational_audit(LocalHealthBoard, "local_health_board")
 def organisational_audit_local_health_board(request, id):
-    context = {}
-    template_name = "epilepsy12/organisational_audit.html"
-    return render(request, template_name, context)
+    local_health_board = LocalHealthBoard.objects.get(id=id)
+
+    return _organisational_audit(request, local_health_board.name)
