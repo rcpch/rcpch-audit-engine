9603268ed7f9b14baaee8af26601597a6af19067
eatyourpeas <simon@eatyourpeas.co.uk>
2024-10-03 18:23:23 +0100

    add styling to dropdowns

--- epilepsy12/views/organisational_audit_views.py
+++ epilepsy12/views/organisational_audit_views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 
 from multiselectfield.utils import MSFList
 
-from ..models import Trust, LocalHealthBoard, OrganisationalAuditSubmissionPeriod, OrganisationalAuditSubmission
+from ..models import (
+    Trust,
+    LocalHealthBoard,
+    OrganisationalAuditSubmissionPeriod,
+    OrganisationalAuditSubmission,
+)
 from ..forms_folder import OrganisationalAuditSubmissionForm
 
-from ..decorator import (
-    login_and_otp_required,
-    user_may_view_organisational_audit
-)
+from ..decorator import login_and_otp_required, user_may_view_organisational_audit
 
 
 # On page load values are integers.
@@ -21,13 +23,14 @@ def get_selected_choice_indices_as_strings(field):
         return [str(field.value())]
     elif type(field.value()) in [list, MSFList]:
         return [str(value) for value in field.value()]
-        
+
     return []
+
 
 def show_child_field(parent, child):
     if not "field" in parent:
         return True
-    
+
     field = parent["field"]
     model_field = OrganisationalAuditSubmission._meta.get_field(field.name)
 
@@ -38,7 +41,7 @@ def show_child_field(parent, child):
     if not model_field.choices:
         if required_parent_value:
             return parent_value == required_parent_value
-        elif parent_value == '0':
+        elif parent_value == "0":
             return False
         else:
             return bool(parent_value)
@@ -47,6 +50,7 @@ def show_child_field(parent, child):
     other_choice_index = str(required_parent_value)
 
     return other_choice_index in selected_choices
+
 
 def accumulate_children(question, parent_hidden):
     ret = []
@@ -58,8 +62,9 @@ def accumulate_children(question, parent_hidden):
     for child in children:
         ret.append(child)
         ret.extend(accumulate_children(child, parent_hidden=question["hidden"]))
-    
+
     return ret
+
 
 def group_questions(fields_by_question_number):
     questions_by_section = {}
@@ -80,6 +85,7 @@ def group_questions(fields_by_question_number):
 
     return questions_by_section
 
+
 def group_form_fields(form):
     fields_by_question_number = {}
 
@@ -90,7 +96,7 @@ def group_form_fields(form):
 
         # TODO MRB: put the help text on the form to avoid migrations every time it changes?
         help_text = field.help_text or model_field.help_text or {}
-        
+
         section = help_text.get("section", "Other")
         parent_question_number = help_text.get("parent_question_number", None)
         parent = fields_by_question_number.get(parent_question_number, None)
@@ -110,7 +116,7 @@ def group_form_fields(form):
                 "question_number": parent_question_number,
                 "label": help_text.get("parent_question_label", None),
                 "reference": help_text.get("parent_question_reference", None),
-                "children": []
+                "children": [],
             }
 
             fields_by_question_number[parent_question_number] = parent
@@ -125,7 +131,7 @@ def group_form_fields(form):
                 "reference": help_text.get("reference", None),
                 "hidden": not show_child_field(parent, field),
                 "is_child": True,
-                "children": []
+                "children": [],
             }
 
             fields_by_question_number[question_number] = child
@@ -138,40 +144,41 @@ def group_form_fields(form):
                 "hide_question_number": hide_question_number,
                 "label": help_text.get("label", field.name),
                 "reference": help_text.get("reference", None),
-                "children": []
+                "children": [],
             }
-        
+
         ix += 1
-    
+
     questions_by_section = group_questions(fields_by_question_number)
 
     return questions_by_section
 
 
 def _organisational_audit(request, group_id, group_model, group_field):
-    submission_period = OrganisationalAuditSubmissionPeriod.objects.filter(is_open=True).order_by('-year').first()
+    submission_period = (
+        OrganisationalAuditSubmissionPeriod.objects.filter(is_open=True)
+        .order_by("-year")
+        .first()
+    )
 
     group = group_model.objects.get(id=group_id)
 
-    submission_filter = {
-        "submission_period": submission_period
-    }
+    submission_filter = {"submission_period": submission_period}
 
     submission_filter[group_field] = group
-    submission = OrganisationalAuditSubmission.objects.filter(**submission_filter).first()
+    submission = OrganisationalAuditSubmission.objects.filter(
+        **submission_filter
+    ).first()
 
     form = OrganisationalAuditSubmissionForm(instance=submission)
 
-    context = {
-        "group_name": group.name,
-        "submission_period": submission_period
-    }
+    context = {"group_name": group.name, "submission_period": submission_period}
 
     if request.method == "POST":
         if not submission:
             submission_args = {
                 "submission_period": submission_period,
-                "created_by": request.user
+                "created_by": request.user,
             }
             submission_args[group_field] = group
 
@@ -182,19 +189,24 @@ def _organisational_audit(request, group_id, group_model, group_field):
 
         context["questions_by_section"] = group_form_fields(form)
 
-        return render(request, "epilepsy12/partials/organisational_audit_form.html", context)
-    
+        return render(
+            request, "epilepsy12/partials/organisational_audit_form.html", context
+        )
+
     form = OrganisationalAuditSubmissionForm(instance=submission)
     context["questions_by_section"] = group_form_fields(form)
+    context["form"] = form
 
     return render(request, "epilepsy12/organisational_audit.html", context)
+
 
 @login_and_otp_required()
 @user_may_view_organisational_audit(Trust, "trust")
 def organisational_audit_trust(request, id):
-    return _organisational_audit(request, id, Trust, 'trust')
+    return _organisational_audit(request, id, Trust, "trust")
+
 
 @login_and_otp_required()
 @user_may_view_organisational_audit(LocalHealthBoard, "local_health_board")
 def organisational_audit_local_health_board(request, id):
-    return _organisational_audit(request, id, LocalHealthBoard, 'local_health_board')
+    return _organisational_audit(request, id, LocalHealthBoard, "local_health_board")
