6c72d434c90e310b21cb6e84030cf61f71e55296
Michael Barton <michael@mbarton.cc>
2024-10-08 12:44:38 +0100

    wip import

--- epilepsy12/management/commands/import_org_audit_submissions.py
+++ epilepsy12/management/commands/import_org_audit_submissions.py
@@ -2,6 +2,13 @@ import pandas as pd
 
 from django.core.management.base import BaseCommand
 
+from ...models import (
+    OrganisationalAuditSubmissionPeriod,
+    OrganisationalAuditSubmission,
+    Trust,
+    LocalHealthBoard
+)
+
 class Command(BaseCommand):
     help = "Import organisational audit submissions from CSV export"
 
@@ -13,9 +20,43 @@ class Command(BaseCommand):
             required=True,
             help="CSV file of submissions to import",
         )
+        parser.add_argument(
+            "-s",
+            "--submission-period",
+            type=int,
+            required=True,
+            help="ID of submission period to import into",
+        )
 
     def handle(self, *args, **options):
         file = options["file"]
         data = pd.read_csv(file)
 
-        print(f"!! {data}")
+        submission_period = OrganisationalAuditSubmissionPeriod.objects.get(
+            id=options["submission_period"]
+        )
+
+        for _, row in data.iterrows():
+            ods_code = row["SiteCode"]
+
+            submission = OrganisationalAuditSubmission()
+
+            try:
+                submission.trust = Trust.objects.get(ods_code=row["SiteCode"])
+            except Trust.DoesNotExist:
+                submission.local_health_board = LocalHealthBoard.objects.get(ods_code=row["SiteCode"])
+            
+            for column, value in row.to_dict().items():
+                if column.startswith("S"):
+                    if value == "Y":
+                        value = True
+                    elif value == "N":
+                        value = False
+
+                    setattr(submission, column, value)
+
+            submission.save()
+            
+            break
+
+            
