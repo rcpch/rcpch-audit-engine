4a616265c9e7bdd86f96ad796ba9d0ed6205ffa0
Michael Barton <michael@mbarton.cc>
2024-10-08 15:14:44 +0100

    first multiselect columns

--- epilepsy12/management/commands/import_org_audit_submissions.py
+++ epilepsy12/management/commands/import_org_audit_submissions.py
@@ -9,19 +9,8 @@ from ...models import (
     LocalHealthBoard
 )
 
-def include_column(column):
-    # TODO: adapt multiselect columns
-    if column.startswith("S01ESN") or \
-        column.startswith("S06Professionals") or \
-        column.startswith("S07ScreenForIssues") or \
-        column.startswith("S07MentalHealthQuestionnaire") or \
-        column.startswith("S07MentalHealthAgreedPathway") or \
-        column.startswith("S07DoesThisCompromise") or \
-        column.startswith("S07TrustAchieve") or \
-        column.startswith("S08AgreedReferral"):
-        return False
-
-    return column.startswith("S0") or column.startswith("S1")
+def adapt_multiselect_field(row, choices_to_column):
+    return [key for key, value in choices_to_column.items() if row[value] == 1]
 
 class Command(BaseCommand):
     help = "Import organisational audit submissions from CSV export"
@@ -61,12 +50,45 @@ class Command(BaseCommand):
             except Trust.DoesNotExist:
                 submission.local_health_board = LocalHealthBoard.objects.get(ods_code=row["SiteCode"])
             
-            for column, raw_value in row.to_dict().items():
-                if include_column(column):
-                    value = None if pd.isnull(raw_value) else raw_value
+            #######################
+            # Single value fields #
+            #######################
 
-                    print(f"!! {column} {raw_value} -> {value}:{type(value)}")
-                    setattr(submission, column, value)
+            for column, raw_value in row.to_dict().items():
+                # Multiselect fields handled below
+                if column.startswith("S01ESN") or \
+                    column.startswith("S06Professionals") or \
+                    column.startswith("S07ScreenForIssues") or \
+                    column.startswith("S07MentalHealthQuestionnaire") or \
+                    column.startswith("S07MentalHealthAgreedPathway") or \
+                    column.startswith("S07DoesThisCompromise") or \
+                    column.startswith("S07TrustAchieve") or \
+                    column.startswith("S08AgreedReferral") or \
+                    not column.startswith("S0") or \
+                    not column.startswith("S01"):
+                        continue
+                
+                value = None if pd.isnull(raw_value) else raw_value
+                setattr(submission, column, value)
+
+            ######################
+            # Multiselect fields #
+            ######################      
+
+            submission.S01ESNFunctions = adapt_multiselect_field(row, {
+                1: 'S01ESNEDVisit',
+                2: 'S01ESNHomeVisit',
+                3: 'S01ESNIndividualHealthcare',
+                4: 'S01ESNNurseLedClinic',
+                5: 'S01ESNNursePrescribing',
+                6: 'S01ESNRescueMedicationParent',
+                7: 'S01ESNRescueMedicationSchool',
+                8: 'S01ESNSchoolMeetings',
+                9: 'S01ESNWardVisits',
+                10: 'S01ESNNoneOfTheAbove'
+            })
+
+            print(submission.S01ESNFunctions)
 
             submission.save()
             
