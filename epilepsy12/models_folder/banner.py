4429b466e58920401ef4343cc5043b97cd16ac1f
Michael Barton <michael@mbarton.cc>
2024-10-15 13:24:47 +0100

    wip target banners to user role

--- epilepsy12/models_folder/banner.py
+++ epilepsy12/models_folder/banner.py
@@ -1,9 +1,15 @@
 from django.db import models
 
+from epilepsy12.constants.user_types import ROLES
+
 class Banner(models.Model):
     url_matcher = models.CharField(max_length=255)
     html = models.TextField()
     disabled = models.BooleanField(default=False)
+
+    # Only show the banner if the user has a given role (eg Lead Clinicians to see Org Audit banner)
+    # RCPCH audit team members always see banners so they can check how they look
+    user_role_to_target = models.PositiveSmallIntegerField(choices=ROLES, blank=True, null=True)
 
     def __str__(self):
         return f"Banner for {self.url_matcher}"
