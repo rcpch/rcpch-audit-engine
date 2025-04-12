9970b5c693a902126cce0839e4e585be1906e893
eatyourpeas <simon@eatyourpeas.co.uk>
2025-04-12 08:28:51 +0100

    add new kpi 8 calculation - note only for cohort 7 and above. add tests for the same

--- epilepsy12/common_view_functions/calculate_kpi_functions/score_kpi_8.py
+++ epilepsy12/common_view_functions/calculate_kpi_functions/score_kpi_8.py
@@ -2,6 +2,7 @@
 
 # django imports
 from django.apps import apps
+from django.db.models import Q
 
 # E12 imports
 from epilepsy12.constants import KPI_SCORE
@@ -64,6 +65,64 @@ def score_kpi_8(registration_instance, age_at_first_paediatric_assessment) -> in
         valproate.is_a_pregnancy_prevention_programme_in_place
         or valproate.has_a_valproate_annual_risk_acknowledgement_form_been_completed
     ):
+        return KPI_SCORE["PASS"]
+    else:
+        return KPI_SCORE["FAIL"]
+
+
+def score_kpi_8_topiramate(
+    registration_instance, age_at_first_paediatric_assessment
+) -> int:
+    AntiEpilepsyMedicine = apps.get_model("epilepsy12", "AntiEpilepsyMedicine")
+    Medicine = apps.get_model("epilepsy12", "Medicine")
+
+    """
+    Title: KPI 8: Medication and Reproduction Risks - Topiramate: Note this only applies to cohort 7
+
+    Description: Percentage of females on valproate treatment and females aged 12 years and above on topiramate with a risk acknowledgement form completed or Pregnancy Prevention Programme in place
+
+    Numerator:
+    (Number of all females on valproate OR females aged 12 years and above on topiramate)
+    AND
+    (Evidence of a risk acknowledgement form completed OR a Pregnancy Prevention Programme in place)
+
+    Denominator:
+    Number of all females on valproate OR females aged 12 years and above on topiramate
+    """
+    # set up parameters
+    male = registration_instance.case.sex == 1
+    female = registration_instance.case.sex == 2
+    age_12_or_above = age_at_first_paediatric_assessment >= 12
+    age_below_12 = age_at_first_paediatric_assessment < 12
+    valproate = AntiEpilepsyMedicine.objects.filter(
+        management=registration_instance.management,
+        medicine_entity=Medicine.objects.get(conceptId="387481005"),  # valproate
+    ).exists()
+    topiramate = AntiEpilepsyMedicine.objects.filter(
+        management=registration_instance.management,
+        medicine_entity=Medicine.objects.get(conceptId="387481005"),  # topiramate
+    ).exists()
+
+    # ineligible - Not on valproate and not ( >12 and on topiramate) or male
+    if male or not (valproate and (age_below_12 and topiramate)):
+        return KPI_SCORE["INELIGIBLE"]
+
+    # not scored
+    if registration_instance.management.has_an_aed_been_given is None:
+        return KPI_SCORE["NOT_SCORED"]
+
+    # not scored
+    if (
+        valproate.is_a_pregnancy_prevention_programme_in_place is None
+        or valproate.has_a_valproate_annual_risk_acknowledgement_form_been_completed
+        is None
+    ):
+        return KPI_SCORE["NOT_SCORED"]
+
+    if (
+        valproate.is_a_pregnancy_prevention_programme_in_place
+        or valproate.has_a_valproate_annual_risk_acknowledgement_form_been_completed
+    ) and (female and (age_12_or_above and topiramate) or valproate):
         return KPI_SCORE["PASS"]
     else:
         return KPI_SCORE["FAIL"]
--- epilepsy12/tests/common_view_functions_tests/calculate_kpi_tests/test_measure_8_topiramate.py
+++ epilepsy12/tests/common_view_functions_tests/calculate_kpi_tests/test_measure_8_topiramate.py
@@ -1,67 +1,74 @@
-"""
-Measure 8 `sodium_valproate` - Percentage of all females 12 years and above currently on valproate treatment with annual risk acknowledgement form completed
-
-Number of females >= 12yo diagnosed with epilepsy at first year AND on valproate AND (
-     annual risk acknowledgement forms completed
-     OR
-     pregnancy prevention programme in place
-    )
-
-- [x] Measure 8 passed (registration.kpi.sodium_valproate == 1) if (age_at_first_paediatric_assessment >= 12 and sex == 2 and medicine is valproate) and (is_a_pregnancy_prevention_programme_in_place==True OR has_a_valproate_annual_risk_acknowledgement_form_been_completed==True)
-- [x] Measure 8 failed (registration.kpi.sodium_valproate == 0) if (age_at_first_paediatric_assessment >= 12 and sex == 2 and medicine is valproate) and is_a_pregnancy_prevention_programme_in_place is False AND has_a_valproate_annual_risk_acknowledgement_form_been_completed is False
-
-- [x] Measure 8 ineligible (registration.kpi.sodium_valproate == 2) if age_at_first_paediatric_assessment < 12
-- [x] Measure 8 ineligible (registration.kpi.sodium_valproate == 2) if registration_instance.case.sex == 1
-- [x] Measure 8 ineligible (registration.kpi.sodium_valproate == 2) if registration_instance.management.has_an_aed_been_given == False
-- [x] Measure 8 ineligible (registration.kpi.sodium_valproate == 2) if AEM is not valproate or AEM is None
-"""
-
-# Standard imports
-from datetime import date
+# Python imports
 from dateutil.relativedelta import relativedelta
+from datetime import date
 
 # Third party imports
 import pytest
 
 # RCPCH imports
 from epilepsy12.common_view_functions import calculate_kpis
-from epilepsy12.constants import KPI_SCORE, SEX_TYPE
-from epilepsy12.models import KPI, AntiEpilepsyMedicine, Registration, Medicine
+from epilepsy12.constants import SEX_TYPE, KPI_SCORE
+from epilepsy12.models import (
+    AntiEpilepsyMedicine,
+    Medicine,
+    Registration,
+    KPI,
+)
 
 
 @pytest.mark.parametrize(
-    "is_a_pregnancy_prevention_programme_in_place, has_a_valproate_annual_risk_acknowledgement_form_been_completed,expected_score",
+    "over_12, valproate, topiramate, is_a_pregnancy_prevention_programme_in_place, has_a_valproate_annual_risk_acknowledgement_form_been_completed,expected_score",
     [
-        (True, True, KPI_SCORE["PASS"]),
-        (True, False, KPI_SCORE["PASS"]),
-        (False, True, KPI_SCORE["PASS"]),
-        (False, False, KPI_SCORE["FAIL"]),
-        (None, True, KPI_SCORE["NOT_SCORED"]),
-        (True, None, KPI_SCORE["NOT_SCORED"]),
+        (True, False, True, True, True, KPI_SCORE["PASS"]),
+        (False, True, False, True, False, KPI_SCORE["PASS"]),
+        (True, True, False, False, True, KPI_SCORE["PASS"]),
+        (True, False, True, False, False, KPI_SCORE["FAIL"]),
+        (False, True, False, False, False, KPI_SCORE["FAIL"]),
+        (True, False, True, None, True, KPI_SCORE["NOT_SCORED"]),
+        (False, True, False, True, None, KPI_SCORE["NOT_SCORED"]),
+        (True, False, True, None, None, KPI_SCORE["NOT_SCORED"]),
+        (False, True, False, None, True, KPI_SCORE["NOT_SCORED"]),
+        (True, False, True, None, False, KPI_SCORE["NOT_SCORED"]),
+        (False, False, False, True, True, KPI_SCORE["INELIGIBLE"]),
+        (False, False, False, False, True, KPI_SCORE["INELIGIBLE"]),
+        (False, False, False, True, False, KPI_SCORE["INELIGIBLE"]),
     ],
 )
 @pytest.mark.django_db
-def test_measure_8_sodium_valproate_risk_eligible(
+def test_measure_8_sodium_valproate_risk_eligible_topiramate(
     e12_case_factory,
+    over_12,
+    valproate,
+    topiramate,
     is_a_pregnancy_prevention_programme_in_place,
     has_a_valproate_annual_risk_acknowledgement_form_been_completed,
     expected_score,
 ):
     """
     *PASS*
-    1) (age_at_first_paediatric_assessment >= 12 && sex == 2 && medicine is valproate) AND ONE OF:
+    1) (age_at_first_paediatric_assessment >= 12 && sex == 2 && medicine is topiramate) AND ONE OF:
+            - is_a_pregnancy_prevention_programme_in_place==True
+            - has_a_valproate_annual_risk_acknowledgement_form_been_completed==True
+        or
+        (age_at_first_paediatric_assessment < 12 && sex == 2 && medicine is valproate) AND ONE OF:
             - is_a_pregnancy_prevention_programme_in_place==True
             - has_a_valproate_annual_risk_acknowledgement_form_been_completed==True
     *FAIL*
-    1) (age_at_first_paediatric_assessment >= 12 && sex == 2 && medicine is valproate) AND BOTH
+    1) (age_at_first_paediatric_assessment >= 12 && sex == 2 && medicine is Topiramate) AND BOTH
+            - is_a_pregnancy_prevention_programme_in_place==False
+            - has_a_valproate_annual_risk_acknowledgement_form_been_completed==False
+        or
+        (age_at_first_paediatric_assessment < 12 && sex == 2 && medicine is valproate) AND BOTH
             - is_a_pregnancy_prevention_programme_in_place==False
             - has_a_valproate_annual_risk_acknowledgement_form_been_completed==False
 
     """
 
-    # Explicitly set age to exactly 12yo and sex female (=2)
     FIRST_PAEDIATRIC_ASSESSMENT_DATE = date(2023, 1, 1)
-    DATE_OF_BIRTH = FIRST_PAEDIATRIC_ASSESSMENT_DATE - relativedelta(years=12)
+    if over_12:
+        DATE_OF_BIRTH = FIRST_PAEDIATRIC_ASSESSMENT_DATE - relativedelta(years=12)
+    else:
+        DATE_OF_BIRTH = FIRST_PAEDIATRIC_ASSESSMENT_DATE - relativedelta(years=11)
     SEX = SEX_TYPE[2][0]
 
     # create case
@@ -79,10 +86,17 @@ def test_measure_8_sodium_valproate_risk_eligible(
     AntiEpilepsyMedicine.objects.filter(management=management).delete()
 
     # create and save a valproate AEM entry with paramtrized constants
+    if topiramate:
+        medicine = Medicine.objects.get(medicine_name="Topiramate")
+    elif valproate:
+        medicine = Medicine.objects.get(medicine_name="Sodium valproate")
+    else:
+        medicine = Medicine.objects.get(medicine_name="Lorazepam")
+
     AntiEpilepsyMedicine.objects.create(
         management=management,
         is_rescue_medicine=False,
-        medicine_entity=Medicine.objects.get(medicine_name="Sodium valproate"),
+        medicine_entity=medicine,
         antiepilepsy_medicine_risk_discussed=True,
         is_a_pregnancy_prevention_programme_in_place=is_a_pregnancy_prevention_programme_in_place,
         has_a_valproate_annual_risk_acknowledgement_form_been_completed=has_a_valproate_annual_risk_acknowledgement_form_been_completed,
@@ -107,53 +121,47 @@ def test_measure_8_sodium_valproate_risk_eligible(
 
 
 @pytest.mark.parametrize(
-    "age, sex, aed_given, not_valproate, expected_score",
+    "age, sex, aed_given, not_valproate, topiramate, expected_score",
     [
         (
             relativedelta(years=11, months=11),
             SEX_TYPE[2][0],
             True,
+            True,
             False,
             KPI_SCORE["INELIGIBLE"],
-        ),  # 11y11mo, F, aed given, valproate
+        ),  # 11y11mo, F, aed given, topiramate
         (
             relativedelta(years=12),
             SEX_TYPE[1][0],
             True,
+            True,
             False,
             KPI_SCORE["INELIGIBLE"],
-        ),  # 12yo, M, aed given, valproate
+        ),  # 12yo, M, aed given, topiramate, not valproate
         (
             relativedelta(years=12),
             SEX_TYPE[2][0],
             False,
             True,
+            False,
             KPI_SCORE["INELIGIBLE"],
         ),  # 12yo, F, aed NOT given, NO valproate
-        (
-            relativedelta(years=12),
-            SEX_TYPE[2][0],
-            True,
-            True,
-            KPI_SCORE["INELIGIBLE"],
-        ),  # 12yo, F, aed given, NOT valproate
     ],
 )
 @pytest.mark.django_db
-def test_measure_8_sodium_valproate_risk_ineligible(
-    e12_case_factory, age, sex, aed_given, not_valproate, expected_score
+def test_measure_8_cohort_7_ineligible(
+    e12_case_factory, age, sex, aed_given, not_valproate, topiramate, expected_score
 ):
     """
     *INELIGIBLE*
-    1) age_at_first_paediatric_assessment < 12
+    1) age_at_first_paediatric_assessment < 12 and topiramate
     2) registration_instance.case.sex == 1
     3) registration_instance.management.has_an_aed_been_given == False
-    4) AEM is not valproate or AEM is None
-
+    4) AEM is not valproate and not topiramate or AEM is None
     """
-
     # Explicitly set paramtrized age and sex
-    FIRST_PAEDIATRIC_ASSESSMENT_DATE = date(2023, 1, 1)
+    FIRST_PAEDIATRIC_ASSESSMENT_DATE = date(2024, 1, 1)  # cohort 7
     DATE_OF_BIRTH = FIRST_PAEDIATRIC_ASSESSMENT_DATE - age
     SEX = sex
 
@@ -185,7 +193,6 @@ def test_measure_8_sodium_valproate_risk_ineligible(
                 is_rescue_medicine=False,
                 medicine_entity=Medicine.objects.get(medicine_name="Lorazepam"),
             )
-
         # create and save a valproate AEM entry. Only case is <12yoF or 12yoM
         else:
             AntiEpilepsyMedicine.objects.create(
@@ -193,6 +200,13 @@ def test_measure_8_sodium_valproate_risk_ineligible(
                 is_rescue_medicine=False,
                 medicine_entity=Medicine.objects.get(medicine_name="Sodium valproate"),
                 antiepilepsy_medicine_risk_discussed=True,
+            )
+
+        if topiramate:
+            AntiEpilepsyMedicine.objects.create(
+                management=management,
+                is_rescue_medicine=False,
+                medicine_entity=Medicine.objects.get(medicine_name="Topiramate"),
             )
 
     else:
