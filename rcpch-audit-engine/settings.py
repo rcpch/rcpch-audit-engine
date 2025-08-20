426e1301ea478d3f6158ae2763d640189a1883b2
Michael Barton <michael.barton@rcpch.ac.uk>
2025-08-20 18:03:32 +0100

    Ignore route that crashes silk

--- rcpch-audit-engine/settings.py
+++ rcpch-audit-engine/settings.py
@@ -362,3 +362,11 @@ SILKY_MAX_RESPONSE_BODY_SIZE = 1024  # If response body>1024 bytes, ignore
 
 SILKY_MAX_RECORDED_REQUESTS = 10**4
 SILKY_MAX_RECORDED_REQUESTS_CHECK_PERCENT = 1
+
+def my_custom_logic(request):
+    if request.path.endswith("/summary"):
+        return False
+
+    return True
+
+SILKY_INTERCEPT_FUNC = my_custom_logic
