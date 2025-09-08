e73ee073e063ad54803f08d7d5443a6f4584d056
Michael Barton <michael.barton@rcpch.ac.uk>
2025-09-08 18:02:38 +0100

    Check for password expiry on every login

--- epilepsy12/views/user_management_views.py
+++ epilepsy12/views/user_management_views.py
@@ -751,63 +751,51 @@ class RCPCHLoginView(TwoFactorLoginView):
     # Override successful login redirect to org summary page
     def done(self, form_list, **kwargs):
         response = super().done(form_list)
-        response_url = getattr(response, "url")
-        login_redirect_url = reverse(settings.LOGIN_REDIRECT_URL)
-        # Successful login, redirect to login page
-        if response_url == login_redirect_url:
-            user = self.get_user()
-            if not user.organisation_employer:
-                org_id = 1
-            else:
-                org_id = user.organisation_employer.id
-                # check for outstanding transfers in to this organisation
-                if Site.objects.filter(
-                    active_transfer=True, organisation=user.organisation_employer
-                ).exists() and user.has_perm(
-                    "epilepsy12.can_transfer_epilepsy12_lead_centre"
-                ):
-                    # there is an outstanding request for transfer in to this user's organisation. User is a lead clinician and can act on this
-                    transfers = Site.objects.filter(
-                        active_transfer=True, organisation=user.organisation_employer
-                    )
-                    for transfer in transfers:
-                        messages.info(
-                            self.request,
-                            f"{transfer.transfer_origin_organisation} have requested transfer of {transfer.case} to {user.organisation_employer} for their Epilepsy12 care. Please find {transfer.case} in the case table to accept or decline this transfer request.",
-                        )
+        user = self.get_user()
 
-            # time since last set password
-            delta = timezone.now() - user.password_last_set
-            # if user has not renewed password in last 90 days, redirect to login page
-            password_reset_date = user.password_last_set + timezone.timedelta(days=90)
-            if user.is_active and (password_reset_date <= timezone.now()):
-                messages.error(
-                    request=self.request,
-                    message=f"Your password has expired. Please reset it.",
-                )
-                # log user out
-                logout(self.request)
-                return redirect(reverse("password_reset"))
-            last_logged_in = VisitActivity.objects.filter(
-                activity=1, epilepsy12user=user
-            ).order_by("-activity_datetime")[:2]
-            if last_logged_in.count() > 1:
-                messages.info(
-                    self.request,
-                    f"You are now logged in as {user.email}. You last logged in at {timezone.localtime(last_logged_in[1].activity_datetime).strftime('%H:%M %p on %A, %d %B %Y')} from {last_logged_in[1].ip_address}.\nYou have {90-delta.days} days remaining until your password needs resetting.",
-                )
-            else:
-                messages.info(
-                    self.request,
-                    f"You are now logged in as {user.email}. Welcome to Epilepsy12! This is your first time logging in ({timezone.localtime(last_logged_in[0].activity_datetime).strftime('%H:%M %p on %A, %d %B %Y')} from {last_logged_in[0].ip_address}).",
-                )
-
-            return redirect(
-                reverse(
-                    "selected_organisation_summary",
-                    kwargs={"organisation_id": org_id},
-                )
+        # check for outstanding transfers in to this organisation
+        if Site.objects.filter(
+            active_transfer=True, organisation=user.organisation_employer
+        ).exists() and user.has_perm(
+            "epilepsy12.can_transfer_epilepsy12_lead_centre"
+        ):
+            # there is an outstanding request for transfer in to this user's organisation. User is a lead clinician and can act on this
+            transfers = Site.objects.filter(
+                active_transfer=True, organisation=user.organisation_employer
             )
+            for transfer in transfers:
+                messages.info(
+                    self.request,
+                    f"{transfer.transfer_origin_organisation} have requested transfer of {transfer.case} to {user.organisation_employer} for their Epilepsy12 care. Please find {transfer.case} in the case table to accept or decline this transfer request.",
+                )
+
+        # time since last set password
+        delta = timezone.now() - user.password_last_set
+        # if user has not renewed password in last 90 days, redirect to login page
+        password_reset_date = user.password_last_set + timezone.timedelta(days=90)
+        if user.is_active and (password_reset_date <= timezone.now()):
+            messages.error(
+                request=self.request,
+                message=f"Your password has expired. Please reset it.",
+            )
+            # log user out
+            logout(self.request)
+            return redirect(reverse("password_reset"))
+        
+        last_logged_in = VisitActivity.objects.filter(
+            activity=1, epilepsy12user=user
+        ).order_by("-activity_datetime")[:2]
+        if last_logged_in.count() > 1:
+            messages.info(
+                self.request,
+                f"You are now logged in as {user.email}. You last logged in at {timezone.localtime(last_logged_in[1].activity_datetime).strftime('%H:%M %p on %A, %d %B %Y')} from {last_logged_in[1].ip_address}.\nYou have {90-delta.days} days remaining until your password needs resetting.",
+            )
+        else:
+            messages.info(
+                self.request,
+                f"You are now logged in as {user.email}. Welcome to Epilepsy12! This is your first time logging in ({timezone.localtime(last_logged_in[0].activity_datetime).strftime('%H:%M %p on %A, %d %B %Y')} from {last_logged_in[0].ip_address}).",
+            )
+
         return response
 
 
