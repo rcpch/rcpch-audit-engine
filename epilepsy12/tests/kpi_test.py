0411280317b9b9217e4546ee81b9127e5d84a88f
mbarton <michael.barton@rcpch.ac.uk>
2026-02-10 17:47:42 +0000

    Failing test for Jersey in KPI export

---
+++ epilepsy12/tests/kpi_test.py
@@ -0,0 +1,94 @@
+from datetime import date
+
+import pytest
+
+from dateutil.relativedelta import relativedelta
+
+from ..models import Organisation
+from ..kpi import download_kpi_summary_as_csv
+from ..common_view_functions.calculate_kpis import calculate_kpis
+from ..common_view_functions.aggregate_by import update_all_kpi_agg_models
+
+
+@pytest.mark.django_db
+def test_remove_jersey_patients_from_kpi_export(
+    e12_case_factory,
+    seed_groups_fixture,
+    seed_users_fixture,
+):
+    english_org = Organisation.objects.filter(
+        country__name="England"
+    ).first()
+
+    jersey_org = Organisation.objects.filter(
+        country__name="Jersey"
+    ).first()
+
+    english_case = e12_case_factory(
+        date_of_birth=date(2013, 1, 1),
+        # Must have completed first year of care
+        registration__first_paediatric_assessment_date=date(2013, 1, 1) + relativedelta(years=10),
+        organisations__organisation=english_org,
+        # pass KPI 10
+        registration__management__individualised_care_plan_in_place=True,
+        registration__management__individualised_care_plan_includes_ehcp=True,
+    )
+
+    calculate_kpis(english_case.registration)
+    update_all_kpi_agg_models(organisation=english_org, cohort=english_case.registration.cohort, open_access=False)
+
+    jersey_case = e12_case_factory(
+        date_of_birth=date(2013, 1, 1),
+        # Must have completed first year of care
+        registration__first_paediatric_assessment_date=date(2013, 1, 1) + relativedelta(years=10),
+        organisations__organisation=jersey_org,
+        # pass KPI 10
+        registration__management__individualised_care_plan_in_place=True,
+        registration__management__individualised_care_plan_includes_ehcp=True,
+    )
+
+    calculate_kpis(jersey_case.registration)
+    update_all_kpi_agg_models(organisation=english_org, cohort=english_case.registration.cohort, open_access=False)
+
+    assert english_case.registration.cohort == jersey_case.registration.cohort
+
+    (
+        country_df,
+        trust_hb_df,
+        icb_df,
+        region_df,
+        network_df,
+        national_df,
+        reference_df,
+        trust_totals_df,
+        local_health_board_totals_df,
+        icb_totals_df,
+        nhs_england_region_totals_df,
+        openuk_network_totals_df,
+        country_totals_df,
+    ) = download_kpi_summary_as_csv(cohort=english_case.registration.cohort)
+
+    countries = set(country_df["Country"])
+    assert "England" in countries
+    assert "Jersey" not in countries
+
+    # Check Jersey patient isn't accidentally included in England
+    english_df = country_df.loc[country_df["Country"] == "England"]
+    english_kpi_10_row = english_df.loc[english_df["Measure"] == "10. School Individual Health Care Plan"].iloc[0]
+
+    assert english_kpi_10_row["Numerator"] == 1
+    assert english_kpi_10_row["Denominator"] == 1
+
+    countries = set(country_totals_df["Name"])
+    assert "England" in countries
+    assert "Jersey" not in countries
+
+    # Check Jersey patient isn't accidentally included in the other countries
+    english_df = country_totals_df.loc[country_totals_df["Name"] == "England"]
+    welsh_df = country_totals_df.loc[country_totals_df["Name"] == "Wales"]
+
+    assert english_df["All Registered Cases"].iloc[0] == 1
+    assert english_df["All Cases"].iloc[0] == 1
+
+    assert welsh_df["All Registered Cases"].iloc[0] == 0
+    assert welsh_df["All Cases"].iloc[0] == 0
